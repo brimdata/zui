95150ae7dbfb30e3355123c82b608b5030d6c509
James Kerr <jkerr@reflektive.com>
2018-08-10 14:46:33 -0700

    Adds autoUpdater

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const {app, BrowserWindow} = require("electron")
+const {app, BrowserWindow, dialog, autoUpdater} = require("electron")
 
+const server = "http://desktop-release.looky.cloud"
+const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+console.log(feed)
+autoUpdater.setFeedURL(feed)
 let win
 
 const createWindow = () => {
@@ -15,7 +19,13 @@ const createWindow = () => {
   })
 }
 
-app.on("ready", createWindow)
+app.on("ready", () => {
+  createWindow()
+  autoUpdater.checkForUpdates()
+  setInterval(() => {
+    autoUpdater.checkForUpdates()
+  }, 15 * 1000)
+})
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
@@ -27,4 +37,35 @@ app.on("activate", () => {
   if (win === null) {
     createWindow()
   }
+})
+
+autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Restart", "Later"],
+    title: "Application Update",
+    message: process.platform === "win32" ? releaseNotes : releaseName,
+    detail:
+      "A new version has been downloaded. Restart the application to apply the updates."
+  }
+
+  dialog.showMessageBox(dialogOpts, response => {
+    if (response === 0) autoUpdater.quitAndInstall()
+  })
+})
+
+autoUpdater.on("error", message => {
+  console.error("Pblem updating the app:", message)
+})
+
+autoUpdater.on("update-available", () => {
+  console.log("update available")
+})
+
+autoUpdater.on("update-not-available", () => {
+  console.log("Up to date!")
+})
+
+autoUpdater.on("checking-for-update", () => {
+  console.log("checking for update")
 })
