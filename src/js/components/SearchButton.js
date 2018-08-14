79331b7cd3d0a9a61428b241f509bb9bb1839b83
James Kerr <jkerr@reflektive.com>
2018-08-14 09:50:52 -0700

    Added options to the search button

--- src/js/components/SearchButton.js
+++ src/js/components/SearchButton.js
@@ -1,14 +1,36 @@
 import React from "react"
+import {ContextMenu, MenuItem} from "./ContextMenu"
 import MagGlass from "../icons/magnifying-glass-md.svg"
+import Arrow from "../icons/caret-bottom-sm.svg"
 
 class SearchButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {menuIsOpen: false}
+    this.openMenu = () => this.setState({menuIsOpen: true})
+    this.closeMenu = () => this.setState({menuIsOpen: false})
+  }
+
   render() {
     const {fetchMainSearch} = this.props
 
     return (
-      <button className="button search-button" onClick={fetchMainSearch}>
-        <MagGlass />
-      </button>
+      <div className="search-button-wrapper">
+        <button className="button search-button" onClick={fetchMainSearch}>
+          <MagGlass />
+        </button>
+
+        <button className="button options-button" onClick={this.openMenu}>
+          <Arrow />
+        </button>
+
+        {this.state.menuIsOpen && (
+          <ContextMenu onOutsideClick={this.closeMenu}>
+            <MenuItem>Save query</MenuItem>
+            <MenuItem>Load query</MenuItem>
+          </ContextMenu>
+        )}
+      </div>
     )
   }
 }
