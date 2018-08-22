c66d3d59e6a6388d0dd739ef97db4400c575f34c
James Kerr <jkerr@reflektive.com>
2018-08-22 12:18:40 -0700

    Removed old code

--- src/js/components/UidWaterfall.test.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import {shallow} from "enzyme"
-import UidWaterfall from "./UidWaterfall"
-
-test("clicking a path tag", () => {
-  shallow(<UidWaterfall broLogs={[]} />)
-})
