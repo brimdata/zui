b013c98e3d7726e321d5f69944fc85d9e38d9b65
James Kerr <jkerr838@gmail.com>
2018-09-18 14:10:38 -0700

    Added shortcut to remove all pins

--- src/js/initShortcuts.js
+++ src/js/initShortcuts.js
@@ -1,6 +1,12 @@
 import {ipcRenderer} from "electron"
-import {pinSearchBar} from "./actions/searchBar"
+import {pinSearchBar, removeAllSearchBarPins} from "./actions/searchBar"
 
 export default store => {
   ipcRenderer.on("pinSearch", () => store.dispatch(pinSearchBar()))
+  ipcRenderer.on("focusSearchBar", () => {
+    const el = document.getElementById("main-search-input")
+    el && el.focus()
+    el && el.select()
+  })
+  ipcRenderer.on("clearPins", () => store.dispatch(removeAllSearchBarPins()))
 }
