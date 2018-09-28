4fd44f130724aad5c94cf249d6310bc3dece995b
James Kerr <jkerr838@gmail.com>
2018-09-28 12:30:50 -0700

    Added some flow annotations in a few files

--- src/js/lib/Tuple.js
+++ src/js/lib/Tuple.js
@@ -1,13 +1,18 @@
-import uniqBy from "lodash/uniqBy"
+/* @flow */
+
+import {uniqBy} from "lodash"
 import isEqual from "lodash/isEqual"
 
-export const id = ([td, path, ts, uid]) => [td, path, ts, uid].join("_")
+type Tuple = string[]
 
-export const uniq = list => uniqBy(list, id)
+export const id = ([td, path, ts, uid]: Tuple) => [td, path, ts, uid].join("_")
 
-export const removeFrom = (list, tuple) =>
+export const uniq = (list: Tuple[]) => uniqBy(list, id)
+
+export const removeFrom = (list: Tuple[], tuple: Tuple): Tuple[] =>
   list.filter(item => !isSame(tuple, item))
 
-export const isSame = (a, b) => isEqual(id(a), id(b))
+export const isSame = (a: Tuple, b: Tuple) => isEqual(id(a), id(b))
 
-export const contains = (list, tuple) => list.find(el => isSame(el, tuple))
+export const contains = (list: Tuple[], tuple: Tuple) =>
+  list.find(el => isSame(el, tuple))
