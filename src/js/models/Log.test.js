970e1d87051d57c083f246854444dd710f470993
James Kerr <jkerr838@gmail.com>
2018-10-03 15:48:52 -0700

    Added tests for new log methods

--- src/js/models/Log.test.js
+++ src/js/models/Log.test.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import Log from "./Log"
+import mockConnLog from "../test/mockConnLog"
 
 test("isSame when they are the same", () => {
   const a = new Log(
@@ -37,4 +38,40 @@ test("isSame when they are different", () => {
   )
 
   expect(Log.isSame(a, b)).toBe(false)
+})
+
+test("getSec on a time field", () => {
+  const log = mockConnLog()
+  // "1425612054.369843"
+  expect(log.getSec("ts")).toEqual(1425612054)
+})
+
+test("getSec on a duration field", () => {
+  const log = mockConnLog()
+  // "2.000293"
+  expect(log.getSec("duration")).toEqual(2)
+})
+
+test("getNs on a time field", () => {
+  const log = mockConnLog()
+  // "1425612054.369843"
+  expect(log.getNs("ts")).toEqual(369843000)
+})
+
+test("getNs on a time field", () => {
+  const log = mockConnLog()
+  // "2.000293"
+  expect(log.getNs("duration")).toEqual(293000)
+})
+
+test("getSec on non time field", () => {
+  const log = mockConnLog()
+
+  expect(() => log.getSec("_path")).toThrow("_path is not a time type")
+})
+
+test("getNs on non time field", () => {
+  const log = mockConnLog()
+
+  expect(() => log.getNs("_path")).toThrow("_path is not a time type")
 })
