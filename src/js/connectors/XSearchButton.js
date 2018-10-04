b0dc818b6c6bc0f3dfa42c7112dda7b272683c87
James Kerr <jkerr838@gmail.com>
2018-10-04 14:22:35 -0700

    Refactored Debug Modal

--- src/js/connectors/XSearchButton.js
+++ src/js/connectors/XSearchButton.js
@@ -2,14 +2,8 @@ import {connect} from "react-redux"
 import {bindActionCreators} from "redux"
 import * as actions from "../actions/searchBar"
 import SearchButton from "../components/SearchButton"
-import {getSearchProgram, getAst} from "../reducers/searchBar"
-
-const stateToProps = state => ({
-  ast: getAst(state),
-  searchProgram: getSearchProgram(state)
-})
 
 export default connect(
-  stateToProps,
+  null,
   dispatch => bindActionCreators(actions, dispatch)
 )(SearchButton)
