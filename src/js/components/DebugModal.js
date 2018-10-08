bb7a4ab93255067f2640a8c924ee13a7e0fd4e97
James Kerr <jkerr838@gmail.com>
2018-10-08 16:50:04 -0700

    Live debug the ast

--- src/js/components/DebugModal.js
+++ src/js/components/DebugModal.js
@@ -4,6 +4,7 @@ import React from "react"
 import Modal from "./Modal"
 import {SmallHeading} from "./Headings"
 import Prism from "prismjs"
+import * as Program from "../lib/Program"
 
 type Props = {
   isOpen: boolean,
@@ -12,8 +13,18 @@ type Props = {
   ast: ?Object
 }
 
-export default class DebugModal extends React.Component<Props> {
+type State = {
+  program: string
+}
+
+export default class DebugModal extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {program: props.searchProgram}
+  }
+
   render() {
+    const [ast, _error] = Program.parse(this.state.program)
     return (
       <Modal
         isOpen={this.props.isOpen}
@@ -21,14 +32,21 @@ export default class DebugModal extends React.Component<Props> {
         className="debug-query-modal"
       >
         <SmallHeading>Search Program</SmallHeading>
-        <pre>{this.props.searchProgram}</pre>
+
+        <input
+          className="debug-modal-input"
+          type="text"
+          value={this.state.program}
+          onChange={e => this.setState({program: e.currentTarget.value})}
+        />
+
         <SmallHeading>Abstract Syntax Tree</SmallHeading>
         <pre>
           <code
             className="language-js"
             dangerouslySetInnerHTML={{
               __html: Prism.highlight(
-                this.props.ast,
+                JSON.stringify(ast, null, 4),
                 Prism.languages.js,
                 "JSON"
               )
