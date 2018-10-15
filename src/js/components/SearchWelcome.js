25ee79876a0835439ee995d43db1274a2e45bbff
James Kerr <jkerr838@gmail.com>
2018-10-15 16:08:41 -0700

    Added body content styles
    These will be for long areas of text

--- src/js/components/SearchWelcome.js
+++ src/js/components/SearchWelcome.js
@@ -10,14 +10,24 @@ class SearchWelcome extends React.Component {
     if (!space) return null
 
     return (
-      <div className="search-welcome">
+      <div className="search-welcome body-content">
         <h1>Space: {space.name}</h1>
         <p>Type a search above to explore the bro logs.</p>
         <p>Example searches:</p>
-        <pre>method=POST</pre>
-        <pre>history=ShA*</pre>
-        <pre>192.32.142.32</pre>
-        <pre>* | count() by _path</pre>
+        <ul>
+          <li>
+            <code>method=POST</code>
+          </li>
+          <li>
+            <code>history=ShA*</code>
+          </li>
+          <li>
+            <code>192.32.142.32</code>
+          </li>
+          <li>
+            <code>* | count() by _path</code>
+          </li>
+        </ul>
       </div>
     )
   }
