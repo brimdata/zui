227207ddf80cc876240372f563d5a85de2de9917
James Kerr <jkerr838@gmail.com>
2018-10-23 09:17:39 -0700

    Program#hasAnalytics becomes more sophisticated
    Don't just check if a proc exists. Look to see which procs are
    being used. SortProcs and HeadProcs are not analytics procs.

--- src/js/lib/Program.js
+++ src/js/lib/Program.js
@@ -1,15 +1,26 @@
 /* @flow */
 
-import Ast from "../models/Ast"
 import {LookyTalk} from "boom-js-client"
 
 type Program = string
 
+const TUPLE_PROCS = ["HeadProc", "SortProc"]
+const COMPOUND_PROCS = ["ParallelProc", "SequentialProc"]
+
 export const hasAnalytics = (string: Program) => {
-  const ast = new Ast(string).toJSON()
-  if (!ast) return false
-  if (ast.proc) return true
-  else return false
+  const [ast] = parse(string)
+  if (!ast || !ast.proc) return false
+
+  const ops = COMPOUND_PROCS.includes(ast.proc.op)
+    ? ast.proc.procs.map(p => p.op)
+    : [ast.proc.op]
+
+  for (let op of ops) {
+    if (!TUPLE_PROCS.includes(op)) {
+      return true
+    }
+  }
+  return false
 }
 
 export const parse = (string: Program) => {
