117b1ed28bfe81e0401ee6896ff04ea54875d566
James Kerr <jkerr838@gmail.com>
2018-10-24 11:33:23 -0700

    Throttle the events as they come in

--- src/js/receivers/countByTimeReceiver.js
+++ src/js/receivers/countByTimeReceiver.js
@@ -1,7 +1,25 @@
 import {receiveCountByTime} from "../actions/countByTime"
+import throttle from "lodash/throttle"
 
-export default dispatch => ({type, results}) => {
-  if (type === "SearchResult") {
-    dispatch(receiveCountByTime(results))
+export default dispatch => {
+  let descriptor
+  let tuples = []
+
+  const throttledDispatch = throttle(
+    () => {
+      if (tuples.length === 0) return
+      dispatch(receiveCountByTime({descriptor, tuples}))
+      tuples = []
+    },
+    50,
+    {leading: false}
+  )
+
+  return ({type, results}) => {
+    if (type === "SearchResult") {
+      descriptor = results.descriptor
+      tuples = [...tuples, ...results.tuples]
+      throttledDispatch()
+    }
   }
 }
