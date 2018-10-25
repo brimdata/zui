9a77f9d4c0f3a09ce8a40249517ba689c304ee8f
James Kerr <jkerr838@gmail.com>
2018-10-25 16:10:19 -0700

    Add missing tooltip file
    It was in dist but not in src. Careful next time with that.

---
+++ src/js/components/Tooltip.js
@@ -0,0 +1,20 @@
+/* @flow */
+import React from "react"
+import ReactDOM from "react-dom"
+import * as Doc from "../lib/Doc"
+
+type Props = {
+  style: {top: number, left: number},
+  children: any
+}
+
+export default class Tooltip extends React.Component<Props> {
+  render() {
+    return ReactDOM.createPortal(
+      <div className="tool-tip" style={this.props.style}>
+        {this.props.children}
+      </div>,
+      Doc.id("tooltip-root")
+    )
+  }
+}
