9a1e2b113e6bf7743010c62c2792530b50477b28
James Kerr <jkerr838@gmail.com>
2018-11-02 09:14:14 -0700

    Remove successCountByTime
    Was not being used at all

--- src/js/actions/countByTime.js
+++ src/js/actions/countByTime.js
@@ -1,9 +1,5 @@
 /* @flow */
 
-export const completeCountByTime = () => ({
-  type: "COUNT_BY_TIME_COMPLETE"
-})
-
 export const receiveCountByTime = (data: Object) => ({
   type: "COUNT_BY_TIME_RECEIVE",
   data
