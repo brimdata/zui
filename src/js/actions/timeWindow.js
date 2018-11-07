772158568d55188351fe76c9d372ef9eb59424e8
James Kerr <jkerr838@gmail.com>
2018-11-07 14:34:11 -0800

    Added more time window actions

--- src/js/actions/timeWindow.js
+++ src/js/actions/timeWindow.js
@@ -7,3 +7,13 @@ export const setInnerTimeWindow = timeWindow => ({
   type: "INNER_TIME_WINDOW_SET",
   timeWindow
 })
+
+export const setOuterFromTime = date => ({
+  type: "OUTER_FROM_TIME_SET",
+  date
+})
+
+export const setOuterToTime = date => ({
+  type: "OUTER_TO_TIME_SET",
+  date
+})
--- src/js/reducers/timeWindow.js
+++ src/js/reducers/timeWindow.js
@@ -24,6 +24,14 @@ export default createReducer(initialState, {
   INNER_TIME_WINDOW_SET: (state, {timeWindow}) => ({
     ...state,
     inner: timeWindow
+  }),
+  OUTER_FROM_TIME_SET: (state, {date}) => ({
+    ...state,
+    outer: [date, state.outer[1]]
+  }),
+  OUTER_TO_TIME_SET: (state, {date}) => ({
+    ...state,
+    outer: [state.outer[0], date]
   })
 })
 
