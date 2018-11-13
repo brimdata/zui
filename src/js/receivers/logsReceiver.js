88ce84eb5bc35bf14820dba65a76fe0d86ef49fe
James Kerr <jkerr838@gmail.com>
2018-11-13 09:53:32 -0800

    Keep track of if there is more events ahead after receiving some

--- src/js/receivers/logsReceiver.js
+++ src/js/receivers/logsReceiver.js
@@ -1,20 +1,28 @@
 /* @flow */
 
 import * as actions from "../actions/mainSearch"
+import * as logViewer from "../actions/logViewer"
 import {discoverDescriptors} from "../actions/descriptors"
 import throttle from "lodash/throttle"
 import type {Tuple, Descriptor} from "../models/Log"
 
-type Payload = {
-  type: string,
+type SearchResult = {
+  type: "SearchResult",
   results: {
     tuples: Tuple[],
     descriptor: Descriptor
   }
 }
 
-export default function(dispatch: Function) {
+type SearchEnd = {
+  type: "SearchEnd"
+}
+
+type Payload = SearchResult | SearchEnd
+
+export default function(dispatch: Function, expectedCount: number) {
   let buffer = []
+  let count = 0
 
   const dispatchEvents = throttle(() => {
     if (buffer.length === 0) return
@@ -27,9 +35,14 @@ export default function(dispatch: Function) {
     if (payload.type === "SearchResult") {
       const tuples = payload.results.tuples
       if (tuples.length) {
+        count += tuples.length
         buffer = [...buffer, ...tuples]
         dispatchEvents()
       }
+    }
+
+    if (payload.type === "SearchEnd") {
+      dispatch(logViewer.setMoreAhead(count >= expectedCount))
     }
   }
 }
