7d1a8e65ed69f05d526a7073a80112af1a046e34
James Kerr <jkerr838@gmail.com>
2018-11-14 13:22:26 -0800

    New action for fetching next page of logss Create action for fetching next page of logs#

--- src/js/actions/logViewer.js
+++ src/js/actions/logViewer.js
@@ -1,5 +1,16 @@
 /* @flow */
 
+import type {Thunk} from "redux-thunk"
+import * as Arr from "../lib/Array"
+import * as Time from "../lib/Time"
+import * as mainSearch from "../reducers/mainSearch"
+import {getTimeWindow} from "../reducers/timeWindow"
+import * as spaces from "../reducers/spaces"
+import * as searchBar from "../reducers/searchBar"
+import * as Program from "../lib/Program"
+import logsReceiver from "../receivers/logsReceiver"
+import pageReceiver from "../receivers/pageReceiver"
+
 export const setMoreBehind = (value: boolean) => ({
   type: "LOG_VIEWER_MORE_BEHIND_SET",
   value
@@ -19,3 +30,27 @@ export const setIsFetchingAhead = (value: boolean) => ({
   type: "LOG_VIEWER_IS_FETCHING_AHEAD_SET",
   value
 })
+
+export const fetchAhead = (): Thunk => (dispatch, getState, api) => {
+  const PER_PAGE = 1000
+  dispatch(setIsFetchingAhead(true))
+  const state = getState()
+  const logs = mainSearch.getLogs(state)
+  let timeWindow = getTimeWindow(state)
+  let spliceIndex = 0
+  if (!Arr.isEmpty(logs)) {
+    const index = Arr.indexOfLastChange(logs, log => log.get("ts"))
+    const from = Time.add(logs[index].getField("ts").toDate(), 1, "ms")
+    const [_, to] = getTimeWindow(state)
+    timeWindow = [from, to]
+    spliceIndex = index + 1
+  }
+  const space = spaces.getCurrentSpaceName(state)
+  const program = searchBar.getPrevSearchProgram(state)
+  const string = Program.addHeadProc(program, PER_PAGE)
+  api
+    .search({string, timeWindow, space})
+    .channel(0, pageReceiver(dispatch, PER_PAGE, spliceIndex))
+    .channel(0, logsReceiver(dispatch))
+    .done(() => dispatch(setIsFetchingAhead(false)))
+}
