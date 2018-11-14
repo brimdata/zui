366959b52fe50bc69da24c5e5ca874ef739f8601
James Kerr <jkerr838@gmail.com>
2018-11-14 13:21:36 -0800

    Splice the events when receiving a new page

--- src/js/receivers/pageReceiver.js
+++ src/js/receivers/pageReceiver.js
@@ -1,14 +1,22 @@
 /* @flow */
 
 import * as logViewer from "../actions/logViewer"
+import * as mainSearch from "../actions/mainSearch"
 import type {Payload} from "./types"
 
-export default (dispatch: Function, perPage: number) => {
+export default (
+  dispatch: Function,
+  perPage: number,
+  spliceIndex: number = 0
+) => {
   let count = 0
 
   return (payload: Payload) => {
     switch (payload.type) {
       case "SearchResult":
+        if (count === 0) {
+          dispatch(mainSearch.spliceMainSearchEvents(spliceIndex))
+        }
         count += payload.results.tuples.length
         break
       case "SearchEnd":
