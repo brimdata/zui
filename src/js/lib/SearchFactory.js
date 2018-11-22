32536e73825e3f1883dfaf455810e2665183e49f
James Kerr <jkerr838@gmail.com>
2018-11-21 22:45:09 -0800

    Use the column receiver

--- src/js/lib/SearchFactory.js
+++ src/js/lib/SearchFactory.js
@@ -11,6 +11,7 @@ import Search from "../models/Search"
 import {getCurrentSpaceName} from "../reducers/spaces"
 import type {State, Dispatch, Api} from "../reducers/types"
 import pageReceiver from "../receivers/pageReceiver"
+import columnsReceiver from "../receivers/columnsReceiver"
 
 export const create = (dispatch: Dispatch, state: State, api: Api) => {
   return new Search(dispatch, api, getArgs(dispatch, state))
@@ -70,7 +71,6 @@ export const logsHeadArgs = (dispatch: Dispatch, state: State) => {
     Program.addHeadProc(getSearchProgram(state), 1000) +
     "; " +
     getCountByTimeProc(state)
-
   return {
     space: getCurrentSpaceName(state),
     program,
@@ -95,6 +95,7 @@ export const logsPagedArgs = (dispatch: Dispatch, state: State) => {
     timeWindow: getTimeWindow(state),
     callbacks: (request: *) =>
       request
+        .channel(1, columnsReceiver(dispatch, state))
         .channel(1, pageReceiver(dispatch, PER_PAGE))
         .channel(1, logsReceiver(dispatch))
         .channel(0, countByTimeReceiver(dispatch))
