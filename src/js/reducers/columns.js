43843bc7e80fe4ae42b1a13d74375527788c7952
James Kerr <jkerr838@gmail.com>
2018-11-27 09:24:04 -0800

    Get columns from the tds

--- src/js/reducers/columns.js
+++ src/js/reducers/columns.js
@@ -2,7 +2,11 @@ import createReducer from "./createReducer"
 import {createSelector} from "reselect"
 import * as columnWidths from "./columnWidths"
 import ColumnWidths from "../components/Viewer/ColumnWidths"
-
+import * as mainSearch from "./mainSearch"
+import * as descriptors from "./descriptors"
+import * as spaces from "./spaces"
+import UniqArray from "../models/UniqArray"
+import isEqual from "lodash/isEqual"
 const initialState = []
 
 export default createReducer(initialState, {
@@ -22,5 +26,22 @@ export const getWidths = createSelector(
     if (columns.length > 0) {
       return new ColumnWidths(columns.map(c => c.name), columnWidths)
     }
+  }
+)
+
+export const getColumnsFromTds = createSelector(
+  mainSearch.getMainSearchEvents,
+  descriptors.getDescriptors,
+  spaces.getCurrentSpaceName,
+  (tuples, descriptors, space) => {
+    const tds = new UniqArray()
+    tuples.forEach(([td]) => tds.push(td))
+    const columns = new UniqArray(isEqual)
+    tds.toArray().forEach(td => {
+      const desc = descriptors[space + "." + td]
+      if (desc) desc.forEach(d => columns.push(d))
+    })
+
+    return columns.toArray()
   }
 )
