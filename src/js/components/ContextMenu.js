01f2a6d33bda06e241c3939c058966fa65ef76d2
James Kerr <jkerr838@gmail.com>
2018-11-28 13:15:41 -0800

    Add className props to context menu Components

--- src/js/components/ContextMenu.js
+++ src/js/components/ContextMenu.js
@@ -4,10 +4,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 import * as Doc from "../lib/Doc"
 import type {FixedPos} from "../lib/Doc"
+import classNames from "classnames"
 
 type Props = {
   children: any,
   style: FixedPos,
+  className?: string,
   onOutsideClick: Function
 }
 
@@ -46,7 +48,7 @@ export class ContextMenu extends React.Component<Props, State> {
     return ReactDOM.createPortal(
       <div className="context-menu-overlay" onClick={this.props.onOutsideClick}>
         <ul
-          className="context-menu"
+          className={classNames("context-menu", this.props.className)}
           ref={r => (this.ref = r)}
           style={{...this.state}}
         >
@@ -58,7 +60,9 @@ export class ContextMenu extends React.Component<Props, State> {
   }
 }
 
-type ItemProps = {onClick: Function, children: any}
-export const MenuItem = ({onClick, children}: ItemProps) => (
-  <li onClick={onClick}>{children}</li>
+type ItemProps = {onClick: Function, children: any, className?: string}
+export const MenuItem = ({onClick, children, className}: ItemProps) => (
+  <li onClick={onClick} className={className}>
+    {children}
+  </li>
 )
