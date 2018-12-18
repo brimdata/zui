fdf932c7477a9805b8defc0569e04bc6861abae1
James Kerr <jkerr838@gmail.com>
2018-12-18 14:47:34 -0800

    Moved connector to the bottom of the component

--- src/js/components/Search.js
+++ src/js/components/Search.js
@@ -81,3 +81,32 @@ export default class Search extends React.Component<Props, State> {
     )
   }
 }
+
+import {connect} from "react-redux"
+import {bindActionCreators} from "redux"
+import * as spaceActions from "../actions/spaces"
+import * as viewActions from "../actions/view"
+import * as spaces from "../reducers/spaces"
+import * as boomdConnection from "../reducers/boomdConnection"
+import * as view from "../reducers/view"
+import * as initialLoad from "../reducers/initialLoad"
+
+const stateToProps = state => ({
+  initialLoad: initialLoad.getInitialLoad(state),
+  isConnected: boomdConnection.getBoomdIsConnected(state),
+  currentSpaceName: spaces.getCurrentSpaceName(state),
+  logsTab: view.getShowLogsTab(state),
+  analyticsTab: view.getShowAnalyticsTab(state),
+  space: spaces.getCurrentSpace(state)
+})
+
+export const XSearch = connect(
+  stateToProps,
+  (dispatch: Function) => ({
+    ...bindActionCreators(
+      {...spaceActions, ...viewActions, dispatch},
+      dispatch
+    ),
+    dispatch
+  })
+)(Search)
