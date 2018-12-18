525e6d086bb61b3dfe8a53c7194ff6b62818f290
James Kerr <jkerr838@gmail.com>
2018-12-18 10:36:11 -0800

    Tooltip upgrades

--- src/js/charts/XPositionTooltip.js
+++ src/js/charts/XPositionTooltip.js
@@ -5,60 +5,81 @@ import Chart from "../models/Chart"
 import * as d3 from "d3"
 import * as Doc from "../lib/Doc"
 import Component from "../components/HistogramTooltip"
+import * as Time from "../lib/Time"
 
 export default class HistogramTooltip {
   mount(chart: Chart) {
     const tooltip = Doc.id("histogram-tooltip")
-    let prevDataIndex = null
+    let prevPoint = null
+
+    const hide = () => {
+      tooltip.style.opacity = "0"
+      prevPoint = null
+    }
+
+    const show = function() {
+      const [left] = d3.mouse(this)
+      const point = getPointAt(left, chart)
+      if (point && point.count) {
+        positionTooltip(tooltip, this, 30)
+        if (prevPoint === point) return
+        prevPoint = point
+        tooltip.innerHTML = renderToString(
+          <Component {...tooltipProps(point)} />
+        )
+      } else {
+        hide()
+      }
+    }
 
     d3.select(chart.svg)
       .select(".brush")
-      .on("mousedown", () => {
-        tooltip.style.opacity = "0"
-      })
-      .on("mouseout", () => {
-        tooltip.style.opacity = "0"
-        prevDataIndex = null
-      })
-      .on("mousemove", function() {
-        const [left] = d3.mouse(this)
-        const ts = chart.scales.timeScale.invert(left)
-        const bisect = d3.bisector(d => d.ts).left
-        const index = bisect(chart.data.data, ts) - 1
-
-        const point = chart.data.data[index]
-        const segments = []
-        for (let key in point) {
-          if (["ts", "count"].includes(key)) continue
-          if (point[key] !== 0) segments.push([key, point[key]])
-        }
-
-        if (!segments.length) return
-
-        requestAnimationFrame(() => {
-          if (index / chart.data.data.length > 0.65) {
-            tooltip.style.right = -1 * left + 30 + "px"
-            tooltip.style.left = ""
-          } else {
-            tooltip.style.right = ""
-            tooltip.style.left = left + 30 + "px"
-          }
-          tooltip.style.display = "block"
-          tooltip.style.opacity = "1"
-        })
-
-        if (prevDataIndex === index) return
-
-        prevDataIndex = index
-        tooltip.innerHTML = renderToString(
-          <Component
-            style={{left}}
-            segments={segments}
-            ts={chart.data.data[index].ts}
-          />
-        )
-      })
+      .on("mousedown.tooltip", hide)
+      .on("mouseout.tooltip", hide)
+      .on("mousemove.tooltip", show)
   }
 
   draw(_chart: Chart) {}
+}
+
+const getPointAt = (left, chart) => {
+  const ts = chart.scales.timeScale.invert(left)
+  const {number, unit} = chart.data.interval
+
+  for (let index = 0; index < chart.data.data.length; index++) {
+    const point = chart.data.data[index]
+    const nextTs = Time.add(point.ts, number, unit)
+
+    if (ts >= point.ts && ts < nextTs) return point
+  }
+
+  return null
+}
+
+const tooltipProps = point => {
+  const segments = []
+  for (let key in point) {
+    if (["ts", "count"].includes(key)) continue
+    if (point[key] !== 0) segments.push([key, point[key]])
+  }
+
+  return {ts: point.ts, segments}
+}
+
+const positionTooltip = (tooltip, parent, padding) => {
+  const [left] = d3.mouse(parent)
+  const {width: tooltipWidth} = tooltip.getBoundingClientRect()
+  const {width: parentWidth} = parent.getBoundingClientRect()
+
+  if (left + tooltipWidth + padding >= parentWidth) {
+    d3.select(tooltip)
+      .style("right", left * -1 + padding + "px")
+      .style("left", "")
+      .style("opacity", "1")
+  } else {
+    d3.select(tooltip)
+      .style("left", left + padding + "px")
+      .style("right", "")
+      .style("opacity", "1")
+  }
 }
