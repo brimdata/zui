0bbd2ae16af782f740ebc82a99095e4dc2c70a00
James Kerr <jkerr838@gmail.com>
2019-01-02 11:31:31 -0800

    Remove RightPane connector

--- src/js/components/RightPane.js
+++ src/js/components/RightPane.js
@@ -107,3 +107,32 @@ export default class RightPane extends React.Component {
     )
   }
 }
+
+import {connect} from "react-redux"
+import {bindActionCreators} from "redux"
+import * as viewActions from "../actions/view"
+import * as starActions from "../actions/starredLogs"
+import * as detailActions from "../actions/logDetails"
+import * as packetActions from "../actions/packets"
+import * as view from "../reducers/view"
+import * as logDetails from "../reducers/logDetails"
+import * as spaces from "../reducers/spaces"
+
+const stateToProps = state => ({
+  isOpen: view.getRightSidebarIsOpen(state) && logDetails.buildLogDetail(state),
+  width: view.getRightSidebarWidth(state),
+  prevExists: logDetails.getPrevExists(state),
+  nextExists: logDetails.getNextExists(state),
+  isStarred: logDetails.getLogDetailIsStarred(state),
+  currentLog: logDetails.buildLogDetail(state),
+  space: spaces.getCurrentSpace(state)
+})
+
+export const XRightPane = connect(
+  stateToProps,
+  dispatch =>
+    bindActionCreators(
+      {...viewActions, ...starActions, ...detailActions, ...packetActions},
+      dispatch
+    )
+)(RightPane)
