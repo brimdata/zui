0047e535f597eb507e6d32780cc3fb1ab42a9bc4
James Kerr <jkerr838@gmail.com>
2019-01-07 13:03:06 -0800

    Straiten out update rules
    If the rows are different, the columns, or the data in the the rows

--- src/js/components/Viewer/Chunk.js
+++ src/js/components/Viewer/Chunk.js
@@ -4,19 +4,31 @@ import React from "react"
 import type {Layout} from "./Layout"
 import type {RowRenderer} from "./types"
 import isEqual from "lodash/isEqual"
+import Log from "../../models/Log"
 
 type Props = {
   rowRenderer: RowRenderer,
   layout: Layout,
-  rows: number[]
+  rows: number[],
+  logs: Log[]
 }
 
 export default class Chunk extends React.Component<Props> {
   shouldComponentUpdate(nextProps: Props) {
-    return (
-      !this.props.layout.isEqual(nextProps.layout) ||
-      !isEqual(this.props.rows, nextProps.rows)
-    )
+    if (!isEqual(this.props.rows, nextProps.rows)) {
+      return true
+    }
+
+    if (!isEqual(this.props.layout.columns, nextProps.layout.columns)) {
+      return true
+    }
+
+    for (let index of this.props.rows) {
+      if (!Log.isSame(this.props.logs[index], nextProps.logs[index]))
+        return true
+    }
+
+    return false
   }
 
   render() {
