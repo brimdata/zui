246818031448654da954b30a0ad880ee21563438
James Kerr <jkerr838@gmail.com>
2019-01-16 11:40:47 -0800

    feat: Add clear starred logs action

--- src/js/reducers/starredLogs.js
+++ src/js/reducers/starredLogs.js
@@ -6,6 +6,7 @@ import * as Tuple from "../lib/Tuple"
 const initialState = []
 
 export default createReducer(initialState, {
+  STARRED_LOGS_CLEAR: () => initialState,
   LOG_STAR: (state, {tuple}) => [...state, tuple],
   LOG_UNSTAR: (state, {tuple}) => Tuple.removeFrom(state, tuple)
 })
