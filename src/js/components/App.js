db26808dfb1d680cc36905706b181f4c91142420
James Kerr <jkerr838@gmail.com>
2019-01-25 16:08:35 -0800

    feat: Add wrapper around the app for global elements that need to appear on every page

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -17,12 +17,28 @@ class App extends Component<Props> {
     const {isConnected, timeZone} = this.props
     Time.setZone(timeZone)
     return (
-      <Switch>
-        {isConnected && <Route path="/search" component={XSearch} />}
-        {isConnected && <Route path="/spaces" component={XSpaces} />}
-        <Route path="/connect" component={XConnect} />
-        <Redirect to="/connect" />
-      </Switch>
+      <div className="app-wrapper">
+        {/* <MessageBox
+          title="Lookytalk Version Error"
+          style={{right: 12, bottom: 12}}
+        >
+          <Paragraph>
+            The server and client lookytalk versions do not match.
+          </Paragraph>
+          <Code>
+            Server: 1.2.0
+            <br />
+            Client: 1.2.3
+          </Code>
+        </MessageBox> */}
+
+        <Switch>
+          {isConnected && <Route path="/search" component={XSearch} />}
+          {isConnected && <Route path="/spaces" component={XSpaces} />}
+          <Route path="/connect" component={XConnect} />
+          <Redirect to="/connect" />
+        </Switch>
+      </div>
     )
   }
 }
