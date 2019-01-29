e8e438001494fe4d8ceb1df1ca84498f1d0dcd27
James Kerr <jkerr838@gmail.com>
2019-01-29 15:09:27 -0800

    feat: Add more Typography components with styles

--- src/js/components/Typography.js
+++ src/js/components/Typography.js
@@ -3,7 +3,10 @@ import * as React from "react"
 import classNames from "classnames"
 
 type Props = {
-  children?: React.Node
+  children?: React.Node,
+  bold?: boolean,
+  underline?: boolean,
+  className?: string
 }
 
 export const Header = ({children, ...props}: Props) => (
@@ -12,8 +15,8 @@ export const Header = ({children, ...props}: Props) => (
   </h2>
 )
 
-export const Paragraph = ({children, ...props}: Props) => (
-  <h2 {...props} className="paragraph">
+export const Paragraph = ({children, bold, underline, ...props}: Props) => (
+  <h2 {...props} className={classNames("paragraph", {bold, underline})}>
     {children}
   </h2>
 )
@@ -31,4 +34,22 @@ export const Code = ({children, ...props}: CodeProps) => (
   >
     {children}
   </pre>
+)
+
+export const Fieldset = ({children, ...props}: Props) => (
+  <p className="fieldset" {...props}>
+    {children}
+  </p>
+)
+
+export const Subscript = ({children, className, ...props}: Props) => (
+  <sup className={classNames("subscript", className)} {...props}>
+    {children}
+  </sup>
+)
+
+export const Label = ({children, ...props}: Props) => (
+  <label className="label" {...props}>
+    {children}
+  </label>
 )
