0c4bffc521c86439818a7c591adc42b575cb01c9
James Kerr <jkerr838@gmail.com>
2019-02-04 13:42:08 -0800

    feat: Add whois lookup to analytic right click

--- src/js/lib/buildAnalyticRightClickMenu.js
+++ src/js/lib/buildAnalyticRightClickMenu.js
@@ -1,5 +1,14 @@
 import * as actions from "../actions/rightClick"
 
-export default ({log, dispatch}) => {
-  return [actions.analyticDetail(log, dispatch)]
+export default ({log, field, dispatch}) => {
+  const menu = []
+
+  if (["addr", "set[addr]"].includes(field.type)) {
+    menu.push(actions.whois(field, dispatch))
+    menu.push(actions.seperator())
+  }
+
+  menu.push(actions.analyticDetail(log, dispatch))
+
+  return menu
 }
