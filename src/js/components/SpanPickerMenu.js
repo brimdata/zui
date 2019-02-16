88def01e5fc8e35ebc21833a0ed0de61df7f58ae
James Kerr <jkerr838@gmail.com>
2019-02-15 17:23:41 -0800

    fix: Update local span before global span

--- src/js/components/SpanPickerMenu.js
+++ src/js/components/SpanPickerMenu.js
@@ -1,13 +1,14 @@
 /* @flow */
 
-import React from "react"
 import {connect} from "react-redux"
-import * as TimeWindow from "../lib/TimeWindow"
-import {setOuterTimeWindow} from "../actions/timeWindow"
-import type {DateTuple} from "../lib/TimeWindow"
-import MenuList from "./MenuList"
-import {getCurrentSpaceTimeWindow} from "../reducers/spaces"
+import React from "react"
+
+import {type DateTuple, last} from "../lib/TimeWindow"
 import {type DispatchProps} from "../reducers/types"
+import {getCurrentSpaceTimeWindow} from "../reducers/spaces"
+import {setOuterTimeWindow} from "../actions/timeWindow"
+import {submitSearchBar} from "../actions/searchBar"
+import MenuList from "./MenuList"
 import dispatchToProps from "../lib/dispatchToProps"
 
 type StateProps = {|
@@ -22,27 +23,20 @@ type Props = {|
 export default class SpanPickerMenu extends React.Component<Props> {
   setSpan(span: DateTuple) {
     this.props.dispatch(setOuterTimeWindow(span))
+    this.props.dispatch(submitSearchBar())
   }
 
   render() {
     return (
       <MenuList>
         <li onClick={() => this.setSpan(this.props.spaceSpan)}>Whole Space</li>
-        <li onClick={() => this.setSpan(TimeWindow.last(30, "minutes"))}>
+        <li onClick={() => this.setSpan(last(30, "minutes"))}>
           Last 30 minutes
         </li>
-        <li onClick={() => this.setSpan(TimeWindow.last(24, "hours"))}>
-          Last 24 hours
-        </li>
-        <li onClick={() => this.setSpan(TimeWindow.last(7, "days"))}>
-          Last 7 days
-        </li>
-        <li onClick={() => this.setSpan(TimeWindow.last(30, "days"))}>
-          Last 30 days
-        </li>
-        <li onClick={() => this.setSpan(TimeWindow.last(90, "days"))}>
-          Last 90 days
-        </li>
+        <li onClick={() => this.setSpan(last(24, "hours"))}>Last 24 hours</li>
+        <li onClick={() => this.setSpan(last(7, "days"))}>Last 7 days</li>
+        <li onClick={() => this.setSpan(last(30, "days"))}>Last 30 days</li>
+        <li onClick={() => this.setSpan(last(90, "days"))}>Last 90 days</li>
       </MenuList>
     )
   }
