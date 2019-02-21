438d0391348dc555f1c45b9fb620273b81ae892a
James Kerr <jkerr838@gmail.com>
2019-02-21 09:49:05 -0800

    feat: Add use cache and index to state

--- src/js/actions/boomd.js
+++ src/js/actions/boomd.js
@@ -6,6 +6,16 @@ import type {Thunk} from "../reducers/types"
 import {addNotification} from "./notifications"
 import {LookytalkVersionError} from "../models/Errors"
 
+export const useBoomCache = (value: boolean) => ({
+  type: "BOOMD_CACHE_USE_SET",
+  value
+})
+
+export const useBoomIndex = (value: boolean) => ({
+  type: "BOOMD_INDEX_USE_SET",
+  value
+})
+
 export const setBoomdCredentials = (credentials: Credentials) => ({
   type: "BOOMD_CREDENTIALS_SET",
   credentials
--- src/js/reducers/boomd.js
+++ src/js/reducers/boomd.js
@@ -7,16 +7,34 @@ const initialState = {
   host: "",
   port: "",
   user: "",
-  pass: ""
+  pass: "",
+  useCache: true,
+  useIndex: true
 }
 
 export type Boomd = typeof initialState
 
 export default createReducer(initialState, {
-  BOOMD_CREDENTIALS_SET: (state, {credentials}) => credentials
+  BOOMD_CREDENTIALS_SET: (state, {credentials}) => ({
+    ...state,
+    ...credentials
+  }),
+  BOOMD_CACHE_USE_SET: (state, {value}) => ({
+    ...state,
+    useCache: value
+  }),
+  BOOMD_INDEX_USE_SET: (state, {value}) => ({
+    ...state,
+    useIndex: value
+  })
 })
 
-export const getCredentials = (state: State) => state.boomd
+export const getCredentials = (state: State) => {
+  const {host, port, user, pass} = state.boomd
+  return {host, port, user, pass}
+}
 export const getBoomHost = (state: State) => state.boomd.host
 export const getBoomPort = (state: State) => state.boomd.port
 export const getBoomUser = (state: State) => state.boomd.user
+export const getUseBoomCache = (state: State) => state.boomd.useCache
+export const getUseBoomIndex = (state: State) => state.boomd.useIndex
