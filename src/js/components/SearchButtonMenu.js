37b3571b29f793365b648d7520856e080f124e16
James Kerr <jkerr838@gmail.com>
2019-02-21 10:05:09 -0800

    feat: Remove 'coming soon' options
    I think they're tacky

--- src/js/components/SearchButtonMenu.js
+++ src/js/components/SearchButtonMenu.js
@@ -12,15 +12,12 @@ export default class SearchButtonMenu extends React.Component<Props> {
   render() {
     return (
       <MenuList>
-        <li onClick={() => {}}>Save query (coming soon)</li>
-        <li onClick={() => {}}>Load query (coming soon)</li>
         <li onClick={() => this.props.dispatch(showModal("debug"))}>
           Debug query
         </li>
         <li onClick={() => this.props.dispatch(showModal("curl"))}>
           Copy for curl
         </li>
-        <li onClick={() => {}}>Copy for CLI (coming soon)</li>
       </MenuList>
     )
   }
