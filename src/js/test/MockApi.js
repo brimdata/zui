410b0b490bd72d5f45dfd9196976a75d9fb2bf4d
James Kerr <jkerr838@gmail.com>
2019-02-27 09:48:08 -0800

    refactor: Removed old MockClient

--- src/js/test/MockApi.js
+++ src/js/test/MockApi.js
@@ -1,23 +1,6 @@
 /* @flow */
 
-import Client, {BoomClient, Handler} from "boom-js-client"
-
-export default class MockApi extends Client {
-  constructor(obj: Object = {}) {
-    super()
-    Object.assign(this, obj)
-    this.send = jest.fn(() => new Handler(() => {}))
-  }
-
-  stub(method: string, returnVal: *) {
-    const handler = new Handler(() => {})
-    this[method] = () => {
-      setTimeout(() => handler.onDone(returnVal))
-      return handler
-    }
-    return this
-  }
-}
+import {BoomClient, Handler} from "boom-js-client"
 
 export function MockBoomClient() {
   BoomClient.call(this)
