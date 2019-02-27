af67c778b9e69ce590cd5878386b6bf51935b7b7
James Kerr <jkerr838@gmail.com>
2019-02-27 09:31:26 -0800

    refactor: Removed unused methods

--- src/js/actions/spaces.js
+++ src/js/actions/spaces.js
@@ -1,42 +1,11 @@
 /* @flow */
 
-import Client from "boom-js-client"
-
 import type {Space} from "../lib/Space"
-
-export function fetchSpaceInfo(name: string) {
-  return (dispatch: *, getState: *, api: Client) => {
-    return api.space({name}).done(space => {
-      dispatch(setSpaceInfo(space))
-    })
-  }
-}
-
-export const fetchAndSetCurrentSpace = (name: string) => {
-  return (dispatch: Function) => {
-    return dispatch(fetchSpaceInfo(name)).done(() => {
-      dispatch(setCurrentSpaceName(name))
-    })
-  }
-}
-
-export const requestAllSpaces = () => {
-  return {
-    type: "ALL_SPACES_REQUEST"
-  }
-}
 
 export function setSpaceInfo(spaceInfo: $Shape<Space>) {
   return {
     type: "SPACE_INFO_SET",
     spaceInfo
-  }
-}
-
-export function requestSpaceInfo(name: string) {
-  return {
-    type: "SPACE_INFO_REQUEST",
-    name
   }
 }
 
