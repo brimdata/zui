18aea2f8471ae1e97c3ab7258d707ad493a8aa31
Michael Brown <mikeb@looky.cloud>
2019-02-27 17:37:55 -0800

    test: add test that logs in and waits for a few elements
    The test demonstrates filling out login and waiting for a few key
    elements to appear. The test attempts to start some better notions of
    abstraction for common tasks. Note the test is disabled here: it won't
    work without a boomd with a space.

--- itest/poc.test.js
+++ itest/poc.test.js
@@ -11,12 +11,43 @@ const Application = require("spectron").Application
 const electronPath = require("electron") // Require Electron from the binaries included in node_modules.
 const path = require("path")
 
+const retry = (f, attempts = 100, delay = 100) => {
+  return new Promise((resolve, reject) => {
+    f()
+      .then(ret => resolve(ret))
+      .catch(err => {
+        setTimeout(() => {
+          if (attempts === 1) {
+            reject(err)
+          } else {
+            retry(f, attempts - 1, delay)
+              .then(ret => resolve(ret))
+              .catch(err => reject(err))
+          }
+        }, delay)
+      })
+  })
+}
+
+const logIn = app => {
+  return app.client
+    .setValue("[name=host]", "localhost")
+    .setValue("[name=port]", "9867")
+    .click("button")
+}
+
+const waitForSearch = app => {
+  return retry(() => app.client.element("#main-search-input").getValue())
+}
+
+const waitForHistogram = app => {
+  return retry(() => app.client.element(".count-by-time").getAttribute("class"))
+}
+
 describe("Application launch", () => {
   let app
   beforeEach(() => {
-    // TODOs:
-    // 1. Move this logic into a library, especially as it expands.
-    // 2. "Reset State" will surely be necessary.
+    // TODO: Move this logic into a library, especially as it expands.
     app = new Application({
       path: electronPath,
       args: [path.join(__dirname, "..")]
@@ -54,6 +85,19 @@ describe("Application launch", () => {
       .getText("//header[@class='looky-header']/h1")
       .then(headerText => {
         expect(headerText).toBe("LOOKY")
+        done()
+      })
+      .catch(done)
+  })
+
+  // Skipped until PROD-308 and PROD-309 are done. If you have an auth-less
+  // boomd running with a space created, you can uncomment and run this test.
+  test.skip("log in and see Search and Histogram", done => {
+    logIn(app)
+      .then(() => waitForHistogram(app))
+      .then(() => waitForSearch(app))
+      .then(val => {
+        expect(val).toBeDefined()
         done()
       })
       .catch(done)
