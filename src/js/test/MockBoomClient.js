3e0ec2d6f4ea441916845b0c4b87762f538edb56
James Kerr <jkerr838@gmail.com>
2019-03-04 10:13:20 -0800

    refactor: Update MockBoomClient to inherit from BoomClient

--- src/js/test/MockBoomClient.js
+++ src/js/test/MockBoomClient.js
@@ -1,79 +1,53 @@
 /* @flow */
 
-import {BoomClient, Handler} from "boom-js-client"
+import BoomClient, {Handler} from "../BoomClient"
 
-export default function MockBoomClient() {
-  BoomClient.call(this)
-  return this
-}
+export default class MockBoomClient extends BoomClient {
+  send() {
+    throw new Error(`
+  Http Requests are disabled in tests.
+  Use #stubStream(method, val) or #stubSend(method, val)
+  to mock the server response.`)
+  }
 
-MockBoomClient.prototype = Object.create(BoomClient.prototype)
-MockBoomClient.prototype.constructor = MockBoomClient
+  stream() {
+    throw new Error(`
+  Http Requests are disabled in tests.
+  Use #stubStream(method, val) or #stubSend(method, val)
+  to mock the server response.`)
+  }
 
-MockBoomClient.prototype.stubPromise = function(method: string, returnVal: *) {
-  const stub = () => new Promise(r => r(returnVal))
-  const props = method.split(".")
+  stubSend(method: string, returnVal: *) {
+    return this.stub(method, () => new Promise(r => r(returnVal)))
+  }
 
-  let parent = this
-  for (let i = 0; i < props.length; i++) {
-    if (!parent[props[i]])
-      throw new Error(`${method} does not exist on BoomClient`)
+  stubSendError(method: string, error: *) {
+    return this.stub(method, () => new Promise((_, r) => r(error)))
+  }
 
-    if (i < props.length - 1) {
-      parent = parent[props[i]]
-    } else {
-      parent[props[i]] = stub
+  stubStream(method: string, returnVal: *) {
+    const handler = new Handler(() => {})
+    return this.stub(method, () => {
+      setTimeout(() => handler.onDone(returnVal), 0)
+      return handler
+    })
+  }
+
+  stub(method: string, methodBody: Function) {
+    const props = method.split(".")
+    let parent = this
+    for (let i = 0; i < props.length; i++) {
+      // $FlowFixMe
+      if (!parent[props[i]])
+        throw new Error(`${method} does not exist on BoomClient`)
+
+      if (i < props.length - 1) {
+        parent = parent[props[i]]
+      } else {
+        // $FlowFixMe
+        parent[props[i]] = methodBody
+      }
     }
+    return this
   }
-
-  return this
-}
-
-MockBoomClient.prototype.stubPromiseError = function(method: string, error: *) {
-  const stub = () => new Promise((_, r) => r(error))
-
-  const props = method.split(".")
-  let parent = this
-  for (let i = 0; i < props.length; i++) {
-    if (!parent[props[i]])
-      throw new Error(`${method} does not exist on BoomClient`)
-
-    if (i < props.length - 1) {
-      parent = parent[props[i]]
-    } else {
-      parent[props[i]] = stub
-    }
-  }
-
-  return this
-}
-
-MockBoomClient.prototype.stubStream = function(method: string, returnVal: *) {
-  const handler = new Handler(() => {})
-  const stub = () => {
-    setTimeout(() => handler.onDone(returnVal), 0)
-    return handler
-  }
-
-  const props = method.split(".")
-  let parent = this
-  for (let i = 0; i < props.length; i++) {
-    if (!parent[props[i]])
-      throw new Error(`${method} does not exist on BoomClient`)
-
-    if (i < props.length - 1) {
-      parent = parent[props[i]]
-    } else {
-      parent[props[i]] = stub
-    }
-  }
-
-  return this
-}
-
-MockBoomClient.prototype.send = () => {
-  throw new Error(`
-Http Requests are disabled in tests.
-Use #stubStream(method, val) or #stubPromise(method, val)
-to mock the server response.`)
 }
