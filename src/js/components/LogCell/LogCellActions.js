fc6324b39138ef4537609ccdb13d6dd79f2ebfb9
James Kerr <jkerr838@gmail.com>
2019-03-15 15:18:40 -0700

    refactor: RightClickMenu
    Use this component for rendering and another for data

--- src/js/components/LogCell/LogCellActions.js
+++ src/js/components/LogCell/LogCellActions.js
@@ -1,21 +1,14 @@
 /* @flow */
 
 import {connect} from "react-redux"
-import Measure from "react-measure"
 import React from "react"
 
-import type {DispatchProps, State} from "../../reducers/types"
 import type {MenuItemData} from "./rightClick"
-import {type ResultsTabEnum, getResultsTab} from "../../reducers/view"
-import type {Space} from "../../lib/Space"
+import type {State} from "../../reducers/types"
 import Field from "../../models/Field"
 import Log from "../../models/Log"
-import MenuList from "../MenuList"
-import * as MenuStyler from "../../lib/MenuStyler"
-import Portal from "../Portal"
+import RightClickMenu from "../RightClickMenu"
 import buildMenu from "./buildMenu"
-import dispatchToProps from "../../lib/dispatchToProps"
-import * as spaces from "../../reducers/spaces"
 
 type OwnProps = {|
   log: Log,
@@ -25,65 +18,30 @@ type OwnProps = {|
 |}
 
 type StateProps = {|
-  space: Space,
-  resultType: ResultsTabEnum,
-  menuActions: MenuItemData[]
+  actions: MenuItemData[]
 |}
 
 type Props = {|
   ...OwnProps,
-  ...StateProps,
-  ...DispatchProps
+  ...StateProps
 |}
 
 export default class LogCellActions extends React.Component<Props> {
-  renderItem = (item: MenuItemData, index: number) => {
-    switch (item.type) {
-      case "action":
-        return (
-          <li
-            key={index}
-            onClick={item.onClick.bind(null, this.props.dispatch)}
-          >
-            {item.text}
-          </li>
-        )
-      case "seperator":
-        return <hr key={index} />
-    }
-  }
-
   render() {
     return (
-      <Measure bounds>
-        {({measureRef, contentRect}) => {
-          return (
-            <Portal
-              isOpen={true}
-              onClose={this.props.onClose}
-              style={MenuStyler.ensureVisible(
-                contentRect.bounds,
-                this.props.style
-              )}
-            >
-              <MenuList ref={measureRef}>
-                {this.props.menuActions.map(this.renderItem)}
-              </MenuList>
-            </Portal>
-          )
-        }}
-      </Measure>
+      <RightClickMenu
+        actions={this.props.actions}
+        onClose={this.props.onClose}
+        style={this.props.style}
+      />
     )
   }
 }
 
 const stateToProps = (state: State, props: OwnProps): StateProps => ({
-  menuActions: buildMenu(state, props),
-  space: spaces.getCurrentSpace(state),
-  resultType: getResultsTab(state)
+  actions: buildMenu(state, props)
 })
 
 export const XLogCellActions = connect<Props, OwnProps, _, _, _, _>(
-  stateToProps,
-  dispatchToProps
+  stateToProps
 )(LogCellActions)
