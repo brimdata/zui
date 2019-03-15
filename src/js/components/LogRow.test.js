f7163dea949091b11d0821b9ac71a3bbce12ee25
James Kerr <jkerr838@gmail.com>
2019-03-15 13:21:22 -0700

    feat: A simple test for the log row

---
+++ src/js/components/LogRow.test.js
@@ -0,0 +1,38 @@
+/* @flow */
+
+import React from "react"
+
+import {shallow} from "enzyme"
+
+import {conn} from "../test/mockLogs"
+import LogRow from "./LogRow"
+import TableColumns from "../models/TableColumns"
+
+let props
+beforeEach(() => {
+  const log = conn()
+  props = {
+    columns: new TableColumns("1", log.descriptor, {
+      "1": log.descriptor.map(d => ({...d, isVisible: true}))
+    }),
+    dimens: {
+      rowHeight: 25,
+      rowWidth: 300,
+      viewHeight: 1000,
+      viewWidth: 1000,
+      listHeight: 20000,
+      listWidth: 10000
+    },
+    highlight: false,
+    index: 1,
+    log: log,
+    timeZone: "UTC",
+    onClick: () => {}
+  }
+})
+
+test("Rendering the row", () => {
+  const comp = shallow(<LogRow {...props} />)
+
+  expect(comp).toMatchSnapshot()
+})
