c6f05aad8d25f159bbbe5d2f959a2c596fd386dd
Michael Brown <mikeb@looky.cloud>
2019-03-18 17:57:19 -0700

    add a workflow that runs integration tests

---
+++ .circleci/rug-component.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This is a thin thing that pulls in the proper rug.git branch and runs
+# it. rug.git is responsible for maintaining and updating this code.
+# Updates to it should go there, be tested for all components, and then
+# deployed to component repositories as needed.
+set -euo pipefail
+
+PRODUCT_BRANCH="master"
+
+rm -rf /var/tmp/rug.git
+git clone --depth=1 --branch="${PRODUCT_BRANCH}" git@github.com:looky-cloud/rug.git /var/tmp/rug.git
+cd /var/tmp/rug.git
+./circleci-build.sh "${PRODUCT_BRANCH}"
