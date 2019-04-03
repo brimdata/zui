7220d1d6b2fb2f33271ee9efd45da7d9ebf54880
James Kerr <jkerr838@gmail.com>
2019-04-03 16:46:10 -0700

    feat: Add mime type correlations

--- src/js/models/searches/Md5Search.test.js
+++ src/js/models/searches/Md5Search.test.js
@@ -6,6 +6,6 @@ test("#getProgram", () => {
   let search = new Md5Search(files(), [new Date(0), new Date(1)])
 
   expect(search.getProgram()).toEqual(
-    "md5=- | count() by md5 | sort -r | head 5; count() by rx_hosts | sort -r | head 5; count() by tx_hosts | sort -r | head 5"
+    "md5=- | count() by filename, mime_type | sort -r | head 5; count() by md5 | sort -r | head 5; count() by rx_hosts | sort -r | head 5; count() by tx_hosts | sort -r | head 5"
   )
 })
--- src/js/models/searches/programs.js
+++ src/js/models/searches/programs.js
@@ -11,3 +11,7 @@ export function txHostsCorrelation(md5: string) {
 export function rxHostsCorrelation(md5: string) {
   return `md5=${md5} | count() by rx_hosts | sort -r | head 5`
 }
+
+export function filenameCorrelation(md5: string) {
+  return `md5=${md5} | count() by filename, mime_type | sort -r | head 5`
+}
