c240fe57533439f40f0c1f89f821c211d6f2a75b
James Kerr <jkerr838@gmail.com>
2019-04-04 08:16:29 -0700

    fix: Only fetch details if right pane is open

--- src/js/actions/logDetails.js
+++ src/js/actions/logDetails.js
@@ -4,12 +4,15 @@ import type {Thunk} from "redux-thunk"
 
 import type {Descriptor, Tuple} from "../types"
 import {fetchByMd5, fetchTuplesByUid} from "./correlations"
+import {getRightSidebarIsOpen} from "../reducers/view"
 import Log from "../models/Log"
 
-export const viewLogDetail = (log: Log): Thunk => dispatch => {
-  dispatch(pushLogDetail(log))
-  dispatch(fetchTuplesByUid(log))
-  dispatch(fetchByMd5(log))
+export const viewLogDetail = (log: Log): Thunk => (dispatch, getState) => {
+  if (getRightSidebarIsOpen(getState())) {
+    dispatch(pushLogDetail(log))
+    dispatch(fetchTuplesByUid(log))
+    dispatch(fetchByMd5(log))
+  }
 }
 
 export const pushLogDetail = ({
