ce9e986103c842065561a9d9be167566dff395fa
James Kerr <jkerr838@gmail.com>
2019-04-15 11:57:19 -0700

    feat: Test HistogramSearch

--- src/js/models/searches/HistogramSearch.test.js
+++ src/js/models/searches/HistogramSearch.test.js
@@ -1,6 +1,21 @@
 /* @flow */
 
+import {Handler} from "../../BoomClient"
 import HistogramSearch from "./HistogramSearch"
+
+const resultPayload = {
+  channel_id: 0,
+  type: "SearchResult",
+  results: {
+    descriptor: [{name: "ts", type: "time"}, {name: "count", type: "count"}],
+    tuples: [["9999", "1"], ["9998", "2"]]
+  }
+}
+
+const endPayload = {
+  channel_id: 0,
+  type: "SearchEnd"
+}
 
 describe("HistogramSearch", () => {
   const program = "_path = conn"
@@ -15,5 +30,23 @@ describe("HistogramSearch", () => {
     expect(search.getProgram()).toEqual(
       "_path = conn | every 1sec count() by _path"
     )
+  })
+
+  test("#receiveData", () => {
+    let handler = new Handler()
+    let dispatch = jest.fn()
+    search.receiveData(handler, dispatch)
+
+    handler.receive(resultPayload)
+    handler.receive(endPayload)
+
+    expect(dispatch).toHaveBeenNthCalledWith(1, {
+      type: "HISTOGRAM_CLEAR"
+    })
+
+    expect(dispatch).toHaveBeenNthCalledWith(2, {
+      type: "HISTOGRAM_SEARCH_RESULT",
+      data: resultPayload.results
+    })
   })
 })
