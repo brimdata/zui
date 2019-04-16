19492e791a8257bf398c52f718bbee7b3091811a
James Kerr <jkerr838@gmail.com>
2019-04-16 15:11:23 -0700

    fix: Ignore the bin dir

--- .importjs.js
+++ .importjs.js
@@ -1,11 +1,12 @@
 module.exports = {
+  environments: ["node"],
   excludes: ["./dist/**", "**/**.test.js"],
   namedExports: {
     reselect: ["createSelector"],
     enzyme: ["mount", "shallow"],
     react: ["useState", "useRef", "useEffect", "useLayoutEffect"],
     "react-dom": ["renderToString"],
-    lodash: ["get", "isEqual", "isEmpty", "every"],
+    lodash: ["get", "isEqual", "isEmpty", "every", "kebabCase"],
     "react-router-dom": ["Redirect"]
   }
 }
