16136bcd7827661cb571c0024a6ab9e21f45e17c
James Kerr <jkerr838@gmail.com>
2019-04-29 16:37:47 -0700

    refactor: Clean Login page

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -1,12 +1,10 @@
 /* @flow */
 
 import {Redirect} from "react-router-dom"
-import {bindActionCreators} from "redux"
 import {connect} from "react-redux"
-import React from "react"
+import React, {useEffect, useState} from "react"
 
 import type {Credentials} from "../lib/Credentials"
-import {checkLookytalkVersion} from "../state/thunks/boomd"
 import {fetchSpaces} from "../backend/fetch"
 import {getCredentials} from "../state/reducers/boomd"
 import {setAppMenu} from "../electron/setAppMenu"
@@ -14,169 +12,90 @@ import {setBoomdCredentials} from "../state/actions"
 import {trim} from "../lib/Str"
 import {updateBoomOptions} from "../backend/options"
 import AdminTitle from "./AdminTitle"
-import AppError from "../models/AppError"
-import BoomRequest from "../BoomClient/lib/BoomRequest"
 import ErrorFactory from "../models/ErrorFactory"
+import LoginForm from "./LoginForm"
 import LookyHeader from "./LookyHeader"
-import delay from "../lib/delay"
+import dispatchToProps from "../lib/dispatchToProps"
 
 type Props = {|
   credentials: Credentials,
-  setBoomdCredentials: Function,
-  connectBoomd: Function,
   dispatch: Function
 |}
 
-type CompState = {|
-  ...$Exact<Credentials>,
-  error: ?AppError,
-  isConnected: boolean,
-  isConnecting: boolean,
-  request: ?BoomRequest
-|}
+export default function Login({dispatch, credentials}: Props) {
+  let [request, setRequest] = useState(null)
+  let [error, setError] = useState(null)
+  let [status, setStatus] = useState("disconnected")
 
-export default class Login extends React.Component<Props, CompState> {
-  constructor(props: Props) {
-    setAppMenu("LOGIN")
-    super(props)
-    this.state = {
-      ...props.credentials,
-      error: null,
-      isConnecting: false,
-      isConnected: false,
-      request: null
+  useEffect(onMount, [])
+
+  function onChange(creds) {
+    dispatch(setBoomdCredentials(creds))
+  }
+
+  function validate({host, port}) {
+    if (!trim(host) || !+port) {
+      setError(ErrorFactory.create("Host and port are required."))
+      return false
+    } else {
+      return true
     }
   }
 
-  onSubmit = (e: Event) => {
-    e.preventDefault()
-    const {host, port} = this.state
-
-    if (!trim(host) || !trim(port)) {
-      this.setState({error: ErrorFactory.create("Host and port are required.")})
-      return
-    }
-
-    this.setState({isConnecting: true, error: null})
-    e.preventDefault()
-
-    this.props.setBoomdCredentials(this.state)
-    this.props.dispatch(updateBoomOptions())
-
-    let request = this.props.dispatch(fetchSpaces())
-
-    request
-      .done(() => {
-        setTimeout(() => this.props.dispatch(checkLookytalkVersion()), 3000)
-        delay(300, () => this.setState({isConnected: true}))
-      })
-      .error((e) => {
-        delay(300, () =>
-          this.setState({
-            isConnecting: false,
-            error: ErrorFactory.create(e)
-          })
-        )
-      })
-      .onAbort(() => {
-        this.setState({isConnecting: false})
-      })
-
-    this.setState({request})
-  }
-
-  render() {
-    if (this.state.isConnected) {
-      return <Redirect to="/search" />
-    }
-
-    return (
-      <main className="admin-page boomd-connect">
-        <LookyHeader />
-        <AdminTitle title="Connect To Boom" />
-
-        <div className="admin-panel-wrapper">
-          <div className="admin-panel">
-            <form className="admin-form" onSubmit={this.onSubmit}>
-              <div className="form-field">
-                <label>Host</label>
-                <input
-                  autoFocus
-                  type="text"
-                  name="host"
-                  value={this.state.host || ""}
-                  onChange={(e) => this.setState({host: e.currentTarget.value})}
-                />
-              </div>
-              <div className="form-field">
-                <label>Port</label>
-                <input
-                  type="text"
-                  name="port"
-                  value={this.state.port || ""}
-                  onChange={(e) => this.setState({port: e.currentTarget.value})}
-                />
-              </div>
-              <div className="form-field">
-                <label>User</label>
-                <input
-                  type="text"
-                  name="user"
-                  value={this.state.user || ""}
-                  onChange={(e) => this.setState({user: e.currentTarget.value})}
-                />
-              </div>
-              <div className="form-field">
-                <label>Pass</label>
-                <input
-                  type="password"
-                  name="pass"
-                  value={this.state.pass || ""}
-                  onChange={(e) => this.setState({pass: e.currentTarget.value})}
-                />
-              </div>
-              <button
-                disabled={this.state.isConnecting}
-                type="submit"
-                className="button"
-              >
-                {this.state.isConnecting ? "Connecting..." : "Connect"}
-              </button>
-              <div className="status-message">
-                {this.state.isConnecting ? (
-                  <ConnectingMessage
-                    onCancel={() => {
-                      this.state.request && this.state.request.abort()
-                    }}
-                  />
-                ) : null}
-                {this.state.error ? (
-                  <p className="form-error">{this.state.error.message()}</p>
-                ) : null}
-              </div>
-            </form>
-          </div>
-        </div>
-      </main>
+  function onSubmit() {
+    setStatus("connecting")
+    dispatch(updateBoomOptions())
+    setRequest(
+      dispatch(fetchSpaces())
+        .done(onSuccess)
+        .error(onError)
     )
   }
+
+  function onSuccess() {
+    setStatus("connected")
+  }
+
+  function onError(e) {
+    setStatus("disconnected")
+    setError(ErrorFactory.create(e))
+  }
+
+  function cancel() {
+    setError(null)
+    setStatus("disconnected")
+    request && request.abort()
+  }
+
+  if (status === "connected") return <Redirect to="/search" />
+
+  return (
+    <main className="admin-page boomd-connect">
+      <LookyHeader />
+      <AdminTitle title="Connect To Boom" />
+      <div className="admin-panel-wrapper">
+        <div className="admin-panel">
+          <LoginForm
+            credentials={credentials}
+            onChange={onChange}
+            onSubmit={onSubmit}
+            status={status}
+            error={error}
+            cancel={cancel}
+            validate={validate}
+          />
+        </div>
+      </div>
+    </main>
+  )
 }
 
-function ConnectingMessage({onCancel}) {
-  return (
-    <p>
-      Connecting to boom...<a onClick={onCancel}>Cancel</a>
-    </p>
-  )
+function onMount() {
+  setAppMenu("LOGIN")
 }
 
 const stateToProps = (state) => ({
   credentials: getCredentials(state)
-})
-
-const dispatchToProps = (dispatch) => ({
-  ...bindActionCreators({setBoomdCredentials}, dispatch),
-  dispatch
 })
 
 export const XLogin = connect<Props, {||}, _, _, _, _>(
