2e3de7b775a0ef66ec3916869541f5ed20e20a80
James Kerr <jkerr838@gmail.com>
2019-04-30 16:23:17 -0700

    feat: Keep track of when bar is focused and allow toggle of focus

--- src/js/charts/elements/focusBar.js
+++ src/js/charts/elements/focusBar.js
@@ -6,23 +6,32 @@ import {getPointAt} from "../getPointAt"
 import Chart from "../Chart"
 
 type Props = {
-  onBarClick: Function
+  onFocus: Function,
+  onBlur: Function
 }
 
-export default function({onBarClick}: Props) {
+function findSpan(ts, chart) {
+  let {number, unit} = chart.data.interval
+  let a = chart.scales.timeScale(ts)
+  let b = chart.scales.timeScale(add(ts, number, unit))
+  let width = Math.max(Math.floor(b - a) - 2, 2)
+
+  return [a, a + width].map(chart.scales.timeScale.invert)
+}
+
+export default function({onFocus, onBlur}: Props) {
+  let focused = false
+
   function mount(chart: Chart) {
     function onClick() {
       let data = getPointAt(d3.event.offsetX, chart)
-      if (data) {
-        const ts = data.ts
-        const {number, unit} = chart.data.interval
-        const a = chart.scales.timeScale(ts)
-        const b = chart.scales.timeScale(add(ts, number, unit))
-        let width = Math.max(Math.floor(b - a) - 2, 2)
 
-        const span = [a, a + width].map(chart.scales.timeScale.invert)
-
-        onBarClick && onBarClick(span)
+      if (data && !focused) {
+        focused = true
+        onFocus(findSpan(data.ts, chart))
+      } else {
+        focused = false
+        onBlur()
       }
     }
 
