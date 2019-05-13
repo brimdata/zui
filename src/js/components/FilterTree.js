3840a8f77797fbaf7ccf3052d7de3da515a287c9
James Kerr <jkerr838@gmail.com>
2019-05-13 14:25:11 -0700

    refactor: convert to function component

--- src/js/components/FilterTree.js
+++ src/js/components/FilterTree.js
@@ -1,84 +1,60 @@
 /* @flow */
 
-import {connect} from "react-redux"
 import {isEqual} from "lodash"
+import {useDispatch, useSelector} from "react-redux"
 import React from "react"
 
-import type {Dispatch, State} from "../state/types"
-import {
-  type Investigation,
-  getInvestigation
-} from "../state/reducers/investigation"
 import {Node} from "../models/Node"
 import {createInvestigationTree} from "../investigation/createTree"
 import {deleteFindingByTs, setSearchBarPins} from "../state/actions"
 import {fetchMainSearch} from "../viewer/fetchMainSearch"
+import {getInvestigation} from "../state/reducers/investigation"
 import {getSearchBarPins} from "../state/selectors/searchBar"
 import CloseSVG from "../icons/circle-x-md.svg"
 import FilterNode from "./FilterNode"
 
-type OwnProps = {||}
+export default function FilterTree() {
+  let dispatch = useDispatch()
+  let investigation = useSelector(getInvestigation)
+  let pinnedFilters = useSelector(getSearchBarPins)
 
-type DispatchProps = {|
-  dispatch: Dispatch
-|}
-
-type StateProps = {|
-  investigation: Investigation,
-  pinnedFilters: string[]
-|}
-
-type AllProps = {
-  ...DispatchProps,
-  ...OwnProps,
-  ...StateProps
-}
-
-export default class FilterTree extends React.Component<AllProps> {
-  onNodeClick = (node: Node) => {
-    this.props.dispatch(setSearchBarPins(getPinnedFilters(node)))
-    this.props.dispatch(fetchMainSearch({saveToHistory: false}))
+  function onNodeClick(node: Node) {
+    dispatch(setSearchBarPins(getPinnedFilters(node)))
+    dispatch(fetchMainSearch({saveToHistory: false}))
   }
 
-  renderNode = (node: Node, i: number) => {
-    const classNames = ["filter-tree-node"]
+  function renderNode(node: Node, i: number) {
+    let classNames = ["filter-tree-node"]
 
-    if (nodeIsPinned(this.props.pinnedFilters, node)) classNames.push("active")
+    if (nodeIsPinned(pinnedFilters, node)) classNames.push("active")
 
     return (
       <div key={i} className={classNames.join(" ")}>
-        <div
-          className="filter-tree-parent"
-          onClick={() => this.onNodeClick(node)}
-        >
+        <div className="filter-tree-parent" onClick={() => onNodeClick(node)}>
           <FilterNode filter={node.data.filter} />
           <a
             className="delete-button"
             onClick={(e) => {
               e.stopPropagation()
               let multiTs = node.mapChildren((node) => node.data.finding.ts)
-              this.props.dispatch(deleteFindingByTs(multiTs))
+              dispatch(deleteFindingByTs(multiTs))
             }}
           >
             <CloseSVG />
           </a>
         </div>
         <div className="filter-tree-children">
-          {node.children.map(this.renderNode)}
+          {node.children.map(renderNode)}
         </div>
       </div>
     )
   }
 
-  render() {
-    let tree = createInvestigationTree(this.props.investigation)
+  let tree = createInvestigationTree(investigation)
 
-    return (
-      <div className="filter-tree">
-        {tree.getRoot().children.map(this.renderNode)}
-      </div>
-    )
-  }
+  return (
+    <div className="filter-tree">{tree.getRoot().children.map(renderNode)}</div>
+  )
 }
 
 export function getPinnedFilters(node: ?Node) {
@@ -105,19 +81,3 @@ export function nodeIsPinned(pinnedFilters: string[], node: ?Node) {
 
   return true
 }
-
-function stateToProps(state: State) {
-  return {
-    investigation: getInvestigation(state),
-    pinnedFilters: getSearchBarPins(state)
-  }
-}
-
-const dispatchToProps = (dispatch: Dispatch) => ({
-  dispatch
-})
-
-export const XFilterTree = connect<AllProps, OwnProps, _, _, _, _>(
-  stateToProps,
-  dispatchToProps
-)(FilterTree)
