4d0ed996e6caff9dde8c2b52da59832bfa3b771f
James Kerr <jkerr838@gmail.com>
2019-05-17 09:22:26 -0700

    feat: Animate the enter and moving of history entries

--- src/js/components/Investigation/InvestigationLinear.js
+++ src/js/components/Investigation/InvestigationLinear.js
@@ -5,34 +5,44 @@ import {useSelector} from "react-redux"
 import React, {useMemo} from "react"
 
 import {getInvestigation} from "../../state/reducers/investigation"
+import AnimateChildren from "../AnimateChildren"
 import FindingCard from "./FindingCard"
 import FindingSpanCard from "./FindingSpanCard"
 
-export default function InvestigationLinear() {
-  let findings = useSelector(getInvestigation)
-  let sorted = useMemo(() => {
-    return [...findings].sort((a, b) => (a.ts < b.ts ? 1 : -1))
-  }, [findings])
-
-  function sameSpan(a, b) {
-    if (!b) return false
-    return isEqual(a.search.span, b.search.span)
-  }
+export default React.memo<{}>(function InvestigationLinear() {
+  let findings = useFindings()
 
   let cards = []
-  sorted.forEach((f, i) => {
-    cards.push(
-      <FindingCard index={i} key={f.ts.getTime().toString()} finding={f} />
-    )
-    if (!sameSpan(f, sorted[i + 1])) {
-      cards.push(
-        <FindingSpanCard
-          key={f.ts.getTime().toString() + "1"}
-          span={f.search.span}
-        />
-      )
+  let keys = {}
+  let autoCount = 0
+
+  findings.forEach((f, i) => {
+    cards.push(<FindingCard index={i} key={getKey(f)} finding={f} />)
+    if (!sameSpan(f, findings[i + 1])) {
+      let key = JSON.stringify(f.search.span)
+      if (keys[key]) key + autoCount
+      autoCount++
+      cards.push(<FindingSpanCard key={key} span={f.search.span} />)
     }
   })
 
-  return <div className="investigation-linear">{cards}</div>
+  return (
+    <AnimateChildren className="investigation-linear">{cards}</AnimateChildren>
+  )
+})
+
+function getKey(finding) {
+  return finding.ts.getTime().toString()
+}
+
+function useFindings() {
+  let findings = useSelector(getInvestigation)
+  return useMemo(() => {
+    return [...findings].sort((a, b) => (a.ts < b.ts ? 1 : -1))
+  }, [findings])
+}
+
+function sameSpan(a, b) {
+  if (!b) return false
+  return isEqual(a.search.span, b.search.span)
 }
