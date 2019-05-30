dcd41daeaf23c63dc313c5869131ea3b4a447530
Michael Brown <mikeb@looky.cloud>
2019-05-30 16:20:10 -0700

    test: test 4 queries
    This commit adds tests for 4 basic query shapes:
    
    - pure search
    - analytics search
    - analytics search with field group-by
    - analytics search with ts group-by (via every)

--- itest/poc.test.js
+++ itest/poc.test.js
@@ -119,6 +119,41 @@ const waitForHistogram = (app) => {
   )
 }
 
+const writeSearch = (app, searchText) =>
+  app.client.setValue(selectors.search.input, searchText)
+
+const startSearch = (app) => app.client.click(selectors.search.button)
+
+const searchDisplay = async (app) => {
+  // This stinks. We have to use getHTML because headers that are off the
+  // screen return as empty strings if you use getText. This isn't required of
+  // actual results.
+  // See http://v4.webdriver.io/api/property/getText.html
+  // app.browserWindow.maximize() fixes the problem on my laptop but not CircleCI.
+  // But what we get back includes the width which can be non-deterministic:
+  // <div class="header-cell" style="width: 192px;">ts<div class="col-resizer"></div></div>
+  // That style width will vary on my laptop vs. CircleCI.
+  // The hack is to split this and extract just the text.
+  // '<div class="header-cell" style="width: 192px;">ts<div // class="col-resizer"></div></div>'.split('>')[1].split('<')[0]
+
+  const _trim = (s: string) => s.split(">")[1].split("<")[0]
+
+  const headerResults = () => {
+    return app.client.getHTML(selectors.viewer.headers)
+    .then((headers) => {
+      if (typeof headers === "string") {
+        headers = [headers]
+      }
+      return headers.map((h) => _trim(h))
+    })
+  }
+  const searchResults = () => app.client.getText(selectors.viewer.results)
+
+  let headers = await headerResults()
+  let search = await searchResults()
+  return headers.concat(search)
+}
+
 const handleError = async (app, initialError, done) => {
   let realError = undefined
   let notificationError = undefined
@@ -294,6 +329,96 @@ describe("Application launch", () => {
               )
             })
           })
+          done()
+        })
+        .catch((err) => {
+          handleError(app, err, done)
+        })
+    },
+    TestTimeout
+  )
+
+  test(
+    "query path=weird | sort",
+    (done) => {
+      waitForLoginAvailable(app)
+        .then(() => logIn(app))
+        .then(() => waitForHistogram(app))
+        .then(() => waitForSearch(app))
+        .then(() => writeSearch(app, "_path=weird | sort"))
+        .then(() => startSearch(app))
+        .then(() => waitForSearch(app))
+        .then(() => searchDisplay(app))
+        .then((results) => {
+          expect(results).toMatchSnapshot()
+          done()
+        })
+        .catch((err) => {
+          handleError(app, err, done)
+        })
+    },
+    TestTimeout
+  )
+
+  test(
+    "query path=_http | count()",
+    (done) => {
+      waitForLoginAvailable(app)
+        .then(() => logIn(app))
+        .then(() => waitForHistogram(app))
+        .then(() => waitForSearch(app))
+        .then(() => writeSearch(app, "_path=http | count()"))
+        .then(() => startSearch(app))
+        .then(() => waitForSearch(app))
+        .then(() => searchDisplay(app))
+        .then((results) => {
+          expect(results).toMatchSnapshot()
+          done()
+        })
+        .catch((err) => {
+          handleError(app, err, done)
+        })
+    },
+    TestTimeout
+  )
+
+  test(
+    "query _path=http | count() by id.resp_p | sort",
+    (done) => {
+      waitForLoginAvailable(app)
+        .then(() => logIn(app))
+        .then(() => waitForHistogram(app))
+        .then(() => waitForSearch(app))
+        .then(() =>
+          writeSearch(app, "_path=http | count() by id.resp_p | sort")
+        )
+        .then(() => startSearch(app))
+        .then(() => waitForSearch(app))
+        .then(() => searchDisplay(app))
+        .then((results) => {
+          expect(results).toMatchSnapshot()
+          done()
+        })
+        .catch((err) => {
+          handleError(app, err, done)
+        })
+    },
+    TestTimeout
+  )
+
+  test(
+    "query _path=http | every 5m count()",
+    (done) => {
+      waitForLoginAvailable(app)
+        .then(() => logIn(app))
+        .then(() => waitForHistogram(app))
+        .then(() => waitForSearch(app))
+        .then(() => writeSearch(app, "_path=http | every 5m count()"))
+        .then(() => startSearch(app))
+        .then(() => waitForSearch(app))
+        .then(() => searchDisplay(app))
+        .then((results) => {
+          expect(results).toMatchSnapshot()
           done()
         })
         .catch((err) => {
