4690e78700c1f69c04da513974dbe279345be4bf
Michael Brown <mikeb@looky.cloud>
2019-07-29 12:32:21 -0700

    itest fix: more verbosity in histogram test

--- itest/tests/histogram.test.js
+++ itest/tests/histogram.test.js
@@ -70,12 +70,14 @@ describe("Histogram tests", () => {
   stdTest("histogram deep inspection", (done) => {
     // This is a data-sensitive test that assumes the histogram has corelight
     // data loaded. There are inline comments that explain the test's flow.
+    console.log("Pre-login")
     waitForLoginAvailable(app)
       .then(() => logIn(app))
       .then(() => waitForHistogram(app))
       .then(() => waitForSearch(app))
-      .then(() =>
-        retryUntil(
+      .then(async () => {
+        console.log("Checking number of histogram rect elements")
+        let result = await retryUntil(
           () => app.client.$$(selectors.histogram.rectElem),
           (rectElements) =>
             rectElements.length ===
@@ -85,7 +87,9 @@ describe("Histogram tests", () => {
             "Histogram did not render the expected number of rect elements"
           )
         })
-      )
+        console.log("Got number of histogram rect elements")
+        return result
+      })
       .then(async () => {
         // Assuming we properly loaded corelight data into the default space, we
         // we must wait until the components of the histogram are rendered. This
@@ -93,12 +97,14 @@ describe("Histogram tests", () => {
         // elements depend on both the dataset itself and the product's behavior.
         // For example, these values will change if the default time window
         // changes from the last 30 minutes.
+        console.log("Getting number of distinct _paths")
         let pathClasses = await retryUntil(
           () => app.client.getAttribute(selectors.histogram.gElem, "class"),
           (pathClasses) =>
             pathClasses.length ===
             dataSets.corelight.histogram.defaultDistinctPaths
         )
+        console.log("Got number of distinct _paths")
         expect(pathClasses.sort()).toMatchSnapshot()
         // Here is the meat of the test verification. Here we fetch all 4
         // attributes' values of all rect elements, in a 2-D array of _path and
@@ -109,17 +115,20 @@ describe("Histogram tests", () => {
         // propagate without doing this using async / await. In some cases
         // exceptions were quashed; in others they were uncaught. I gave up
         // because I got this pattern to work.
+        console.log("Getting all rect elements")
         let allRectValues = await Promise.all(
           pathClasses.map(
             async (pathClass) => await verifyPathClassRect(app, pathClass)
           )
         )
+        console.log("Got all rect elements")
         expect(allRectValues.length).toBe(
           // Whereas we just counted g elements before, this breaks down rect
           // elements within their g parent, ensuring rect elements are of the
           // proper _path.
           dataSets.corelight.histogram.defaultDistinctPaths
         )
+        console.log("Ensuring all rect elements' attributes are sane")
         allRectValues.forEach((pathClass) => {
           // The 4 comes from each of x, y, width, height for a rect element.
           expect(pathClass.length).toBe(4)
@@ -129,9 +138,13 @@ describe("Histogram tests", () => {
             )
           })
         })
+        console.log("Ensured all rect elements' attributes are sane")
+        console.log("Switching to 'Whole Space'")
         // Now set to "Whole Space" to make sure this histogram is redrawn.
         await setSpan(app, "Whole Space")
+        console.log("Switched to 'Whole Space'")
         // Just count a higher number of _paths, not all ~1500 rect elements.
+        console.log("Checking rect elements in Whole Space")
         await retryUntil(
           () => app.client.getAttribute(selectors.histogram.gElem, "class"),
           (pathClasses) =>
