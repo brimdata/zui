74c9a55ab399e72a67b645984e2e9a73e6e20b21
Michael Brown <mikeb@brimsecurity.com>
2019-09-03 15:44:27 -0700

    test feature: application step debug-level logging

--- itest/lib/app.js
+++ itest/lib/app.js
@@ -3,12 +3,22 @@
 import {retry} from "./control"
 import {selectors} from "../../src/js/test/integration"
 import {Application} from "spectron"
+import {LOG} from "./log"
+
+const appStep = async (stepMessage, f) => {
+  LOG.debug(`Starting step "${stepMessage}"`)
+  let result = await f()
+  LOG.debug(`Finished step "${stepMessage}"`)
+  return result
+}
 
 export const logIn = (app: Application) => {
-  return app.client
-    .setValue(selectors.login.host, "localhost")
-    .setValue(selectors.login.port, "9867")
-    .click(selectors.login.button)
+  return appStep("fill out login page and log in", () =>
+    app.client
+      .setValue(selectors.login.host, "localhost")
+      .setValue(selectors.login.port, "9867")
+      .click(selectors.login.button)
+  )
 }
 
 export const waitForLoginAvailable = (app: Application) => {
@@ -21,29 +31,41 @@ export const waitForLoginAvailable = (app: Application) => {
   const waitForButton = () => {
     return app.client.waitForExist(selectors.login.button)
   }
-  return waitForHostname()
-    .then(() => waitForPort())
-    .then(() => waitForButton())
+  return appStep("wait for hostname, port, and login widget", () =>
+    waitForHostname()
+      .then(() => waitForPort())
+      .then(() => waitForButton())
+  )
 }
 
 export const waitForSearch = (app: Application) => {
-  return retry(() => app.client.element("#main-search-input").getValue())
+  return appStep("wait for main search input to appear", () =>
+    retry(() => app.client.element("#main-search-input").getValue())
+  )
 }
 
 export const waitForHistogram = (app: Application) => {
-  return retry(() =>
-    app.client.element(selectors.histogram.topLevel).getAttribute("class")
+  return appStep("wait for base histogram element hierarchy", () =>
+    retry(() =>
+      app.client.element(selectors.histogram.topLevel).getAttribute("class")
+    )
   )
 }
 
 export const writeSearch = (app: Application, searchText: string) =>
-  app.client.setValue(selectors.search.input, searchText)
+  appStep("write to main search", () =>
+    app.client.setValue(selectors.search.input, searchText)
+  )
 
 export const getSearchText = (app: Application) =>
-  app.client.getValue(selectors.search.input)
+  appStep("get contents of main search", () =>
+    app.client.getValue(selectors.search.input)
+  )
 
 export const startSearch = (app: Application) =>
-  app.client.click(selectors.search.button)
+  appStep("click the search button", () =>
+    app.client.click(selectors.search.button)
+  )
 
 export const searchDisplay = async (app: Application) => {
   // This stinks. We have to use getHTML because headers that are off the
@@ -58,16 +80,19 @@ export const searchDisplay = async (app: Application) => {
   const _trim = (s: string) => s.split(">")[1].split("<")[0]
 
   const headerResults = () => {
-    return retry(() => app.client.getHTML(selectors.viewer.headers)).then(
-      (headers) => {
-        if (typeof headers === "string") {
-          headers = [headers]
-        }
-        return headers.map((h) => _trim(h))
+    return appStep("get search fields", () =>
+      retry(() => app.client.getHTML(selectors.viewer.headers))
+    ).then((headers) => {
+      if (typeof headers === "string") {
+        headers = [headers]
       }
-    )
+      return headers.map((h) => _trim(h))
+    })
   }
-  const searchResults = () => app.client.getText(selectors.viewer.results)
+  const searchResults = () =>
+    appStep("get search tuples", () =>
+      app.client.getText(selectors.viewer.results)
+    )
 
   let headers = await headerResults()
   let search = await searchResults()
@@ -76,10 +101,14 @@ export const searchDisplay = async (app: Application) => {
 }
 
 export const getCurrentSpace = (app: Application) =>
-  app.client.getText(selectors.spaces.button)
+  appStep("get the current space", () =>
+    app.client.getText(selectors.spaces.button)
+  )
 
 const getSearchStat = (app: Application, selector: string) =>
-  app.client.getText(selector).then((text) => parseFloat(text.split(" ")[0]))
+  appStep(`get search stats for selector "${selector}"`, () =>
+    app.client.getText(selector).then((text) => parseFloat(text.split(" ")[0]))
+  )
 
 export const getSearchSpeed = (app: Application) =>
   getSearchStat(app, selectors.search.speed)
@@ -88,9 +117,15 @@ export const getSearchTime = (app: Application) =>
   getSearchStat(app, selectors.search.time)
 
 export const setSpan = (app: Application, span: string) => {
-  const clickSpanButton = () => app.client.click(selectors.span.button)
+  const clickSpanButton = () =>
+    appStep("click span selector", () =>
+      app.client.click(selectors.span.button)
+    )
 
-  const clickSpan = () => app.client.click(selectors.span.menuItem(span))
+  const clickSpan = () =>
+    appStep(`select span ${span}`, () =>
+      app.client.click(selectors.span.menuItem(span))
+    )
 
   return clickSpanButton().then(() => clickSpan())
 }
