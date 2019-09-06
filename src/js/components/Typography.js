3f67e75b6cd97143e8d55cd3b31df1410e4c5e95
James Kerr <jkerr838@gmail.com>
2019-09-06 15:15:56 -0700

    Remove used code

--- src/js/components/Typography.js
+++ src/js/components/Typography.js
@@ -4,14 +4,11 @@ import * as React from "react"
 import classNames from "classnames"
 import createTypeEl from "./createTypeEl"
 
-export const Header = createTypeEl("h2", "header")
 export const Paragraph = createTypeEl("p", "paragraph")
 export const Mono = createTypeEl("p", "mono")
 export const Fieldset = createTypeEl("p", "fieldset")
 export const Subscript = createTypeEl("sup", "subscript")
 export const Label = createTypeEl("label", "label")
-export const Title = createTypeEl("h1", "title")
-export const Link = createTypeEl("a", "link")
 export const LinkButton = createTypeEl("a", "link-button")
 export const Stats = createTypeEl("span", "stats")
 
