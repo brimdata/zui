b807d8c5f4caa9fedeb6fb1c7e13a966239a8e6f
James Kerr <jkerr838@gmail.com>
2019-09-24 10:48:27 -0700

    refactor: use a loging controller

--- src/js/components/Login/LoginPage.js
+++ src/js/components/Login/LoginPage.js
@@ -1,72 +1,71 @@
 /* @flow */
-import {useDispatch, useSelector} from "react-redux"
-import React, {useEffect, useState} from "react"
+import React from "react"
 
-import {ipcRenderer} from "electron"
-
-import {addCluster} from "../../state/clusters/actions"
-import {connectCluster} from "../../state/clusters/thunks"
-import {getBackendError} from "../../backend"
-import {getSavedClusters} from "../../state/clusters/selectors"
+import {Input, InputCheckbox, InputSubmit} from "../form/Inputs"
+import {reactElementProps} from "../../test/integration"
 import Brand from "./Brand"
 import EmptyCheck from "../EmptyCheck"
-import LoginForm from "./LoginForm"
+import Form from "../form/Form"
 import LoginWelcome from "./LoginWelcome"
 import SavedClusters from "./SavedClusters"
+import useLoginController from "./useLoginController"
 
 export default function LoginPage() {
-  let saved = useSelector(getSavedClusters)
-  let error = useSelector(getBackendError)
-  let dispatch = useDispatch()
-  let [form, setForm] = useState({
-    host: "",
-    username: "",
-    password: "",
-    save: true
-  })
-
-  useEffect(() => {
-    ipcRenderer.send("open-login-window")
-  }, [])
-
-  function onChange(e) {
-    if (e.target.type === "checkbox") {
-      setForm({...form, [e.target.name]: e.target.checked})
-    } else {
-      setForm({...form, [e.target.name]: e.target.value})
-    }
-  }
-
-  function submit(form) {
-    let hostparts = form.host.split(":")
-    let host = hostparts[0]
-    let port = hostparts[1] || "9867"
-    let creds = {
-      host,
-      port,
-      username: form.username,
-      password: form.password
-    }
-    dispatch(connectCluster(creds)).then(() => {
-      if (form.save) dispatch(addCluster(creds))
-    })
-  }
+  let login = useLoginController()
 
   return (
     <div className="login">
       <aside>
         <Brand />
-        <EmptyCheck array={saved} empty={<LoginWelcome />}>
-          <SavedClusters saved={saved} setForm={setForm} submit={submit} />
+        <EmptyCheck array={login.saved} empty={<LoginWelcome />}>
+          <SavedClusters
+            onClick={login.submitSaved}
+            onRightClick={login.showSavedMenu}
+            saved={login.saved}
+          />
         </EmptyCheck>
       </aside>
       <main>
-        <LoginForm
-          data={form}
-          onChange={onChange}
-          status={error ? error.message() : ""}
-          submit={submit}
-        />
+        <div className="login-form" {...reactElementProps("login")}>
+          <h3 className="section-heading">New Connection</h3>
+          <Form onSubmit={login.submitForm}>
+            <Input
+              label="Host:"
+              name="host"
+              type="text"
+              required
+              autoFocus
+              value={login.form.host}
+              onChange={login.onFormChange}
+            />
+            <Input
+              label="Username:"
+              name="username"
+              type="text"
+              value={login.form.username}
+              onChange={login.onFormChange}
+            />
+            <Input
+              label="Password:"
+              name="password"
+              type="password"
+              value={login.form.password}
+              onChange={login.onFormChange}
+            />
+            <InputCheckbox
+              label="Save credentials"
+              name="save"
+              checked={login.form.save}
+              onChange={login.onFormChange}
+            />
+            <InputSubmit value="Connect" />
+          </Form>
+          {login.error && (
+            <p className="status">
+              {login.error.message()} <a onClick={login.clearError}>Dismiss</a>{" "}
+            </p>
+          )}
+        </div>
       </main>
     </div>
   )
---
+++ src/js/components/Login/types.js
@@ -0,0 +1,7 @@
+/* @flow */
+export type FormData = {
+  host: string,
+  username: string,
+  password: string,
+  save: boolean
+}
