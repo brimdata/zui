ed8cbdcd43046568cdd48349ee6b269a5a73c029
James Kerr <jkerr838@gmail.com>
2019-09-27 09:38:49 -0700

    fix: Include first, always add sorts

--- src/js/rightclick/logsMenu.js
+++ src/js/rightclick/logsMenu.js
@@ -33,14 +33,14 @@ export function logsMenu(program: string, space: Space) {
       menu.queryAction(fromTime(field), toTime(field))
     } else {
       menu.queryAction(
-        exclude(field),
         include(field),
+        exclude(field),
         countBy(field),
-        freshInclude(field),
-        sortAsc(field),
-        sortDesc(field)
+        freshInclude(field)
       )
     }
+
+    menu.queryAction(sortAsc(field), sortDesc(field))
 
     if (["addr", "set[addr]"].includes(field.type)) {
       menu.fieldAction(whoisRightclick(field))
