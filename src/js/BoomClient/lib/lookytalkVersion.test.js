042f37d2d71049405d2dddbdc226c6e81cd8cb95
Andrew Swan <aswan@mozilla.com>
2019-10-17 09:12:09 -0500

    revert test-only stuff for merging

--- src/js/BoomClient/lib/lookytalkVersion.test.js
+++ src/js/BoomClient/lib/lookytalkVersion.test.js
@@ -2,6 +2,5 @@
 import lookytalkVersion from "./lookytalkVersion"
 
 test("version", () => {
-  // expect(lookytalkVersion()).toMatch(/v\d+\.\d+\.\d+/)
-  expect(lookytalkVersion()).toMatch(/.*/)
+  expect(lookytalkVersion()).toMatch(/v\d+\.\d+\.\d+/)
 })
