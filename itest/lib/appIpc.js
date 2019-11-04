b611e00cd20f18ff562436c4d46b930908cabf1a
Michael Brown <mikeb@brimsecurity.com>
2019-11-04 15:55:30 -0800

    test chore: add logging to appIpc.contextMenu()

--- itest/lib/appIpc.js
+++ itest/lib/appIpc.js
@@ -4,6 +4,7 @@ import type {Space} from "../../src/js/lib/Space"
 import Field from "../../src/js/models/Field"
 import Log from "../../src/js/models/Log"
 import menu from "../../src/js/electron/menu"
+import {LOG} from "./log"
 
 // This module exists to work around difficulties in using Spectron to drive
 // Brim with its components that use Electron's native support. Right now
@@ -23,15 +24,19 @@ export default function contextMenu(
     },
 
     click(label: string, field: Field, log: Log) {
-      let item = menu
-        .fieldContextMenu(program, columns, space)(
-          field.toBrimField(),
-          log,
-          false
-        )
-        .find((item) => item.label && item.label === label)
-
+      LOG.debug(
+        'Creating menu from log entry on field "' + field.toString() + '"...'
+      )
+      let ipcMenu = menu.fieldContextMenu(program, columns, space)(
+        field.toBrimField(),
+        log,
+        false
+      )
+      LOG.debug(`Finding menu item with label "${label}"...`)
+      let item = ipcMenu.find((item) => item.label && item.label === label)
+      // The item.click test below is needed to make flow happy.
       if (item && item.click) {
+        LOG.debug("About to click menu item...")
         item.click(item, {webContents: app.webContents})
       } else {
         throw new Error("Could not find '" + label + "' in context menu")
