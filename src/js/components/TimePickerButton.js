16f616500c64859ec8806e34ab5a1e7372b72a35
James Kerr <jkerr838@gmail.com>
2019-11-11 15:27:50 -0800

    feat: Time Picker Steppers Works

---
+++ src/js/components/TimePickerButton.js
@@ -0,0 +1,157 @@
+/* @flow */
+import React, {useState} from "react"
+import classNames from "classnames"
+
+import {add, format, subtract} from "../lib/Time"
+import Animate from "./Animate"
+import CarrotDown from "../icons/carrot-down.svg"
+import CarrotUp from "../icons/carrot-up.svg"
+import MenuBarButton from "./MenuBarButton"
+import useFuzzyHover from "../hooks/useFuzzyHover"
+
+type Props = {
+  date: Date,
+  onChange: Function
+}
+
+export default function TimePickerButton({date, onChange}: Props) {
+  let [[x, y], setPosition] = useState([0, 0])
+  let [unit, setUnit] = useState("month")
+  let fuzzy = useFuzzyHover(0, 150)
+
+  function updatePosition(e) {
+    fuzzy.mouseEnter()
+    let {width, x, y} = e.currentTarget.getBoundingClientRect()
+    let stepperWidth = 20
+    let centeredX = x + width / 2 - stepperWidth / 2
+    setUnit(e.currentTarget.dataset.unit)
+    setPosition([centeredX, y])
+  }
+
+  function onUp() {
+    onChange(add(date, 1, unit))
+  }
+
+  function onDown() {
+    onChange(subtract(date, 1, unit))
+  }
+
+  return (
+    <div
+      className={classNames("time-picker-button", {hovering: fuzzy.hovering})}
+      onMouseLeave={fuzzy.mouseLeave}
+    >
+      <div className="hover-zone" />
+      <Steppers
+        show={fuzzy.hovering}
+        onUp={onUp}
+        onDown={onDown}
+        style={{transform: `translate(${x}px, ${y}px)`}}
+      />
+      <MenuBarButton>
+        <TimePiece data-unit="month" onMouseEnter={updatePosition}>
+          {format(date, "MMM")}
+        </TimePiece>
+        <TimePiece data-unit="day" onMouseEnter={updatePosition}>
+          {format(date, "DD")}
+        </TimePiece>
+        ,
+        <TimePiece data-unit="year" onMouseEnter={updatePosition}>
+          {format(date, "YYYY")}
+        </TimePiece>
+        <TimePiece data-unit="hour" onMouseEnter={updatePosition}>
+          {format(date, "HH")}
+        </TimePiece>
+        :
+        <TimePiece data-unit="minute" onMouseEnter={updatePosition}>
+          {format(date, "mm")}
+        </TimePiece>
+      </MenuBarButton>
+    </div>
+  )
+}
+
+function TimePiece({children, ...rest}) {
+  return (
+    <div className="time-piece" {...rest}>
+      <div className="hover-zone" />
+      {children}
+    </div>
+  )
+}
+
+function Steppers({show, onUp, onDown, style}) {
+  const enter = (anime, el) =>
+    anime
+      .timeline({
+        easing: "easeOutExpo",
+        duration: 400
+      })
+      .add({
+        targets: el.querySelector(".step-up"),
+        translateY: [0, "-100%"],
+        opacity: 1
+      })
+      .add(
+        {
+          targets: el.querySelector(".step-down"),
+          translateY: [0, "100%"],
+          opacity: 1
+        },
+        75
+      )
+      .add(
+        {
+          targets: el.querySelector(".step-up svg"),
+          opacity: [0, 1]
+        },
+        200
+      )
+      .add(
+        {
+          targets: el.querySelector(".step-down svg"),
+          opacity: [0, 1]
+        },
+        200 + 75
+      )
+
+  const exit = (anime, el) =>
+    anime
+      .timeline({duration: 100, easing: "linear"})
+      .add({
+        targets: el.querySelector(".step-down"),
+        opacity: 0
+      })
+      .add(
+        {
+          targets: el.querySelector(".step-up"),
+          opacity: 0
+        },
+        0
+      )
+
+  return (
+    <Animate show={show} enter={enter} exit={exit}>
+      <div className="steppers" style={style}>
+        <StepUp onClick={onUp} />
+        <StepDown onClick={onDown} />
+      </div>
+    </Animate>
+  )
+}
+
+function StepUp(props) {
+  return (
+    <div className="stepper step-up" {...props}>
+      <CarrotUp />
+    </div>
+  )
+}
+
+function StepDown(props) {
+  return (
+    <div className="stepper step-down" {...props}>
+      <CarrotDown />
+    </div>
+  )
+}
---
+++ src/js/components/TimeSpanPickers.js
@@ -0,0 +1,30 @@
+/* @flow */
+import {useDispatch, useSelector} from "react-redux"
+import React from "react"
+import classNames from "classnames"
+
+import {getTimeWindow} from "../state/reducers/timeWindow"
+import {setOuterTimeWindow} from "../state/actions"
+import SpanDuration from "./SpanDuration"
+import TimePickerButton from "./TimePickerButton"
+
+export default function TimeSpanPickers() {
+  let [from, to] = useSelector(getTimeWindow)
+  let dispatch = useDispatch()
+
+  function fromChange(date) {
+    dispatch(setOuterTimeWindow([date, to]))
+  }
+
+  function toChange(date) {
+    dispatch(setOuterTimeWindow([from, date]))
+  }
+
+  return (
+    <div className={classNames("time-span-pickers")}>
+      <TimePickerButton date={from} onChange={fromChange} />
+      <SpanDuration span={[from, to]} />
+      <TimePickerButton date={to} onChange={toChange} />
+    </div>
+  )
+}
