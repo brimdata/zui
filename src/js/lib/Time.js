4c4b819bd58a79b1eb5ef9ba1dc197ff6a12f471
James Kerr <jkerr838@gmail.com>
2019-11-14 16:00:49 -0800

    refactor: removed time parse func

--- src/js/lib/Time.js
+++ src/js/lib/Time.js
@@ -6,12 +6,3 @@ export const moment = Moment
 
 export const format = (date: Date, format: string) =>
   Moment(date).format(format)
-
-export const parse = (
-  string: string,
-  format: string,
-  strict: boolean = true
-): ?Date => {
-  const m = Moment(string, format, strict)
-  return m.isValid() ? m.toDate() : null
-}
