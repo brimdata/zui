72aeb86f4b007838277e861933d5187396d130d9
James Kerr <jkerr838@gmail.com>
2019-11-21 16:45:42 -0800

    fix: work around webdriver limitation

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -8,18 +8,21 @@ import {XLatestError} from "./LatestError"
 import {getTimeZone} from "../state/reducers/view"
 import ClusterGate from "./Login/ClusterGate"
 import brim from "../brim"
+import useListener from "../hooks/useListener"
 
 export default function App() {
   brim.time.setZone(useSelector(getTimeZone))
   let [mouse, setMouse] = useState(true)
+
+  useListener(document, "mousedown", () => setMouse(true))
+  useListener(document, "keydown", () => setMouse(false))
+
   return (
     <div
       className={classNames("app-wrapper", {
         "using-mouse": mouse,
         "using-keyboard": !mouse
       })}
-      onMouseDown={() => setMouse(true)}
-      onKeyDown={() => setMouse(false)}
     >
       <div className="title-bar-drag-area" />
       <XLatestError />
