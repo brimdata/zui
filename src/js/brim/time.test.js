1a6fac8730603fa84bd33bafb2b3e7401270c554
James Kerr <jkerr838@gmail.com>
2019-11-25 11:22:58 -0800

    fix: TS as internal time representation
    I was previously converting ts into dates and loosing 6 digits
    of precision. This caused some NaNs to appear on the page
    when subtracting two close dates resulted in 0.
    
    Keep the ns for as long as possible.

--- src/js/brim/time.test.js
+++ src/js/brim/time.test.js
@@ -11,3 +11,27 @@ test("date to ts", () => {
 
   expect(brim.time(ts).toDate()).toEqual(new Date(1))
 })
+
+test("add", () => {
+  let ts = brim
+    .time(new Date(1))
+    .add(1, "ms")
+    .toTs()
+
+  expect(ts).toEqual({
+    ns: 2000000,
+    sec: 0
+  })
+})
+
+test("subtract", () => {
+  let ts = brim
+    .time(new Date(1))
+    .subtract(1, "second")
+    .toTs()
+
+  expect(ts).toEqual({
+    ns: 1000000,
+    sec: -1
+  })
+})
