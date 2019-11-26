61eefad4108b4d993ccdb425b58812d2be64b491
James Kerr <jkerr838@gmail.com>
2019-11-26 14:55:55 -0800

    feat: Replace 'Other' with 'New connection'

--- src/js/components/ClusterPicker.js
+++ src/js/components/ClusterPicker.js
@@ -24,7 +24,7 @@ export default function CluterPicker() {
 
   template.push({type: "divider"})
   template.push({
-    label: "Other...",
+    label: "New connection...",
     click: () => {
       dispatch(disconnectCluster())
     }
