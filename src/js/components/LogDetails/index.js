07e5e31305425b2c999bee8ff1b85b24548408b8
James Kerr <jkerr838@gmail.com>
2019-11-27 17:46:39 -0800

    feat: useSearch in Md5Panel

--- src/js/components/LogDetails/index.js
+++ src/js/components/LogDetails/index.js
@@ -4,36 +4,26 @@ import {useSelector} from "react-redux"
 import React from "react"
 
 import {Md5Panel} from "./Md5Panel"
-import type {Search} from "../../state/searches/types"
 import {buildLogDetail, getIsGoingBack} from "../../state/selectors/logDetails"
-import {getLogDetailSearches} from "../../state/searches/selector"
 import ConnPanel from "./ConnPanel"
 import FieldsPanel from "./FieldsPanel"
-import Log from "../../models/Log"
 import NavAnimation from "./NavAnimation"
 import NoSelection from "./NoSelection"
 import UidPanel from "./UidPanel"
 
-export type PanelProps = {|
-  searches: Search[],
-  log: Log
-|}
-
 export default function LogDetails() {
   let log = useSelector(buildLogDetail)
   let isGoingBack = useSelector(getIsGoingBack)
-  let searches = useSelector(getLogDetailSearches)
-  let panelProps = {log, searches}
 
   if (!log) return <NoSelection />
 
   return (
     <NavAnimation log={log} prev={isGoingBack}>
       <div className="log-detail">
-        <FieldsPanel {...panelProps} />
-        {log.correlationId() && <UidPanel {...panelProps} />}
-        <ConnPanel {...panelProps} />
-        <Md5Panel {...panelProps} />
+        <FieldsPanel log={log} />
+        {log.correlationId() && <UidPanel log={log} />}
+        <ConnPanel log={log} />
+        {log.get("md5") && <Md5Panel log={log} />}
       </div>
     </NavAnimation>
   )
