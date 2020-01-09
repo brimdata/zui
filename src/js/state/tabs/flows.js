ec5c7a08f725244d0a12f6f0e4256a4c1900beaa
James Kerr <jkerr838@gmail.com>
2020-01-09 13:44:20 -0800

    feat: Add a flows file for tabs state
    The flows functions are for conditional or complex state changes,
    where you need to look at the state to determine what to do.
    
    I think it makes sense to put then in their state objects if they
    are only concerned with that slice of state. More complex state
    maniuplaton goes in the flows top level directory.

---
+++ src/js/state/tabs/flows.js
@@ -0,0 +1,42 @@
+/* @flow */
+import type {Thunk} from "../types"
+import Tabs from "./"
+import brim from "../../brim"
+
+export default {
+  new: (): Thunk => (dispatch, getState) => {
+    let {search} = Tabs.getActiveTab(getState())
+    let id = brim.randomHash()
+    dispatch(Tabs.add(id, search))
+    dispatch(Tabs.activate(id))
+    let el = document.getElementById("main-search-input")
+    if (el) el.focus()
+  },
+
+  closeActive: (): Thunk => (dispatch, getState) => {
+    let id = Tabs.getActive(getState())
+    dispatch(Tabs.remove(id))
+  },
+
+  activateNext: (): Thunk => (dispatch, getState) => {
+    let id = Tabs.getActive(getState())
+    let tabs = Tabs.getData(getState())
+    let index = tabs.findIndex((tab) => tab.id === id)
+    let next = tabs[(index + 1) % tabs.length]
+    dispatch(Tabs.activate(next.id))
+  },
+
+  activatePrev: (): Thunk => (dispatch, getState) => {
+    let id = Tabs.getActive(getState())
+    let tabs = Tabs.getData(getState())
+    let index = tabs.findIndex((tab) => tab.id === id)
+    let next = tabs[(tabs.length + index - 1) % tabs.length]
+    dispatch(Tabs.activate(next.id))
+  },
+
+  activateByIndex: (index: number): Thunk => (dispatch, getState) => {
+    let tabs = Tabs.getData(getState())
+    let tab = tabs[index]
+    if (tab) dispatch(Tabs.activate(tab.id))
+  }
+}
--- src/js/state/tabs/index.js
+++ src/js/state/tabs/index.js
@@ -1,10 +1,12 @@
 /* @flow */
 import actions from "./actions"
+import flows from "./flows"
 import reducer from "./reducer"
 import selectors from "./selectors"
 
 export default {
   ...actions,
   ...selectors,
+  ...flows,
   reducer
 }
