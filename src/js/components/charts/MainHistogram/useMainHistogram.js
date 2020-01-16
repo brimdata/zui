11e7b69c1334c979f02a972e0378e1aa910e0804
James Kerr <jkerr838@gmail.com>
2020-01-16 10:04:06 -0800

    refactor: Normalized Chart state

--- src/js/components/charts/MainHistogram/useMainHistogram.js
+++ src/js/components/charts/MainHistogram/useMainHistogram.js
@@ -7,12 +7,12 @@ import * as d3 from "d3"
 import type {DateTuple} from "../../../lib/TimeWindow"
 import type {Pen, HistogramChart} from "../types"
 import {innerHeight, innerWidth} from "../dimens"
+import Chart from "../../../state/Chart"
 import EmptyMessage from "../../EmptyMessage"
 import HistogramTooltip from "../../HistogramTooltip"
 import LoadingMessage from "../../LoadingMessage"
 import barStacks from "../pens/barStacks"
 import brim from "../../../brim"
-import chart from "../../../state/chart"
 import focusBar from "../pens/focusBar"
 import format from "./format"
 import hoverLine from "../pens/hoverLine"
@@ -28,8 +28,8 @@ import xPositionTooltip from "../pens/xPositionTooltip"
 import yAxisSingleTick from "../pens/yAxisSingleTick"
 
 export default function(width: number, height: number): HistogramChart {
-  let records = useSelector(chart.getRecords)
-  let status = useSelector(chart.getStatus)
+  let records = useSelector(Chart.getRecords)
+  let status = useSelector(Chart.getStatus)
   let span = useSelector(tab.getSpanAsDates)
   let innerSpan = useSelector(tab.getSpanFocusAsDates)
 
