9b6208b808ad98c1305b94513deed9fb05c4c305
James Kerr <jkerr838@gmail.com>
2020-02-18 08:47:05 -0800

    feat: Started shaping the mainReducer

--- src/js/state/mainReducer.js
+++ src/js/state/mainReducer.js
@@ -1,15 +1,10 @@
 /* @flow */
-export type MainState = {count: number}
-let initState = {count: 1}
-const reducer = (state: MainState = initState, action: *) => {
-  switch (action.type) {
-    case "UP":
-      return {count: state.count + 1}
-    case "DOWN":
-      return {count: state.count - 1}
-    default:
-      return state
-  }
-}
+import {combineReducers} from "redux"
 
-export default reducer
+import {VERSION} from "../initializers/initPersistance"
+import Investigation from "./Investigation"
+
+export default combineReducers<*, *>({
+  investigation: Investigation.reducer,
+  version: () => VERSION
+})
