fb25c66e3654031fd7ec538e02d9b5315e044694
James Kerr <jkerr838@gmail.com>
2020-02-18 11:14:08 -0800

    fix: Lint error unused var

--- src/js/components/SearchPage.test.js
+++ src/js/components/SearchPage.test.js
@@ -5,11 +5,10 @@ import SearchPage from "./SearchPage"
 import loginTo from "../test/helpers/loginTo"
 import provide from "../test/helpers/provide"
 
-let boom, store, cluster
+let store, cluster
 beforeEach(async () => {
   jest.useRealTimers()
   let setup = await loginTo("cluster1", "space1")
-  boom = setup.boom
   store = setup.store
   cluster = setup.cluster
   jest.useFakeTimers()
