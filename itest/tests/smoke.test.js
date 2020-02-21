70f1233eef2a2e7a8dd4c4d2abd0a83a07ff6a17
Michael Brown <mikeb@brimsecurity.com>
2020-02-20 18:29:28 -0800

    test: adjust smoke test for Brim

--- itest/tests/smoke.test.js
+++ itest/tests/smoke.test.js
@@ -2,7 +2,12 @@
 
 import {basename} from "path"
 
-import {logIn, newAppInstance, resetState, startApp} from "../lib/app.js"
+import {
+  newAppInstance,
+  resetState,
+  startApp,
+  waitForSearch
+} from "../lib/app.js"
 import {handleError, stdTest} from "../lib/jest.js"
 
 describe("Smoke test", () => {
@@ -20,27 +25,8 @@ describe("Smoke test", () => {
     }
   })
 
-  // TODO: Parallel runs across files are not supported due to chromebrowser
-  // port contention. Support that later.
-  stdTest("show a sane window; log in and see Search and Histogram", (done) => {
-    app.client
-      // Use waitForExist, not waitForVisible: title isn't visible in the app.
-      .waitForExist("title")
-      .then(() => app.client.getTitle())
-      .then((title) => {
-        // TODO: Brim shouldn't be hardcoded but instead read from a title
-        // defined elsewhere.
-        expect(title.toLowerCase()).toBe("brim")
-      })
-      .then(() => app.client.waitForVisible(".brand h1"))
-      // TODO: Don't use selectors as literals in tests. These definitions
-      // should be defined in a single place and ideally be tested to ensure
-      // they can be found.
-      .then(() => app.client.getText(".brand h1"))
-      .then((headerText) => {
-        expect(headerText.toLowerCase()).toBe("welcome to brim")
-      })
-      .then(() => logIn(app))
+  stdTest("show a sane window; log in and see Search", (done) => {
+    waitForSearch(app)
       .then((val) => {
         expect(val).toBeDefined()
         done()
