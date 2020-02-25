afd89879bbe4f1b2eca2ee820c5d732263440f4f
Alfred Landrum <alfred@leakybucket.org>
2020-02-25 12:58:45 -0800

    fetch and use zqd release binaries (#410)
    This change adds an npm postinstall step that will download a zqd
    binary, placing it inside a new zdeps directory. At runtime, the ZQD
    class will launch the zqd binary from that location.
    
    During development, its possible to ignore that binary, and instead use
    zqd from the path, via an environment variable:
    
    brim_zqd_from_path=1 npm start
    
    This also causes the zqd instance launched by Brim to listen only to
    localhost by default, instead of all interfaces.

---
+++ scripts/download-zqd/index.js
@@ -0,0 +1,108 @@
+/* @noflow */
+
+const fs = require("fs-extra")
+const got = require("got")
+const path = require("path")
+const tmp = require("tmp")
+const { unzip } = require("cross-unzip")
+
+// zqd version to download & use.
+const zqdVersion = "v0.2.0"
+
+// Path and filename for the zqd executable.
+const zqdPath = ["zdeps"]
+const platformDefs = {
+  "linux": {
+    zqdBin: "zqd",
+    osarch: "linux-amd64",
+  },
+  "win32": {
+    zqdBin: "zqd.exe",
+    osarch: "windows-amd64",
+  },
+  "darwin": {
+    zqdBin: "zqd",
+    osarch: "darwin-amd64",
+  }
+}
+
+async function download(url, targetfile) {
+  await fs.mkdirp(path.dirname(targetfile))
+  const writeStream = fs.createWriteStream(targetfile)
+  return new Promise((resolve, reject) => {
+    const gotStream = got.stream(url)
+    gotStream.pipe(writeStream)
+    gotStream.on("error", err => {
+      if (writeStream.destroy) {
+        writeStream.destroy(err)
+      }
+      reject(err)
+    })
+    writeStream.on("error", err => reject(err))
+    writeStream.on("close", () => resolve())
+  })
+}
+
+async function unzipTo(zipfile, dir) {
+  await fs.mkdirp(dir)
+  return new Promise((resolve, reject) => {
+    unzip(zipfile, dir, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+
+function zqdArtifactPaths(version) {
+  if (!(process.platform in platformDefs)) {
+    throw new Error("unsupported platform")
+  }
+  const plat = platformDefs[process.platform]
+
+  const artifactFile = `zq-${version}.${plat.osarch}.zip`
+  const artifactUrl = `https://github.com/brimsec/zq/releases/download/${version}/${artifactFile}`
+  const relativeBinPath = path.join(`zq-${version}.${plat.osarch}`, plat.zqdBin)
+
+  return {
+    artifactFile,
+    artifactUrl,
+    relativeBinPath,
+    binName: plat.zqdBin,
+  }
+}
+
+// Download and extract the zqd binary for this platform to the specified
+// directory. Returns the absolute path of the zqd binary file.
+async function zqdDownload(version, destPath) {
+  const paths = zqdArtifactPaths(version)
+  const destdir = path.join(...destPath)
+  const destBinLocation = path.resolve(path.join(destdir, paths.binName))
+
+  const tmpdir = tmp.dirSync({unsafeCleanup: true})
+  try {
+    const destArchive = path.join(tmpdir.name, paths.artifactFile)
+    await download(paths.artifactUrl, destArchive)
+    await unzipTo(destArchive, tmpdir.name)
+
+    const zqdBinPath = path.join(tmpdir.name, paths.relativeBinPath)
+    fs.mkdirpSync(destdir)
+    fs.moveSync(zqdBinPath, destBinLocation, {overwrite: true})
+  } finally {
+    tmpdir.removeCallback()
+  }
+
+  return destBinLocation
+}
+
+zqdDownload(zqdVersion,zqdPath)
+  .then((location) => {
+    console.log("zqd " + zqdVersion + " downloaded to " + location)
+  })
+  .catch((err) => {
+    console.error("zqd setup: ", err)
+    process.exit(1)
+  })
--- src/js/zqd/zqd.js
+++ src/js/zqd/zqd.js
@@ -1,15 +1,52 @@
 /* @flow */
-import {mkdirSync} from "fs"
+
+import {pathExistsSync, mkdirSync} from "fs-extra"
 import {spawn, ChildProcess} from "child_process"
+import {join, resolve} from "path"
 
 import * as cmd from "../stdlib/cmd"
+import electronIsDev from "../electron/isDev"
+
+// Path and filename for the zqd executable.
+const zqdPath = ["zdeps"]
+const platformDefs = {
+  darwin: {
+    zqdBin: "zqd"
+  },
+  linux: {
+    zqdBin: "zqd"
+  },
+  win32: {
+    zqdBin: "zqd.exe"
+  }
+}
+
+function zqdCommand(): string {
+  const plat = platformDefs[process.platform]
+  if (!plat) {
+    throw new Error("unsupported platform for zqd")
+  }
+
+  if (electronIsDev && process.env.brim_zqd_from_path) {
+    if (cmd.notExists("zqd").length > 0) {
+      throw new Error("brim_zqd_from_path is set but zqd not in path")
+    }
+    return plat.zqdBin
+  }
+
+  const zqdBin = resolve(join(...zqdPath, plat.zqdBin))
+  if (!pathExistsSync(zqdBin)) {
+    throw new Error("zqd binary not present at " + zqdBin)
+  }
+  return zqdBin
+}
 
 export class ZQD {
   zqd: ChildProcess
   root: string
 
   constructor(rootDir: string) {
-    let not = cmd.notExists("zeek", "zqd", "zq", "mergecap")
+    let not = cmd.notExists("zeek", "zq", "mergecap")
     if (not.length > 0) {
       throw new Error("missing required executables: " + not.join(", "))
     }
@@ -22,10 +59,11 @@ export class ZQD {
       cwd: this.root,
       stdio: "inherit"
     }
-    this.zqd = spawn("zqd", ["listen"], opts)
+
+    this.zqd = spawn(zqdCommand(), ["listen", "-l", this.addr()], opts)
     this.zqd.on("error", (err) => {
       // XXX should notify renderers of error
-      console.log("error", err)
+      console.log("zqd spawn error", err)
     })
   }
 
