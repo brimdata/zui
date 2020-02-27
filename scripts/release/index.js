8a04eb4d41f9593210c9c15828d353d220bcc091
Henri DF <henridf@gmail.com>
2020-02-27 16:44:39 +0100

    Add notarization
    Notarization needs to be done after signing but before creating the
    disk image.

--- scripts/release/index.js
+++ scripts/release/index.js
@@ -1,5 +1,6 @@
 /* @noflow */
 
+const notarize = require("electron-notarize").notarize;
 const program = require("commander")
 const pack = require("./pack")
 const install = require("./install")
@@ -7,9 +8,19 @@ const install = require("./install")
 program
   .option("--win32", "Release for Windows")
   .option("--darwin", "Release for macOS")
-  .option("--sign", "Sign package (currently only macOS)", false)
+  .option("--sign", "Sign package (macOS only)", false)
+  .option("--notarize", "Notarize package (macOS only)", false)
   .action(function(cmd) {
-    if (cmd.darwin) pack.darwin(cmd.sign).then(() => install.darwin())
+      if (cmd.darwin) {
+          p = pack.darwin(cmd.sign || cmd.notarize)
+          if (cmd.notarize) p.then(() => notarize({
+            appBundleId: "com.electron.brim",
+            appPath: "dist/packages/Brim-darwin-x64/Brim.app",
+            appleId: process.env.MACOS_APPLEID_USER,
+            appleIdPassword: process.env.MACOS_APPLEID_PASSWORD,
+          }))
+          p.then(() => install.darwin())
+      }
     if (cmd.win32) pack.win32().then(() => install.win32())
   })
   .parse(process.argv)
