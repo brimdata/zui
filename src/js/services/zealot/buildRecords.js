09a04afb78c84eeaaaf7eb044712c6f374f786fa
James Kerr <jkerr838@gmail.com>
2020-03-04 18:02:58 -0800

    fix: Comment out for now

--- src/js/services/zealot/buildRecords.js
+++ src/js/services/zealot/buildRecords.js
@@ -1,13 +1,12 @@
 /* @flow */
-import nestedRecord from "../../brim/flatRecordsBuffer/nestedRecord"
-
 export default function buildRecords(input: *[]) {
-  let types = {}
-
-  let records = input.map((r) => {
-    if (r.type) types[r.id] = r.type
-    return nestedRecord(r.values, types[r.id]).flatten()
-  })
-
-  return {records, types}
+  return input
+  // let types = {}
+  //
+  // let records = input.map((r) => {
+  //   if (r.type) types[r.id] = r.type
+  //   return nestedRecord(r.values, types[r.id]).flatten()
+  // })
+  //
+  // return {records, types}
 }
