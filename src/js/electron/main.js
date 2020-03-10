eccf29398164288e210fe7d2b14d7dc815b877e1
Mason Fish <mason@looky.cloud>
2020-03-10 16:37:01 -0700

    adjust about menu
    Signed-off-by: Mason Fish <mason@looky.cloud>

--- src/js/electron/main.js
+++ src/js/electron/main.js
@@ -38,6 +38,24 @@ async function main() {
   windowsMainHandler(winMan)
   globalStoreMainHandler(store, winMan)
 
+  app.setAboutPanelOptions({
+    applicationName: app.getName(),
+    applicationVersion: app.getVersion(),
+    // copyright: "",
+    // version: "",
+    website: "https://www.brimsecurity.com",
+    iconPath: "dist/static/AppIcon.icns"
+  })
+  // options Object
+  // applicationName String (optional) - The app's name.
+  // applicationVersion String (optional) - The app's version.
+  // copyright String (optional) - Copyright information.
+  //   version String (optional) macOS - The app's build version number.
+  // credits String (optional) macOS Windows - Credit information.
+  //   authors String Linux - List of app authors.
+  //   website String (optional) Linux - The app's website.
+  // iconPath String (optional) Linux Windows - Path to the app's icon. On Linux, will be shown as 64x64 pixels while retaining aspect ratio.
+
   app.on("ready", () => {
     installExtensions()
     winMan.init(sessionState)
