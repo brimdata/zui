31c48f2b1bc0a46b1fb7f67ed33f83f21cbbc26d
Michael Brown <mikeb@apache.org>
2020-03-16 17:04:17 -0700

    fix: make zeek.org URLs return 200

--- src/js/brim/zeekLogInfo.js
+++ src/js/brim/zeekLogInfo.js
@@ -4,53 +4,53 @@ import zeekLogDescriptions from "../services/zeekLogDescriptions"
 
 const specialUrls = {
   netcontrol_drop:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/netcontrol/drop.zeek.html",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/netcontrol/drop.zeek.html",
   broker:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/broker/log.zeek.html",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/broker/log.zeek.html",
   kerberos:
-    "http://docs.zeek.org/en/stable/scripts/base/protocols/krb/main.zeek.html#type-KRB::Info",
+    "http://docs.zeek.org/en/current/scripts/base/protocols/krb/main.zeek.html#type-KRB::Info",
   tunnel:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/tunnels/main.zeek.html#type-Tunnel::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/tunnels/main.zeek.html#type-Tunnel::Info",
   reporter:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/reporter/main.zeek.html#type-Reporter::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/reporter/main.zeek.html#type-Reporter::Info",
   x509:
-    "http://docs.zeek.org/en/stable/scripts/base/files/x509/main.zeek.html#type-X509::Info",
+    "http://docs.zeek.org/en/current/scripts/base/files/x509/main.zeek.html#type-X509::Info",
   netcontrol_shunt:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/netcontrol/shunt.zeek.html#type-NetControl::ShuntInfo",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/netcontrol/shunt.zeek.html#type-NetControl::ShuntInfo",
   dce_rpc:
-    "http://docs.zeek.org/en/stable/scripts/base/protocols/dce-rpc/main.zeek.html#type-DCE_RPC::Info",
+    "http://docs.zeek.org/en/current/scripts/base/protocols/dce-rpc/main.zeek.html#type-DCE_RPC::Info",
   smb_mapping:
-    "http://docs.zeek.org/en/stable/scripts/base/protocols/smb/main.zeek.html#type-SMB::TreeInfo",
+    "http://docs.zeek.org/en/current/scripts/base/protocols/smb/main.zeek.html#type-SMB::TreeInfo",
   signatures:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/signatures/main.zeek.html#type-Signatures::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/signatures/main.zeek.html#type-Signatures::Info",
   openflow:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/openflow/plugins/log.zeek.html#type-OpenFlow::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/openflow/plugins/log.zeek.html#type-OpenFlow::Info",
   pe:
-    "http://docs.zeek.org/en/stable/scripts/base/files/pe/main.zeek.html#type-PE::Info",
+    "http://docs.zeek.org/en/current/scripts/base/files/pe/main.zeek.html#type-PE::Info",
   files:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/files/main.zeek.html#type-Files::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/files/main.zeek.html#type-Files::Info",
   weird:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/notice/weird.zeek.html#type-Weird::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/notice/weird.zeek.html#type-Weird::Info",
   cluster:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/cluster/main.zeek.html#type-Cluster::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/cluster/main.zeek.html#type-Cluster::Info",
   notice:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/notice/main.zeek.html#type-Notice::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/notice/main.zeek.html#type-Notice::Info",
   notice_alarm:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/notice/main.zeek.html#type-Notice::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/notice/main.zeek.html#type-Notice::Info",
   intel:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/intel/main.zeek.html#type-Intel::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/intel/main.zeek.html#type-Intel::Info",
   dpd:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/dpd/main.zeek.html#type-DPD::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/dpd/main.zeek.html#type-DPD::Info",
   software:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/software/main.zeek.html#type-Software::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/software/main.zeek.html#type-Software::Info",
   packet_filter:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/packet-filter/main.zeek.html#type-PacketFilter::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/packet-filter/main.zeek.html#type-PacketFilter::Info",
   config:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/config/main.zeek.html#type-Config::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/config/main.zeek.html#type-Config::Info",
   netcontrol:
-    "http://docs.zeek.org/en/stable/scripts/base/frameworks/netcontrol/main.zeek.html#type-NetControl::Info",
+    "http://docs.zeek.org/en/current/scripts/base/frameworks/netcontrol/main.zeek.html#type-NetControl::Info",
   smb_files:
-    "http://docs.zeek.org/en/stable/scripts/base/protocols/smb/main.zeek.html#type-SMB::FileInfo"
+    "http://docs.zeek.org/en/current/scripts/base/protocols/smb/main.zeek.html#type-SMB::FileInfo"
 }
 
 type ZeekLogInfoClass = ReturnType<typeof knownPath>
@@ -71,7 +71,7 @@ function knownPath(path: string, info: ZeekInfo) {
     docsUrl: () => {
       return path in specialUrls
         ? specialUrls[path]
-        : `http://docs.zeek.org/en/stable/scripts/base/protocols/${path}/main.zeek.html`
+        : `http://docs.zeek.org/en/current/scripts/base/protocols/${path}/main.zeek.html`
     },
     describeColumn: (col: {name: string, type: string}) => {
       let firstPartOfName = col.name.split(".")[0]
