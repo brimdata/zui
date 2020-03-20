82c2240d162519b5574b372ffee4e681180df9a8
Mason Fish <mason@looky.cloud>
2020-03-20 16:35:52 -0700

    address pr comments
    Signed-off-by: Mason Fish <mason@looky.cloud>

--- src/js/flows/deleteSpace.js
+++ src/js/flows/deleteSpace.js
@@ -2,14 +2,13 @@
 import type {Thunk} from "../state/types"
 import Tab from "../state/Tab"
 import refreshSpaceNames from "./refreshSpaceNames"
-import {globalDispatch} from "../state/GlobalContext"
 
 const deleteSpace = (name: string): Thunk => (dispatch, getState) => {
   let state = getState()
   let zealot = Tab.getZealot(state)
 
   return zealot.spaces.delete(name).then(() => {
-    globalDispatch(refreshSpaceNames())
+    dispatch(refreshSpaceNames())
   })
 }
 
