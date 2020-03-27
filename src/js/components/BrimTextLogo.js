ade558b5fe1f48733c559fa3e951a7f6c5ee9966
James Kerr <jkerr838@gmail.com>
2020-03-27 14:00:19 -0700

    refactor: Move logo into its own component

---
+++ src/js/components/BrimTextLogo.js
@@ -0,0 +1,12 @@
+/* @flow */
+import React from "react"
+
+import LogoType from "../icons/LogoType"
+
+export default function BrimTextLogo() {
+  return (
+    <div className="brim-text-logo">
+      <LogoType />
+    </div>
+  )
+}
