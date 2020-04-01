5f7e24c1bac7ea15c320946df4c392e89dae0ab7
James Kerr <jkerr838@gmail.com>
2020-03-31 17:11:45 -0700

    refactor: Remove code to get downloads directory

--- src/js/lib/System.js
+++ src/js/lib/System.js
@@ -2,12 +2,6 @@
 
 import {remote} from "electron"
 
-import downloads from "./downloadsDir"
-
-export const downloadsDir = () => {
-  return downloads()
-}
-
 export function showContextMenu(template: Object) {
   new remote.Menu.buildFromTemplate(template).popup()
 }
