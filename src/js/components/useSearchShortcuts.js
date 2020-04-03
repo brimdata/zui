9678dda487ebafc448da0d72592cf686aedf850e
Mason Fish <mason@looky.cloud>
2020-04-03 13:04:04 -0700

    add flow for index adjustment
    Signed-off-by: Mason Fish <mason@looky.cloud>

--- src/js/components/useSearchShortcuts.js
+++ src/js/components/useSearchShortcuts.js
@@ -5,10 +5,13 @@ import {useEffect} from "react"
 import Mousetrap from "mousetrap"
 
 import Tabs from "../state/Tabs"
+import {adjustSelectedLogIndex} from "../flows/adjustSelectedLogIndex"
 
 export default function() {
   let dispatch = useDispatch()
   useEffect(() => {
+    Mousetrap.bind("down", () => dispatch(adjustSelectedLogIndex(1)))
+    Mousetrap.bind("up", () => dispatch(adjustSelectedLogIndex(-1)))
     Mousetrap.bind("mod+t", () => dispatch(Tabs.new()))
     Mousetrap.bind("mod+w", () => dispatch(Tabs.closeActive()))
     Mousetrap.bind("ctrl+tab", () => dispatch(Tabs.activateNext()))
