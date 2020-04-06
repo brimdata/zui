82c60ce42e124ef37d37660348b80e3e6c8fec26
Mason Fish <mason.e.fish@gmail.com>
2020-04-06 12:02:31 -0700

    PROD-1538 use arrow keys to select logs (#550)
    * add flow for index adjustment
    
    Signed-off-by: Mason Fish <mason@looky.cloud>
    
    * prevent default since scrolling already broken
    
    Signed-off-by: Mason Fish <mason@looky.cloud>
    
    Co-authored-by: Mason Fish <mason@looky.cloud>

--- src/js/components/useSearchShortcuts.js
+++ src/js/components/useSearchShortcuts.js
@@ -5,10 +5,19 @@ import {useEffect} from "react"
 import Mousetrap from "mousetrap"
 
 import Tabs from "../state/Tabs"
+import {adjustSelectedLogIndex} from "../flows/adjustSelectedLogIndex"
 
 export default function() {
   let dispatch = useDispatch()
   useEffect(() => {
+    Mousetrap.bind("down", (e) => {
+      e.preventDefault()
+      dispatch(adjustSelectedLogIndex(1))
+    })
+    Mousetrap.bind("up", (e) => {
+      e.preventDefault()
+      dispatch(adjustSelectedLogIndex(-1))
+    })
     Mousetrap.bind("mod+t", () => dispatch(Tabs.new()))
     Mousetrap.bind("mod+w", () => dispatch(Tabs.closeActive()))
     Mousetrap.bind("ctrl+tab", () => dispatch(Tabs.activateNext()))
