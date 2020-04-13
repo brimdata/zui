bab138f706c78a35288a5287e69f8d026d15d46b
James Kerr <jkerr838@gmail.com>
2020-04-13 15:07:27 -0700

    fix: Do not mutate the window state object

--- src/js/flows/saveWindowState.js
+++ src/js/flows/saveWindowState.js
@@ -6,13 +6,13 @@ import rpc from "../electron/rpc"
 
 export default (): Thunk => (_, getState) => {
   rpc.log("Saving window state")
-  let state = getState()
+  let state = {...getState()}
   // remove state pieces which we are not interested in persisting
   delete state.errors
   delete state.notice
   delete state.handlers
 
   return ipcRenderer
-    .invoke("windows:saveState", global.windowId, global.getState())
+    .invoke("windows:saveState", global.windowId, getState())
     .then(() => rpc.log("Window state saved"))
 }
