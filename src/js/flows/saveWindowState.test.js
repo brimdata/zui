d95f93129db2ce75478cbee04978826975f634e1
James Kerr <jkerr838@gmail.com>
2020-04-14 13:17:26 -0700

    fix: Do not mutate the window state object (#604)

---
+++ src/js/flows/saveWindowState.test.js
@@ -0,0 +1,12 @@
+/* @flow */
+import initTestStore from "../test/initTestStore"
+import saveWindowState from "./saveWindowState"
+
+test("does not mutate state", () => {
+  let store = initTestStore()
+  let prevState = {...store.getState()}
+
+  store.dispatch(saveWindowState())
+
+  expect(store.getState()).toEqual(prevState)
+})
