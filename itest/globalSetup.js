3be76276e99c53f1532d0042723feaf736f0371c
Alfred Landrum <alfred@leakybucket.org>
2020-04-16 16:50:27 -0700

    clear itest directory when itest starts

---
+++ itest/globalSetup.js
@@ -0,0 +1,11 @@
+/* @flow */
+
+// $FlowFixMe
+import "regenerator-runtime/runtime"
+
+import {LOGDIR} from "./lib/log"
+import {remove} from "fs-extra"
+
+module.exports = async() => {
+  return await remove(LOGDIR)
+}
