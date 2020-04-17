234629b1d4b820132e06eedfd8eab9f5eb7e8189
Alfred Landrum <alfred@leakybucket.org>
2020-04-16 20:48:40 -0700

    lint

--- itest/globalSetup.js
+++ itest/globalSetup.js
@@ -3,7 +3,7 @@
 // $FlowFixMe
 import "regenerator-runtime/runtime"
 
-import {remove,mkdirp} from "fs-extra"
+import {remove, mkdirp} from "fs-extra"
 import {itestDir} from "./lib/env"
 
 module.exports = async () => {
