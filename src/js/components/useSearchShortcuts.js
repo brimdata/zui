b2d1ee71539584934cf29c20a1ad3d9d8205b09f
Mason Fish <mason.e.fish@gmail.com>
2020-04-22 16:55:24 -0700

    throttle key events and prevent default (#670)
    Signed-off-by: Mason Fish <mason@looky.cloud>
    
    Co-authored-by: Mason Fish <mason@looky.cloud>

--- src/js/components/useSearchShortcuts.js
+++ src/js/components/useSearchShortcuts.js
@@ -5,21 +5,31 @@ import {useEffect} from "react"
 import Mousetrap from "mousetrap"
 
 import Tabs from "../state/Tabs"
+import throttle from "lodash/throttle"
 import {adjustSelectedLogIndex} from "../flows/adjustSelectedLogIndex"
 
 export default function() {
   let dispatch = useDispatch()
   useEffect(() => {
-    Mousetrap.bind("down", (e) => {
-      e.preventDefault()
-      dispatch(adjustSelectedLogIndex(1))
-    })
-    Mousetrap.bind("up", (e) => {
-      e.preventDefault()
-      dispatch(adjustSelectedLogIndex(-1))
-    })
+    Mousetrap.bind(
+      "down",
+      throttle((e) => {
+        e.preventDefault()
+        dispatch(adjustSelectedLogIndex(1))
+      }, 200)
+    )
+    Mousetrap.bind(
+      "up",
+      throttle((e) => {
+        e.preventDefault()
+        dispatch(adjustSelectedLogIndex(-1))
+      }, 200)
+    )
     Mousetrap.bind("mod+t", () => dispatch(Tabs.new()))
-    Mousetrap.bind("mod+w", () => dispatch(Tabs.closeActive()))
+    Mousetrap.bind("mod+w", (e) => {
+      e.preventDefault()
+      dispatch(Tabs.closeActive())
+    })
     Mousetrap.bind("ctrl+tab", () => dispatch(Tabs.activateNext()))
     Mousetrap.bind("ctrl+shift+tab", () => dispatch(Tabs.activatePrev()))
     for (let i = 0; i < 8; ++i) {
