295f1da81be365caba581b302422e9bb72b3e0e1
James Kerr <jkerr838@gmail.com>
2020-04-22 16:37:26 -0700

    feat: Show the ingest warnings in the status bar

--- src/js/components/StatusBar.js
+++ src/js/components/StatusBar.js
@@ -1,22 +1,60 @@
 /* @flow */
 
-import {useSelector} from "react-redux"
+import {isEmpty} from "lodash"
+import {useDispatch, useSelector} from "react-redux"
 import React from "react"
+import classNames from "classnames"
 
 import {isNumber} from "../lib/is"
-import PacketPostProgress from "./PacketPostProgress"
+import {reactElementProps} from "../test/integration"
+import Modal from "../state/Modal"
+import ProgressIndicator from "./ProgressIndicator"
 import Spaces from "../state/Spaces"
 import Tab from "../state/Tab"
+import Warning from "./icons/warning-sm.svg"
+import brim from "../brim"
 
 export default function StatusBar() {
+  let dispatch = useDispatch()
   let id = useSelector(Tab.clusterId)
   let name = useSelector(Tab.spaceName)
+  let space = useSelector(Tab.space)
   let value = useSelector(Spaces.getIngestProgress(id, name))
-  if (!isNumber(value)) return null
+  let warnings = useSelector(Spaces.getIngestWarnings(id, name))
+  if (!isNumber(value) && isEmpty(warnings)) return null
+  let s = brim.space(space)
+
+  function onWarningsClick() {
+    dispatch(Modal.show("ingest-warnings"))
+  }
+
+  function getMessage() {
+    if (s.queryable() && s.ingesting()) {
+      return "Partial data available while loading…"
+    }
+  }
 
   return (
     <div className="status-bar">
-      <PacketPostProgress percent={value} />
+      <div
+        className="packet-post-progress"
+        {...reactElementProps("ingestProgress")}
+      >
+        <label>{getMessage()}</label>
+        <div className="group">
+          {s.ingesting() && <ProgressIndicator percent={value} />}
+          {!s.ingesting() && s.empty() && (
+            <label>Ingest failed with warnings.</label>
+          )}
+          <div
+            className={classNames("warnings", {disabled: isEmpty(warnings)})}
+            onClick={onWarningsClick}
+          >
+            <Warning />
+            <label>{warnings.length}</label>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
