dd381fa99031aeddff0ee388905ca94b67e886ca
Mason Fish <mason@looky.cloud>
2020-04-24 16:06:22 -0700

    use navAnimation
    Signed-off-by: Mason Fish <mason@looky.cloud>

--- src/js/components/LogDetailsWindow/index.js
+++ src/js/components/LogDetailsWindow/index.js
@@ -12,6 +12,7 @@ import Tab from "../../state/Tab"
 import HistoryButtons from "../common/HistoryButtons"
 import PacketsButton from "../PacketsButton"
 import menu from "../../electron/menu"
+import NavAnimation from "../LogDetails/NavAnimation"
 
 export default function LogDetailsWindow() {
   let dispatch = useDispatch()
@@ -19,37 +20,43 @@ export default function LogDetailsWindow() {
   const nextExists = useSelector(LogDetails.getHistory).nextExists()
   const log = useSelector(LogDetails.build)
   const space = useSelector(Tab.space)
+  const isGoingBack = useSelector(LogDetails.getIsGoingBack)
 
   return (
-    <div className="log-detail-window">
-      <PaneHeader>
-        <Left>
-          <HistoryButtons
-            prevExists={prevExists}
-            nextExists={nextExists}
-            backFunc={() => dispatch(LogDetails.back())}
-            forwardFunc={() => dispatch(LogDetails.forward())}
-          />
-        </Left>
-        <Center>
-          <PaneTitle>Log details for space: {space.name}</PaneTitle>
-        </Center>
-        <Right>
-          <PacketsButton label={false} id="detail-window-packets" />
-        </Right>
-      </PaneHeader>
-      <div className="log-detail-container">
-        <div className="log-detail-body-wrapper">
-          <div className="log-detail-body">
-            <FieldsPanel log={log} contextMenu={menu.detailFieldContextMenu} />
-            {log.correlationId() && <UidPanel log={log} />}
-            <ConnPanel log={log} contextMenu={menu.detailFieldContextMenu} />
-            {log.getString("md5") && (
-              <Md5Panel log={log} contextMenu={menu.detailFieldContextMenu} />
-            )}
+    <NavAnimation log={log} prev={isGoingBack}>
+      <div className="log-detail-window">
+        <PaneHeader>
+          <Left>
+            <HistoryButtons
+              prevExists={prevExists}
+              nextExists={nextExists}
+              backFunc={() => dispatch(LogDetails.back())}
+              forwardFunc={() => dispatch(LogDetails.forward())}
+            />
+          </Left>
+          <Center>
+            <PaneTitle>Log details for space: {space.name}</PaneTitle>
+          </Center>
+          <Right>
+            <PacketsButton label={false} id="detail-window-packets" />
+          </Right>
+        </PaneHeader>
+        <div className="log-detail-container">
+          <div className="log-detail-body-wrapper">
+            <div className="log-detail-body">
+              <FieldsPanel
+                log={log}
+                contextMenu={menu.detailFieldContextMenu}
+              />
+              {log.correlationId() && <UidPanel log={log} />}
+              <ConnPanel log={log} contextMenu={menu.detailFieldContextMenu} />
+              {log.getString("md5") && (
+                <Md5Panel log={log} contextMenu={menu.detailFieldContextMenu} />
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </NavAnimation>
   )
 }
