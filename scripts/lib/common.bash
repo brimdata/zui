6453cc021e6b7310f73127aacfe8c166a479b552
Michael Brown <mikeb@apache.org>
2020-04-28 16:40:42 -0700

    test fix: allow for possibility of dimg mounted volume to disappear

--- scripts/lib/common.bash
+++ scripts/lib/common.bash
@@ -5,7 +5,9 @@ function retry_until_success {
     # Retry until a command succeeds.
     # Args:
     # First positional: Number of attempts to try.
-    # Second positionals: Delay in seconds between attempts.
+    # Second positional: Delay in seconds between attempts.
+    # Other positionals: Command "List" as described by bash(1) man
+    # page. Compound commands won't work. Write logic into a function.
     # Example usage tries 5 times waiting 1 second between attempts to
     # see if a file foo exists.
     # retry_until_success 5 1 test -e foo
@@ -23,4 +25,16 @@ function retry_until_success {
         fi
         sleep "${delay}"
     done
+}
+
+function umount_macos_ci_dimg {
+    # https://github.com/brimsec/brim/issues/690
+    # It's possible for hdiutil to fail with an error code, yet
+    # subsequently the volume disappears. This is a function so that
+    # its logic can be used with retry_until_success.
+    local vol="$1"
+    if [ -e "${vol}" ]
+    then
+        hdiutil detach "${vol}"
+    fi
 }
