f6675cfa718673db8b73b8d28dfc68a956c6c72b
James Kerr <jkerr838@gmail.com>
2020-04-29 12:13:41 -0700

    feat: Add useThrottle to slow fast changing values

---
+++ src/js/components/hooks/useThrottle.js
@@ -0,0 +1,32 @@
+/* @flow */
+import {useEffect, useRef, useState} from "react"
+
+export default function(value: *, wait: number) {
+  let [state, setState] = useState(value)
+  let timeout = useRef(null)
+  let pending = useRef(false)
+  let nextValue = useRef(null)
+
+  useEffect(() => {
+    if (!timeout.current) {
+      setState(value)
+
+      let callback = () => {
+        if (pending.current) {
+          pending.current = false
+          setState(nextValue.current)
+          timeout.current = setTimeout(callback, wait)
+        } else {
+          timeout.current = undefined
+        }
+      }
+
+      timeout.current = setTimeout(callback, wait)
+    } else {
+      pending.current = true
+      nextValue.current = value
+    }
+  }, [value])
+
+  return state
+}
