0e012d7671631cadf94af6683f96f4b3d963d7fd
James Kerr <jkerr838@gmail.com>
2020-05-04 13:58:27 -0700

    feat: Set user tasks on windows

---
+++ src/js/electron/userTasks.js
@@ -0,0 +1,14 @@
+/* @flow */
+
+export default function(app: *) {
+  app.setUserTasks([
+    {
+      program: process.execPath,
+      arguments: "--new-window",
+      iconPath: process.execPath,
+      iconIndex: 0,
+      title: "New Window",
+      description: "Create a new window"
+    }
+  ])
+}
