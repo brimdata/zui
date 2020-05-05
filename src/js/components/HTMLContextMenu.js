84cb44327a191fccec730dc1aedf2da94f47b67b
James Kerr <jkerr838@gmail.com>
2020-05-05 14:54:36 -0700

    feat: Cleaned up code and added comments

--- src/js/components/HTMLContextMenu.js
+++ src/js/components/HTMLContextMenu.js
@@ -1,55 +1,65 @@
-/* @flow */
-import React, {useEffect, useState} from "react"
+/* @flow
+
+  This is a component only used for integration tests. Since spectron cannot
+  interact with the native right click menus, this component creates an
+  html version of the right click menus with the same labels and click handlers
+  that the native menu will recieve.
+
+  Usually, when a native menu is clicked, it sends an event to the originating
+  renderer process. This component simulates that simply by emiting the event
+  here in the renderer. See the stubIpcSend() function below.
+*/
+
+import React, {useState} from "react"
 import ReactDOM from "react-dom"
 import classNames from "classnames"
 
 import {ipcRenderer} from "electron"
 
 import lib from "../lib"
+import useEventListener from "./hooks/useEventListener"
 
 export default function HTMLContextMenu() {
   let [template, setTemplate] = useState(null)
-
-  useEffect(() => {
-    const name = "nativeContextMenu"
-    const handler = (e) => setTemplate(e.detail)
-
-    // $FlowFixMe
-    document.addEventListener(name, handler, false)
-    document.addEventListener("click", () => setTemplate(null), false)
-    // $FlowFixMe
-    return () => document.removeEventListener(name, handler, false)
-  }, [])
+  const openMenu = (e) => setTemplate(e.detail)
+  useEventListener(document, "click", () => setTemplate(null), [])
+  useEventListener(document, "nativeContextMenu", openMenu, [])
 
   if (!template) return null
-
-  function onClick(item) {
-    item.click(item, {
-      webContents: {
-        send: (name, ...args) => {
-          ipcRenderer.emit(name, null, ...args)
-        }
-      }
-    })
-  }
   return ReactDOM.createPortal(
     <div className="html-context-menu">
       <ul>
-        {template.map((item, i) => {
-          if (item.type === "separator") return <hr key={i} />
-          else
-            return (
-              <li
-                key={i}
-                onClick={() => onClick(item)}
-                className={classNames({disabled: !item.enabled})}
-              >
-                {item.label}
-              </li>
-            )
-        })}
+        {template.map((item, i) => (
+          <HTMLMenuItem item={item} key={i} />
+        ))}
       </ul>
     </div>,
     lib.doc.id("tooltip-root")
   )
 }
+
+function HTMLMenuItem({item}) {
+  if (item.type === "separator") {
+    return <hr />
+  } else {
+    return (
+      <li
+        onClick={() => item.click(item, stubbedBrowserWindow)}
+        className={classNames({disabled: !item.enabled})}
+      >
+        {item.label}
+      </li>
+    )
+  }
+}
+
+function stubIpcSend(name, ...args) {
+  /*
+    Since ipcRendere inherits from EventEmitter, we can simply emit this event
+    right here in the renderer to simulate it being sent over from the main
+    processes like it does with the native context menu.
+  */
+  ipcRenderer.emit(name, null /* event */, ...args)
+}
+
+const stubbedBrowserWindow = {webContents: {send: stubIpcSend}}
