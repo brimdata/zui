a8cf6673ca1b75390b7659da104aadea61559e16
James Kerr <jkerr838@gmail.com>
2020-05-04 17:20:19 -0700

    fix: Use a better error messages for ingest import errors
    This would happen during ingest if someone closes their laptop
    then re-opens it, or if the computer falls asleep.
    
    If the computer sleeps before the first IngestStatus is returned,
    the error message is "Failed to fetch". If it happens after the
    first status is returned, the error message is "network error".
    
    We now look for both of these and present the importInterupt error
    message.

--- src/js/errors/index.js
+++ src/js/errors/index.js
@@ -8,5 +8,10 @@ export default {
   spaceDeleted: (name: string) => ({
     type: "SpaceDeletedError",
     message: `The space "${name}" previously on this tab has been deleted.`
+  }),
+  importInterrupt: () => ({
+    type: "ImportInterruptError",
+    message: "The import was interrupted.",
+    details: ["To prevent this, keep your computer awake during the import."]
   })
 }
