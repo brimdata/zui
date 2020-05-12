352e79a91c1aaf8acd24b6601bd4d70ba309d8cd
James Kerr <jkerr838@gmail.com>
2020-05-12 15:54:45 -0700

    feat: Write to Prefs.zeekRunner

--- src/js/state/Prefs/types.js
+++ src/js/state/Prefs/types.js
@@ -1,11 +1,15 @@
 /* @flow */
 
-export type PrefsState = {
+export type PrefsState = {|
   jsonTypeConfig: string,
-  timeFormat: string
-}
+  timeFormat: string,
+  zeekRunner: string
+|}
 
-export type PrefsAction = PREFS_JSON_TYPES_CONFIG_SET | PREFS_TIME_FORMAT_SET
+export type PrefsAction =
+  | PREFS_JSON_TYPES_CONFIG_SET
+  | PREFS_TIME_FORMAT_SET
+  | PREFS_ZEEK_RUNNER_SET
 
 export type PREFS_JSON_TYPES_CONFIG_SET = {
   type: "PREFS_JSON_TYPES_CONFIG_SET",
@@ -15,4 +19,9 @@ export type PREFS_JSON_TYPES_CONFIG_SET = {
 export type PREFS_TIME_FORMAT_SET = {
   type: "PREFS_TIME_FORMAT_SET",
   format: string
+}
+
+export type PREFS_ZEEK_RUNNER_SET = {
+  type: "PREFS_ZEEK_RUNNER_SET",
+  zeekRunner: string
 }
