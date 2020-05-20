d35738c1beb028e564b05af662c4f495aab34ade
James Kerr <jkerr838@gmail.com>
2020-05-20 15:15:06 -0700

    feat: setTimeout function that cleans up on its own

---
+++ src/js/components/hooks/useSetTimeout.js
@@ -0,0 +1,16 @@
+/* @flow */
+import {useEffect, useRef} from "react"
+
+export default function useSetTimeout() {
+  let timers = useRef([])
+
+  useEffect(() => {
+    return () => timers.current.forEach(clearTimeout)
+  }, [])
+
+  return (fn: Function, ms: number) => {
+    let id = setTimeout(fn, ms)
+    timers.current.push(id)
+    return id
+  }
+}
