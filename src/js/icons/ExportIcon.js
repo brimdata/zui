b0169c977e94ff5118bf50d1ad7340e8bdb18579
James Kerr <jkerr838@gmail.com>
2020-05-20 15:13:42 -0700

    feat: Add export icon

---
+++ src/js/icons/ExportIcon.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+import React from "react"
+
+import ExportSvg from "../../static/icons/export.svg"
+
+export default function ExportIcon(props: *) {
+  return <ExportSvg className="export-icon" {...props} />
+}
