c3bf3eb3ef729d9f99955bd4363472c7278b7dc7
Mason Fish <mason@looky.cloud>
2020-05-26 15:17:49 -0700

    migrate investigation spaceId
    Signed-off-by: Mason Fish <mason@looky.cloud>

---
+++ src/js/state/migrations/202005261448_setInvestigationSpaceToNameAndId.js
@@ -0,0 +1,21 @@
+/* @flow */
+
+import {getAllStates} from "../../test/helpers/getTestState"
+
+export default function setInvestigationSpaceToNameAndId(state: *) {
+  for (const s of getAllStates(state)) {
+    if (!s.investigation) continue
+
+    for (const {search} of s.investigation) {
+      const oldName = search.space
+      if (oldName) {
+        search.spaceName = oldName
+        search.spaceId = oldName
+      }
+
+      delete search.space
+    }
+  }
+
+  return state
+}
