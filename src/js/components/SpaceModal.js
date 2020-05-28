603d47c58e6b3148897a47c822f3578548b62c5a
Mason Fish <mason@looky.cloud>
2020-05-28 15:31:54 -0700

    refactor space modal to use modal buttons
    Signed-off-by: Mason Fish <mason@looky.cloud>

--- src/js/components/SpaceModal.js
+++ src/js/components/SpaceModal.js
@@ -1,34 +1,25 @@
 /* @flow */
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 import ModalBox from "./ModalBox/ModalBox"
 import {useDispatch, useSelector} from "react-redux"
 import {reactElementProps} from "../test/integration"
 import Modal from "../state/Modal"
 import Spaces from "../state/Spaces"
-import {Input, InputSubmit} from "./form/Inputs"
-import Form from "./form/Form"
+import {Input} from "./form/Inputs"
 import renameSpace from "../flows/renameSpace"
 
 export default function SpaceModal() {
-  return (
-    <ModalBox
-      name="space"
-      title=""
-      buttons={[]}
-      {...reactElementProps("spaceModal")}
-    >
-      <SpaceModalContents />
-    </ModalBox>
-  )
-}
-
-const SpaceModalContents = () => {
-  const {spaceId, clusterId} = useSelector(Modal.getArgs)
+  const args = useSelector(Modal.getArgs) || {}
+  const {clusterId, spaceId} = args
   const space = useSelector(Spaces.get(clusterId, spaceId))
   const {name} = space || {name: ""}
   const [nameInput, setNameInput] = useState(name)
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    setNameInput(name)
+  }, [name])
 
   const onSubmit = () => {
     dispatch(renameSpace(clusterId, spaceId, nameInput))
@@ -36,17 +27,28 @@ const SpaceModalContents = () => {
   }
 
   return (
-    <div>
-      <Form onSubmit={onSubmit}>
-        <Input
-          label="Space Name"
-          value={nameInput}
-          onChange={(e) => {
-            setNameInput(e.target.value)
-          }}
-        />
-        <InputSubmit value={"Rename"} />
-      </Form>
+    <ModalBox
+      name="space"
+      title=""
+      buttons={[{label: "OK", click: onSubmit}]}
+      className="space-modal"
+      {...reactElementProps("spaceModal")}
+    >
+      <SpaceModalContents value={nameInput} onChange={setNameInput} />
+    </ModalBox>
+  )
+}
+
+const SpaceModalContents = ({value, onChange}) => {
+  return (
+    <div className="space-modal-contents">
+      <Input
+        label="Space Name"
+        value={value}
+        onChange={(e) => {
+          onChange(e.target.value)
+        }}
+      />
     </div>
   )
 }
