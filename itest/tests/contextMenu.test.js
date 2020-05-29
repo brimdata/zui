4570277c539c6daab6c3c7703731e89217272ab4
Michael Brown <mikeb@apache.org>
2020-05-29 13:21:18 -0700

    start comprehensive context menu tests (fixes #804) (#828)
    * fix: generate xpaths that support either quote
    
    * feat: table-driven context menu test with multiple values and types
    
    * add a test to ensure ingest
    
    * test fix: use fewer rows and hope windows is happy
    
    * fix: move testId decl

--- itest/tests/contextMenu.test.js
+++ itest/tests/contextMenu.test.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import {basename} from "path"
+import {sprintf} from "sprintf-js"
 
 import {
   click,
@@ -17,14 +18,40 @@ import {
 import {handleError, stdTest} from "../lib/jest.js"
 import {selectors} from "../../src/js/test/integration"
 
-describe("Test search mods via right-clicks", () => {
+// Fields we can test in types.tsv
+const FIELDS = ["scalar", "record.scalar"]
+// Brim character that represents an unset zng value. It must be
+// handled specially as it can be a string value or the unset
+// representation. That representation is only used to find a cell, or
+// to exclude the actual character from search results.
+const UNSET = "⦻"
+const STRINGS = [
+  "mystr",
+  "\\x2d",
+  "\\x09",
+  '"',
+  "'",
+  ",",
+  ";",
+  "∫£œßü™",
+  '"mystr"',
+  "'mystr'",
+  "*",
+  '"*"',
+  "1.1.1.1",
+  " ",
+  "null",
+  "⦻" // The actual character, not unset.
+]
+const ADDRS = ["1.1.1.1", "fe80::58d2:2d09:e8cb:a8ad", "::"]
+
+describe("type-wise Filter = value searches", () => {
   let app
-  let testIdx = 0
 
   beforeAll(async () => {
-    app = newAppInstance(basename(__filename), ++testIdx)
+    app = newAppInstance(basename(__filename) + "-types", 0)
     await startApp(app)
-    await ingestFile(app, "sample.pcap")
+    await ingestFile(app, "types.tsv")
     await setSpan(app, "Whole Space")
   })
 
@@ -34,24 +61,62 @@ describe("Test search mods via right-clicks", () => {
     }
   })
 
-  beforeEach(async () => {
-    await writeSearch(app, "")
-    await startSearch(app)
-    await waitForResults(app)
-  })
-
-  stdTest("count by _path; sort asc", (done) => {
-    rightClick(app, selectors.viewer.resultCellContaining("capture_loss"))
+  stdTest("FilterEq ensure ingest", (done) => {
+    writeSearch(app, `* | count() by _path | sort _path`)
       .then(async () => {
-        await click(app, selectors.viewer.contextMenuItem("Count by field"))
-        await rightClick(app, selectors.viewer.resultCellContaining("dns"))
-        await click(app, selectors.viewer.contextMenuItem("Sort A...Z"))
-        let results = await searchDisplay(app)
-        expect(results).toMatchSnapshot()
+        await startSearch(app)
+        expect(await searchDisplay(app)).toMatchSnapshot()
         done()
       })
       .catch((err) => {
         handleError(app, err, done)
       })
   })
+
+  const run = (path: string, values: string[]) => {
+    let testIdx = 0
+    FIELDS.forEach((fieldName) => {
+      values.forEach((s) => {
+        let testId = sprintf("%04d", testIdx++)
+        stdTest(`FilterEq${testId}: ${path} ${fieldName}="${s}"`, (done) => {
+          writeSearch(
+            app,
+            `_path=${path} ${fieldName}!=null | cut id, ${fieldName}`
+          )
+            .then(async () => {
+              await startSearch(app)
+              await waitForResults(app)
+              rightClick(app, selectors.viewer.resultCellContaining(s))
+              await click(
+                app,
+                selectors.viewer.contextMenuItem("Filter = value")
+              )
+              expect(await searchDisplay(app)).toMatchSnapshot()
+              done()
+            })
+            .catch((err) => {
+              handleError(app, err, done)
+            })
+        })
+      })
+    })
+  }
+  run("string", STRINGS)
+
+  stdTest(`FilterEq unset/${UNSET} string`, (done) => {
+    writeSearch(app, `_path=string | cut id, scalar | sort -r id | head 10`)
+      .then(async () => {
+        await startSearch(app)
+        await waitForResults(app)
+        rightClick(app, selectors.viewer.resultCellContaining(UNSET))
+        await click(app, selectors.viewer.contextMenuItem("Filter = value"))
+        expect(await searchDisplay(app)).toMatchSnapshot()
+        done()
+      })
+      .catch((err) => {
+        handleError(app, err, done)
+      })
+  })
+
+  run("addr", ADDRS)
 })
