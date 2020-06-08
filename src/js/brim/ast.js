d3415f5bd82cbc5c685c2239dfbd8a91a1b36755
Henri DF <henridf@gmail.com>
2020-06-08 15:14:44 +0200

    Update for GroupbyProc ast format change (#863)

--- src/js/brim/ast.js
+++ src/js/brim/ast.js
@@ -11,7 +11,8 @@ export default function ast(tree: Object) {
     },
     groupByKeys() {
       let g = this.proc("GroupByProc")
-      return g ? g.keys : []
+      let keys = g ? g.keys : []
+      return keys.map<string>((k) => k.target)
     },
     proc(name: string) {
       return getProcs(tree).find((p) => p.op === name)
