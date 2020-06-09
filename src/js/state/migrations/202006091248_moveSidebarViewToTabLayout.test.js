bf7d01a25a1cdf0513322bda6182242b4147b8e5
Mason Fish <mason@looky.cloud>
2020-06-09 14:34:35 -0700

    fix flow
    Signed-off-by: Mason Fish <mason@looky.cloud>

--- src/js/state/migrations/202006091248_moveSidebarViewToTabLayout.test.js
+++ src/js/state/migrations/202006091248_moveSidebarViewToTabLayout.test.js
@@ -7,8 +7,10 @@ test("migrating 202006091248_moveSidebarViewToTabLayout", () => {
   let prev = getTestState("v0.9.1")
 
   let next = migrate(prev)
-  for (const win of Object.values(next.windows)) {
-    for (const {layout} of win.state.tabs.data) {
+
+  // $FlowFixMe
+  for (const {state} of Object.values(next.windows)) {
+    for (const {layout} of state.tabs.data) {
       expect(layout.investigationView).toEqual("linear")
       expect(layout.leftSidebarIsOpen).toEqual(false)
       expect(layout.leftSidebarWidth).toEqual(350)
