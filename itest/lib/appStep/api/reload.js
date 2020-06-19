c712675f73b877d0615a0e108d7d478ae5c09bc8
Michael Brown <mikeb@apache.org>
2020-06-19 11:40:01 -0700

    fix: retry browser window reload (#882)

--- itest/lib/appStep/api/reload.js
+++ itest/lib/appStep/api/reload.js
@@ -2,6 +2,12 @@
 import {Application} from "spectron"
 
 import logStep from "../util/logStep"
+import {retry} from "../../control"
 
 export default (app: Application) =>
-  logStep("app reload", () => app.browserWindow.reload())
+  // Issue a browser window reload(). This is flaky in the Spectron,
+  // Chromedriver stack, but appears be contained within the method.
+  // It seems safe to retry.
+  // https://github.com/brimsec/brim/issues/878
+  // https://github.com/electron-userland/spectron/issues/493
+  logStep("app reload", () => retry(() => app.browserWindow.reload(), 60, 1000))
