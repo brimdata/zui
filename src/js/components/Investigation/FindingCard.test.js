ae3bb6d34d1944743a80caa3b7183763b68c88d2
James Kerr <jkerr838@gmail.com>
2020-07-27 12:04:44 -0700

    Fix deleting a history item issuing a search

---
+++ src/js/components/Investigation/FindingCard.test.js
@@ -0,0 +1,43 @@
+/* @flow */
+
+import React from "react"
+
+import FindingCard from "./FindingCard"
+import brim from "../../brim"
+import loginTo from "../../test/helpers/loginTo"
+import provide from "../../test/helpers/provide"
+
+let store
+beforeEach(async () => {
+  const setup = await loginTo("cluster1", "space1")
+  store = setup.store
+})
+
+function getActionTypes() {
+  return store.getActions().map((a) => a.type)
+}
+
+const finding = {
+  ts: brim.time().toTs(),
+  search: {
+    program: "finding card test",
+    pins: [],
+    spanArgs: [brim.time().toTs(), brim.time().toTs()],
+    spaceId: "1",
+    spaceName: "space1"
+  }
+}
+
+test("Clicking the history submits the search", () => {
+  const el = provide(store, <FindingCard finding={finding} />)
+  store.clearActions()
+  el.simulate("click")
+  expect(getActionTypes()).toContain("SEARCH_BAR_SUBMIT")
+})
+
+test("Clicking remove does not submit the search", () => {
+  const el = provide(store, <FindingCard finding={finding} />)
+  store.clearActions()
+  el.find(".remove-button").simulate("click")
+  expect(getActionTypes()).not.toContain("SEARCH_BAR_SUBMIT")
+})
