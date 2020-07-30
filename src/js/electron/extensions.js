7a41d07c20c673e7fc501be7538173f186b34f53
James Kerr <jkerr838@gmail.com>
2020-07-30 11:26:49 -0700

    See if windows is breaking due to devtools

--- src/js/electron/extensions.js
+++ src/js/electron/extensions.js
@@ -1,13 +1,13 @@
 /* @flow */
-import installExtension, {
-  REACT_DEVELOPER_TOOLS,
-  REDUX_DEVTOOLS
-} from "electron-devtools-installer"
+// import installExtension, {
+// REACT_DEVELOPER_TOOLS,
+// REDUX_DEVTOOLS
+// } from "electron-devtools-installer"
 
-import log from "electron-log"
+// import log from "electron-log"
 
 export function installExtensions() {
-  return installExtension([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS])
-    .then(() => log.info("Devtools loaded"))
-    .catch((err) => log.error("Devtools error occurred: ", err))
+  // return installExtension([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS])
+  // .then(() => log.info("Devtools loaded"))
+  // .catch((err) => log.error("Devtools error occurred: ", err))
 }
