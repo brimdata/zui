3c84480e48680f2993b2477d9a9917f420f8587a
James Kerr <jkerr838@gmail.com>
2020-07-30 14:33:41 -0700

    If we are in CI, do not install devtools

--- src/js/electron/extensions.js
+++ src/js/electron/extensions.js
@@ -1,13 +1,14 @@
 /* @flow */
-// import installExtension, {
-// REACT_DEVELOPER_TOOLS,
-// REDUX_DEVTOOLS
-// } from "electron-devtools-installer"
+import installExtension, {
+  REACT_DEVELOPER_TOOLS,
+  REDUX_DEVTOOLS
+} from "electron-devtools-installer"
 
-// import log from "electron-log"
+import log from "electron-log"
 
 export function installExtensions() {
-  // return installExtension([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS])
-  // .then(() => log.info("Devtools loaded"))
-  // .catch((err) => log.error("Devtools error occurred: ", err))
+  if (process.env.CI === "true") return
+  return installExtension([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS])
+    .then(() => log.info("Devtools loaded"))
+    .catch((err) => log.error("Devtools error occurred: ", err))
 }
