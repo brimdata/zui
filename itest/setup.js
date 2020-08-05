42da2eccda8a1f3bc6fb8529aa77577036f6ad78
James Kerr <jkerr838@gmail.com>
2020-08-04 17:47:10 -0700

    Change the ENV var for skipping devtools

--- itest/setup.js
+++ itest/setup.js
@@ -7,5 +7,6 @@ import "regenerator-runtime/runtime"
 
 // This signals the path setup in main.js to not override the userData
 // directory it sees on startup, so that we can store complete application
-// state and logs for each individual integration test.
+// state and logs for each individual integration test. It also signals not
+// try to install the dev tools.
 process.env.BRIM_ITEST = "true"
