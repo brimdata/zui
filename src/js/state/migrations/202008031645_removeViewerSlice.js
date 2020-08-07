9947e2decf2e501ef0f716abd6883d26459709be
James Kerr <jkerr838@gmail.com>
2020-08-07 09:55:11 -0700

    No longer persist the viewer slice of a tab

---
+++ src/js/state/migrations/202008031645_removeViewerSlice.js
@@ -0,0 +1,13 @@
+/* @flow */
+
+import {getAllStates} from "../../test/helpers/getTestState"
+
+export default function removeViewerSlice(state: *) {
+  for (const s of getAllStates(state)) {
+    if (!s.tabs) continue
+    for (const t of s.tabs.data) {
+      delete t.viewer
+    }
+  }
+  return state
+}
---
+++ src/js/state/migrations/202008031645_removeViewerSlice.test.js
@@ -0,0 +1,19 @@
+/* @flow */
+
+import getTestState from "../../test/helpers/getTestState"
+import migrate from "./202008031645_removeViewerSlice"
+
+test("migrating 202008031645_removeViewerSlice", () => {
+  let {data} = getTestState("v0.14.0")
+
+  const next = migrate(data)
+
+  const windows = Object.values(next.windows)
+
+  for (const win of windows) {
+    // $FlowFixMe
+    for (const tab of win.state.tabs.data) {
+      expect(tab.viewer).toBe(undefined)
+    }
+  }
+})
