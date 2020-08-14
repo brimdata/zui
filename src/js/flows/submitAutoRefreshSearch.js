b29adb29eba748c802964234a4548188978ff6c6
Mason Fish <mason@looky.cloud>
2020-08-14 09:19:57 -0700

    another idea
    Signed-off-by: Mason Fish <mason@looky.cloud>

--- src/js/flows/submitAutoRefreshSearch.js
+++ src/js/flows/submitAutoRefreshSearch.js
@@ -6,6 +6,11 @@ import Tab from "../state/Tab"
 import Tabs from "../state/Tabs"
 import Viewer from "../state/Viewer"
 import executeHistogramSearch from "./executeHistogramSearch"
+import Columns from "../state/Columns"
+import zngToZeekTypes from "../brim/flatRecordsBuffer/zngToZeekTypes"
+import nestedRecord from "../brim/flatRecordsBuffer/nestedRecord"
+import {isArray} from "../lib/is"
+import executeTableSearch from "./executeTableSearch"
 
 export default function submitAutoRefreshSearch(): Thunk {
   return function(dispatch, getState, {zealot}) {
@@ -22,15 +27,53 @@ export default function submitAutoRefreshSearch(): Thunk {
     switch (args.type) {
       case "analytics":
       case "zoom":
-        break
+        return dispatch(executeTableSearch(tabId, args, true))
       default:
         dispatch(executeHistogramSearch(tabId, args, prevArgs))
+        return dispatch(executeTableSearch(tabId, args, true))
     }
 
-    zealot.search(tableProgram, {from, to, spaceId}).then((stream) => {
-      stream.flatRecords().then((records) => {
-        dispatch(Viewer.setRecords(tabId, records))
-      })
-    })
+    // getAndSetRecords(zealot, tableProgram, {from, to, spaceId}).then(
+    //   ({records, columns}) => {
+    //     dispatch(Viewer.setRecords(tabId, records))
+    //     dispatch(Viewer.updateColumns(tabId, columns))
+    //     dispatch(Columns.touch(columns))
+    //   }
+    // )
   }
 }
+
+// const getAndSetRecords = async (zealot, program, args) => {
+//   const stream = await zealot.search(program, args)
+//
+//   const collectedRecords = []
+//   const types = {}
+//   for await (let payload of stream) {
+//     const {records} = payload
+//     if (!records) {
+//       continue
+//     }
+//     const newRecords = records.map((r) => {
+//       if (r.type) types[r.id] = zngToZeekTypes(r.type)
+//       return nestedRecord(r.values, types[r.id]).flatten()
+//     })
+//     collectedRecords.push(...newRecords)
+//   }
+//
+//   const columns = Object.keys(types).reduce((all, id) => {
+//     all[id] = flattenType(types[id])
+//     return all
+//   }, {})
+//
+//   return {collectedRecords, columns}
+// }
+//
+// function flattenType(descriptor, prefix = "") {
+//   return descriptor.reduce((flat, {name, type}) => {
+//     let cols = isArray(type)
+//       ? flattenType(type, `${prefix}${name}.`)
+//       : [{name: prefix + name, type}]
+//
+//     return flat.concat(cols)
+//   }, [])
+// }
