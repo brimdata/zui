549a9e826c8c4e63a29c206bcaaf686a2cb404e0
Mason Fish <mason@looky.cloud>
2020-08-21 09:54:53 -0700

    move beforeunload handler to just search
    Signed-off-by: Mason Fish <mason@looky.cloud>

--- src/js/electron/tron/window.js
+++ src/js/electron/tron/window.js
@@ -17,7 +17,7 @@ export default function window(name: WindowName, params: WindowParams) {
     case "about":
       return aboutWindow()
     case "detail":
-      return detailWindow({...params, name})
+      return detailWindow({...params})
     default:
       throw new Error(`Unknown window name: ${name}`)
   }
@@ -71,7 +71,7 @@ function aboutWindow() {
 }
 
 function detailWindow(params) {
-  let {size, position, query, id, name} = params
+  let {size, position, query, id} = params
   let win = new BrowserWindow({
     resizable: true,
     width: 360,
@@ -90,7 +90,7 @@ function detailWindow(params) {
     win.center()
   }
 
-  win.loadFile("detail.html", {query: {...query, id, name}})
+  win.loadFile("detail.html", {query: {...query, id}})
 
   return win
 }
--- src/js/initializers/initCleanup.js
+++ src/js/initializers/initCleanup.js
@@ -3,14 +3,7 @@ import {ipcRenderer} from "electron"
 
 import type {Store} from "../state/types"
 import closeWindow from "../flows/closeWindow"
-import refreshWindow from "../flows/refreshWindow"
-import getUrlSearchParams from "../lib/getUrlSearchParams"
 
 export default function(store: Store) {
   ipcRenderer.on("close", () => store.dispatch(closeWindow()))
-  global.onbeforeunload = () => {
-    const {name} = getUrlSearchParams()
-    if (name === "detail") return
-    store.dispatch(refreshWindow())
-  }
 }
