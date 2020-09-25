dd52330c2724a756fd89b7b107d0ec8976662f43
Mason Fish <mason@looky.cloud>
2020-09-25 11:52:53 -0700

    refactor logic for initZQDConnection
    Signed-off-by: Mason Fish <mason@looky.cloud>

--- src/js/initializers/initZqdConnection.ts
+++ src/js/initializers/initZqdConnection.ts
@@ -4,34 +4,35 @@ import Clusters from "../state/Clusters"
 import Current from "../state/Current"
 import getUrlSearchParams from "../lib/getUrlSearchParams"
 import refreshSpacesForAllConnections from "../flows/refreshSpacesForAllConnections"
-import {globalDispatch} from "../state/GlobalContext"
+
+const setupConnection = (host, port) => (dispatch, _, {globalDispatch}) => {
+  const cluster = {
+    host,
+    port,
+    id: [host, port].join(":"),
+    username: "",
+    password: ""
+  }
+  dispatch(Clusters.add(cluster))
+  globalDispatch(Clusters.add(cluster))
+  dispatch(Current.setConnectionId(cluster.id))
+}
 
 export default async function(store: Store) {
   const {space, host, port, id} = getUrlSearchParams()
   global.windowId = id
 
-  const lastConn = Current.getConnection(store.getState())
-  if ((port && host) || !lastConn) {
-    const clusterHost = host || "localhost"
-    const clusterPort = port || "9867"
-    const clusterId = `${clusterHost}:${clusterPort}`
-    const cluster = {
-      id: clusterId,
-      host: clusterHost,
-      port: clusterPort,
-      username: "",
-      password: ""
-    }
+  const existingConnection = Current.getConnection(store.getState())
 
-    store.dispatch(Clusters.add(cluster))
-    globalDispatch(Clusters.add(cluster))
-    store.dispatch(Current.setConnectionId(cluster.id))
+  if (host && port) {
+    store.dispatch(setupConnection(host, port))
+  } else if (!existingConnection) {
+    store.dispatch(setupConnection("localhost", "9867"))
   }
 
   await store.dispatch(refreshSpacesForAllConnections())
 
-  const lastId = Current.getSpaceId(store.getState())
-  const spaceId = space || lastId
+  const spaceId = space || Current.getSpaceId(store.getState())
 
   if (spaceId) store.dispatch(initSpace(spaceId))
 }
