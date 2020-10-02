0b3d944c58af3dd297c947e83491cd3758fef59c
James Kerr <jkerr838@gmail.com>
2020-10-02 16:20:28 -0700

    Save error-gradient

--- src/js/components/StartupError.tsx
+++ src/js/components/StartupError.tsx
@@ -8,7 +8,7 @@ const Wrap = styled.div`
   padding: 24px;
   padding-top: 54px;
   min-height: 100vh;
-  background: linear-gradient(to bottom right, #f8eee3, #fae1e0);
+  background: var(--error-gradient);
 `
 
 const WindowDragArea = styled.div`
