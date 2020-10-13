fbde41dd0ae708eb0f9a1a3f541cbd979450602b
James Kerr <jkerr838@gmail.com>
2020-10-13 06:38:34 -0700

    Update itest ingest step to look for popNotice

--- itest/lib/appStep/api/ingestFile.ts
+++ itest/lib/appStep/api/ingestFile.ts
@@ -7,6 +7,7 @@ import {click} from "./click"
 import logStep from "../util/logStep"
 import {LOG} from "../../log"
 import {selectors} from "../../../../src/js/test/integration"
+import {popNoticeLocator} from "../../../../src/js/test/locators"
 
 export default async (app: Application, file: string) => {
   // Ingest a file inside the itest/testdata directory
@@ -23,33 +24,8 @@ export default async (app: Application, file: string) => {
     await fileInput.addValue(remoteFile)
   })
 
-  try {
-    await logStep("wait for ingest to start", () =>
-      retryUntil(
-        async () =>
-          await (
-            await app.client.$(selectors.status.ingestProgress)
-          ).isExisting(),
-        (ingesting) => ingesting === true,
-        100,
-        100
-      )
-    )
-  } catch {
-    LOG.debug(
-      "ingest never appeared; let's hope it finished and ended before we could observe it"
-    )
-  }
-
-  return logStep("wait for ingest to finish", async () => {
-    retryUntil(
-      async () =>
-        await (
-          await app.client.$(selectors.status.ingestProgress)
-        ).isExisting(),
-      (ingesting) => ingesting === false,
-      100,
-      100
-    )
-  })
+  const notice = await app.client.$(popNoticeLocator.css)
+  app.client.waitUntil(() =>
+    notice.getText().then((text) => text === "Import complete.")
+  )
 }
