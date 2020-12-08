7a8711ad2e574a92d7f063256ae0ee811beaf9c8
James Kerr <jkerr838@gmail.com>
2020-12-08 13:51:18 -0800

    Fix another test

--- src/js/searches/programs.test.ts
+++ src/js/searches/programs.test.ts
@@ -95,6 +95,6 @@ test("conn correlation", () => {
   const record = zng.Record.deserialize(conn)
 
   expect(connCorrelation(record)).toBe(
-    'uid="CbOjYpkXn9LfqV51c" or "CbOjYpkXn9LfqV51c" in conn_uids or "CbOjYpkXn9LfqV51c" in uids or referenced_file.uid="CbOjYpkXn9LfqV51c" or (community_id = "1:h09VUfAoDYfBA0xGKuKCQ7nOxqU=" and ts >= 1425568032.998 and ts < 1425568033.707) | head 100'
+    'uid="CbOjYpkXn9LfqV51c" or "CbOjYpkXn9LfqV51c" in conn_uids or "CbOjYpkXn9LfqV51c" in uids or referenced_file.uid="CbOjYpkXn9LfqV51c" or (community_id = "1:h09VUfAoDYfBA0xGKuKCQ7nOxqU=" and ts >= 1425568032.998 and ts < 1425568123.707) | head 100'
   )
 })
