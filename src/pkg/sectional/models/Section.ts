54f65a0f69ac792b5e341c134f34ed162fde7cf5
James Kerr <jkerr838@gmail.com>
2020-12-15 12:02:25 -0800

    Set max to infinity if its null

--- src/pkg/sectional/models/Section.ts
+++ src/pkg/sectional/models/Section.ts
@@ -20,6 +20,7 @@ export default class Section {
     public isOpen: boolean = true,
     public closedSize: number = min
   ) {
+    if (!max) this.max = Infinity
     this.size = this.clamp(size)
   }
 
