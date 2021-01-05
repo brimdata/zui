4f0c3799296e6588ba62c9c0216d7b3350d2fd54
James Kerr <jkerr838@gmail.com>
2021-01-04 20:54:24 -0800

    Query for all in a new tab

--- ppl/detail/EventLimit.tsx
+++ ppl/detail/EventLimit.tsx
@@ -2,6 +2,7 @@ import {Caption} from "app/detail/Shared"
 import React, {memo, useCallback} from "react"
 import {useDispatch} from "react-redux"
 import Link from "src/js/components/common/Link"
+import {openNewSearchTab} from "src/js/flows/openNewSearchWindow"
 import {submitSearch} from "src/js/flows/submitSearch/mod"
 import {UID_CORRELATION_LIMIT} from "src/js/searches/programs"
 import SearchBar from "src/js/state/SearchBar"
@@ -16,7 +17,11 @@ export default memo<Props>(function EventLimit({query, count}) {
   const onClick = useCallback(() => {
     dispatch(SearchBar.clearSearchBar())
     dispatch(SearchBar.changeSearchBarInput(query))
-    dispatch(submitSearch())
+    if (global.windowName === "detail") {
+      dispatch(openNewSearchTab())
+    } else {
+      dispatch(submitSearch())
+    }
   }, [query])
 
   if (count < UID_CORRELATION_LIMIT) return null
