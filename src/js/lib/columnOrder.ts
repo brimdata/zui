b7e612a21daa942785c4af9f1bda5e8c651e1422
James Kerr <jkerr838@gmail.com>
2021-01-08 15:08:24 -0800

    Column order:  ts, _path, event_type (#1339)
    * Column order:  ts, _path, event_type
    
    * Give CI longer to complete an import

--- src/js/lib/columnOrder.ts
+++ src/js/lib/columnOrder.ts
@@ -1,13 +1,15 @@
 import {$Column} from "../state/Columns/models/column"
 
-const EXCLUDED = ["ts", "_td", "event_type"]
+const EXCLUDED = ["ts", "event_type", "_path"]
 
 export default (cols: $Column[]) => {
   let orderedCols = cols.filter(({name}) => !EXCLUDED.includes(name))
   const ts = cols.find(({name}) => name === "ts")
   const eventType = cols.find(({name}) => name === "event_type")
+  const path = cols.find(({name}) => name === "_path")
 
   if (eventType) orderedCols = [eventType, ...orderedCols]
+  if (path) orderedCols = [path, ...orderedCols]
   if (ts) orderedCols = [ts, ...orderedCols]
   return orderedCols
 }
