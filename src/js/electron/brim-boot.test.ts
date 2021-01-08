7e0ab78e35576834cda1bd1c2e15972a1a76f530
James Kerr <jkerr838@gmail.com>
2021-01-07 16:58:26 -0800

    Cross platfrom data path in test

--- src/js/electron/brim-boot.test.ts
+++ src/js/electron/brim-boot.test.ts
@@ -1,14 +1,15 @@
 import {Brim} from "./brim"
+import path from "path"
 
-const path = `tmp-boot-test/appState.json`
+const file = `tmp-boot-test/appState.json`
 
 test("boot starts zqd with defaults", async () => {
   const createSession = () => ({
     load: () => Promise.resolve(undefined)
   })
   // @ts-ignore
-  const brim = await Brim.boot(path, createSession)
-  expect(brim.zqd.root).toBe("/fake/path/data/spaces")
+  const brim = await Brim.boot(file, createSession)
+  expect(brim.zqd.root).toBe(path.normalize("/fake/path/data/spaces"))
   expect(brim.zqd.suricataRunner).toBe("")
   expect(brim.zqd.suricataUpdater).toBe("")
   expect(brim.zqd.zeekRunner).toBe("")
