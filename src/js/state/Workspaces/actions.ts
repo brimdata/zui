3e7622b90ba40fbc2e9d703200356ae3329138f8
Mason Fish <mason@looky.cloud>
2021-01-18 15:45:14 -0800

    grab authConfig from zqd
    Signed-off-by: Mason Fish <mason@looky.cloud>

--- src/js/state/Workspaces/actions.ts
+++ src/js/state/Workspaces/actions.ts
@@ -1,8 +1,19 @@
-import {WORKSPACE_ADD, WORKSPACE_REMOVE, Workspace} from "./types"
+import {
+  WORKSPACE_ADD,
+  WORKSPACE_REMOVE,
+  Workspace,
+  WORKSPACE_SET_TOKEN
+} from "./types"
 
 export default {
   add(workspace: Workspace): WORKSPACE_ADD {
     return {type: "WORKSPACE_ADD", workspace}
+  },
+  setWorkspaceToken(
+    workspaceId: string,
+    accessToken: string
+  ): WORKSPACE_SET_TOKEN {
+    return {type: "WORKSPACE_SET_TOKEN", workspaceId, accessToken}
   },
   remove(id: string): WORKSPACE_REMOVE {
     return {type: "WORKSPACE_REMOVE", id}
