f69505618449cbda79b61ff3c235feda38a12a59
Mason Fish <mason@looky.cloud>
2021-01-20 11:12:24 -0800

    add migration
    Signed-off-by: Mason Fish <mason@looky.cloud>

---
+++ src/js/state/migrations/202101201109_moveQueriesStateToGlobal.test.ts
@@ -0,0 +1,29 @@
+import getTestState, {getAllStates} from "../../test/helpers/getTestState"
+import migrate from "./202101201109_moveQueriesStateToGlobal"
+import init from "../../../../ppl/queries/initial"
+
+test("migrating 202101201109_moveQueriesStateToGlobal", () => {
+  let {data} = getTestState("v0.22.0")
+
+  let next = migrate(data)
+
+  const expectedQueries = init()
+  expectedQueries.items.push(
+    {
+      id: "dd6381e231",
+      value: "added query A",
+      name: "added query A",
+      description: "",
+      tags: []
+    },
+    {
+      id: "ddea06acc0",
+      value: "added query B",
+      name: "added query B",
+      description: "",
+      tags: []
+    }
+  )
+
+  expect(next.globalState.queries).toStrictEqual(expectedQueries)
+})
---
+++ src/js/state/migrations/202101201109_moveQueriesStateToGlobal.ts
@@ -0,0 +1,27 @@
+import {getAllStates} from "../../test/helpers/getTestState"
+
+export default function moveQueriesStateToGlobal(state: any) {
+  // Migrate state here
+  const mergedQueryMap = {}
+  const mergedQueryItems = []
+  for (const s of getAllStates(state)) {
+    if (!s.queries) continue
+    s.queries.items.forEach((q) => {
+      // attempt to preserve order of array
+      if (!(q.id in mergedQueryMap)) {
+        mergedQueryMap[q.id] = q
+        mergedQueryItems.push(q)
+      }
+    })
+
+    delete s.queries
+  }
+
+  state.globalState.queries = {
+    id: "root",
+    name: "root",
+    items: mergedQueryItems
+  }
+
+  return state
+}
