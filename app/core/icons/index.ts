ef7df2ceb9ef684f652eed948868c1791044459a
James Kerr <jkerr838@gmail.com>
2021-02-03 17:14:23 -0800

    Added 2 new icons, move 5 others into icons folder

--- app/core/icons/index.ts
+++ app/core/icons/index.ts
@@ -1,5 +1,21 @@
 import chart from "./chart.svg"
 import grid from "./grid.svg"
 import list from "./list.svg"
+import view from "./view.svg"
+import sharkfin from "./sharkfin.svg"
+import exportIcon from "./export.svg"
+import columns from "./columns.svg"
+import chevronDown from "./chevron-down.svg"
+import doubleChevronRight from "./double-chevron-right.svg"
 
-export default {chart, grid, list}
+export default {
+  chart,
+  grid,
+  list,
+  view,
+  sharkfin,
+  export: exportIcon,
+  columns,
+  "chevron-down": chevronDown,
+  "double-chevron-right": doubleChevronRight
+}
