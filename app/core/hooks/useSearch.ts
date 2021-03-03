1cac488927bf02b81ad9cfbf1d940ad13fae8bed
James Kerr <jkerr838@gmail.com>
2021-03-02 16:12:06 -0800

    Dont need empty check

--- app/core/hooks/useSearch.ts
+++ app/core/hooks/useSearch.ts
@@ -1,7 +1,6 @@
 import {useEffect, useState} from "react"
-import {useDispatch, useSelector} from "react-redux"
+import {useDispatch} from "react-redux"
 import {search} from "src/js/flows/search/mod"
-import Current from "src/js/state/Current"
 import {AppDispatch} from "src/js/state/types"
 import {zng} from "zealot"
 
@@ -11,16 +10,13 @@ export default function useSearch(query: string, deps?: any[]): R {
   const dispatch = useDispatch<AppDispatch>()
   const [records, setRecords] = useState<zng.Record[]>([])
   const [isFetching, setIsFetching] = useState<boolean>(true)
-  const space = useSelector(Current.mustGetSpace)
 
   useEffect(() => {
-    if (!space.empty()) {
-      setIsFetching(true)
-      const {response, abort} = dispatch(search({query}))
-      response.chan(0, (records) => setRecords(records))
-      response.status((status) => setIsFetching(status === "FETCHING"))
-      return abort
-    }
+    setIsFetching(true)
+    const {response, abort} = dispatch(search({query}))
+    response.chan(0, (records) => setRecords(records))
+    response.status((status) => setIsFetching(status === "FETCHING"))
+    return abort
   }, deps)
 
   return [records, isFetching]
