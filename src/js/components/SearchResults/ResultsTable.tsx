9dc02f418ed7b96127901d45aca6228bac83cd53
James Kerr <jkerr838@gmail.com>
2021-03-10 14:50:51 -0800

    Key off the index

--- src/js/components/SearchResults/ResultsTable.tsx
+++ src/js/components/SearchResults/ResultsTable.tsx
@@ -89,7 +89,7 @@ export default function ResultsTable(props: Props) {
     return (
       <LogRow
         columns={props.tableColumns}
-        key={logs[index].get("ts").toString() + index}
+        key={index}
         index={index}
         log={logs[index]}
         timeZone={props.timeZone}
