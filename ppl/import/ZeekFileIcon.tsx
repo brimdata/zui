dd401891fb7d77d0b86624127cd345b3bf4ffaa5
James Kerr <jkerr838@gmail.com>
2021-03-29 16:18:50 -0700

    Make import page generic (#1548)

--- ppl/import/ZeekFileIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-import ZeekFileSvg from "src/static/icons/zeek-file.svg"
-
-export default function ZeekFileIcon(props: any) {
-  return <ZeekFileSvg className="zeek-file-icon" {...props} />
-}
