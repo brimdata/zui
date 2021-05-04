b5f816655449a8efc4aa7c69d858631bbba9c895
James Kerr <jkerr838@gmail.com>
2021-05-04 13:17:57 -0700

    Remove unused test file

--- test/fixtures/zjson-types.ts
+++
@@ -1,7 +0,0 @@
-import {PrimitiveType} from "zealot/zjson"
-
-export const STRING = {kind: "primitive", name: "string"} as PrimitiveType
-export const TIME = {kind: "primitive", name: "time"} as PrimitiveType
-export const COUNT = {kind: "primitive", name: "count"} as PrimitiveType
-export const INTERVAL = {kind: "primitive", name: "interval"} as PrimitiveType
-export const IP = {kind: "primitive", name: "ip"} as PrimitiveType
