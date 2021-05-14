0585d3e3b234591bd4919067bea9faa5ff90dba7
James Kerr <jkerr838@gmail.com>
2021-05-14 08:14:38 -0700

    Migrate the name of the sidebar sections

--- src/js/state/migrations/202104291255_dropSpaces.test.ts
+++ src/js/state/migrations/202104291255_dropSpaces.test.ts
@@ -1,11 +1,16 @@
+import {getAllTabs} from "src/js/test/helpers/getTestState"
 import {migrate} from "src/js/test/helpers/migrate"
 
 test("migrating 202104291255_dropSpaces", async () => {
   const next = await migrate({state: "v0.24.0", to: "202104291255"})
-  expect.assertions(1)
+  expect.assertions(3)
 
   // @ts-ignore
   for (const {state} of Object.values(next.windows)) {
     expect(state.spaces).toBe(undefined)
+  }
+
+  for (let tab of getAllTabs(next)) {
+    expect(tab.layout.sidebarSections[0].id).toBe("pools")
   }
 })
--- src/js/state/migrations/202104291255_dropSpaces.ts
+++ src/js/state/migrations/202104291255_dropSpaces.ts
@@ -1,9 +1,14 @@
-import {getAllStates} from "../../test/helpers/getTestState"
+import {getAllStates, getAllTabs} from "../../test/helpers/getTestState"
 
 export default function dropSpaces(state: any) {
   // Default search records to "events"
   for (const s of getAllStates(state)) {
     delete s.spaces
+  }
+  for (const tab of getAllTabs(state)) {
+    tab.layout.sidebarSections.forEach((section) => {
+      if (section.id === "spaces") section.id = "pools"
+    })
   }
   return state
 }
