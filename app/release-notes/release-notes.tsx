d933c2d728f934b8b05a5346e3a70d7a8fd636ae
James Kerr <jkerr838@gmail.com>
2021-05-18 14:23:45 -0700

    Created React Component for Release Notes

--- app/release-notes/release-notes.tsx
+++ app/release-notes/release-notes.tsx
@@ -1,5 +1,44 @@
 import React from "react"
+import ReactMarkdown from "react-markdown"
+import Link from "src/js/components/common/Link"
+import styled from "styled-components"
+// @ts-ignore
+import {version} from "../../../package.json"
+import {useReleaseNotes} from "./use-release-notes"
+
+const Scrollable = styled.div`
+  overflow: auto;
+`
+const BG = styled.div`
+  margin: 48px auto;
+  padding: 24px;
+  max-width: 600px;
+  line-height: 1.6;
+  a {
+    color: var(--azure);
+  }
+  hr {
+    border: none;
+    border-top: 1px solid var(--cloudy);
+  }
+`
+
+const components = {
+  // eslint-disable-next-line
+  a: ({children, ...props}) => {
+    return <Link {...props}>{children}</Link>
+  }
+}
 
 export default function ReleaseNotes() {
-  return <h1>Release Notes</h1>
+  const notes = useReleaseNotes(version)
+
+  return (
+    <Scrollable>
+      <BG className="release-notes">
+        <h1>Release Notes for Version {version}</h1>
+        <ReactMarkdown components={components}>{notes}</ReactMarkdown>
+      </BG>
+    </Scrollable>
+  )
 }
