498e6d7eea6346bd0f9d8205a48a1dbff451a0ca
James Kerr <jkerr838@gmail.com>
2021-05-21 15:55:37 -0700

    Add detail pane test with new itest pattern

---
+++ itest/tests/detail-pane.test.ts
@@ -0,0 +1,53 @@
+import {text} from "d3"
+import itest from "itest/lib/itest"
+import {toLower} from "lodash"
+
+describe("Detail Pane", () => {
+  // Names of the sections to test for
+  const FIELDS = "fields"
+  const UID = "correlation"
+  const ALERTS = "related alerts"
+  const HISTORY = "conn history"
+  const CONNECTIONS = "related connections"
+  const MD5 = "md5 correlation"
+
+  const [brim, $] = itest("detail-pane")
+  beforeAll(async () => {
+    await brim.ingest("ifconfig.pcapng")
+    await brim.search("")
+    await brim.click($.toolbarView)
+    await brim.clickContextMenuItem("Right Pane")
+  })
+
+  // Helper function to perform the common actions
+  async function testDetailHeaders(path, sectionNames) {
+    await brim.click($.viewerCellContaining(path))
+    const sections = await brim.findAll($.detailPaneSections)
+    const headers = await Promise.all(sections.map((s) => s.getText()))
+    expect(headers.map(toLower)).toEqual(expect.arrayContaining(sectionNames))
+  }
+
+  test("for capture loss", () => {
+    return testDetailHeaders("capture_loss", [FIELDS])
+  })
+
+  test("for stats", () => {
+    return testDetailHeaders("stats", [FIELDS])
+  })
+
+  test("for alert", () => {
+    return testDetailHeaders("alert", [FIELDS, ALERTS, CONNECTIONS])
+  })
+
+  test("for file", () => {
+    return testDetailHeaders("files", [FIELDS, MD5])
+  })
+
+  test("for http", () => {
+    return testDetailHeaders("files", [FIELDS, UID])
+  })
+
+  test("for conn", () => {
+    return testDetailHeaders("conn", [FIELDS, UID, HISTORY])
+  })
+})
