79904053e1cf28c418a668b62317280f2a3ce915
James Kerr <jkerr838@gmail.com>
2021-05-24 13:10:04 -0700

    Log the error

--- src/js/state/GlobalContext.ts
+++ src/js/state/GlobalContext.ts
@@ -5,7 +5,9 @@ export const globalDispatchMiddleware = (_store) => (next) => (action) => {
   const result = next(action)
   if (action.type.startsWith("$") && !action.remote) {
     // Don't re-broadcast a message that was broadcast to you
-    invoke(ipc.globalStore.dispatch(action))
+    invoke(ipc.globalStore.dispatch(action)).catch((e) => {
+      console.error(e)
+    })
   }
   return result
 }
