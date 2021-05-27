0eb90067a8225308232750a4a8d0ae2aa76ab560
Brim Automation <automation@brimsecurity.com>
2021-05-27 15:57:05 +0000

    Zed update through "add line number to TestMarkdownExample subtest names" by nwt
    This is an auto-generated commit with a Zed dependency update. The Zed PR
    https://github.com/brimdata/zed/pull/2768, authored by @nwt,
    has been merged.
    
    add line number to TestMarkdownExample subtest names
    
    TestMarkdownExample subtests with a single Markdown file have names
    ending in sequential numbers, making the test source hard to find when a
    test fail. Change the sequential numbers to line numbers so that, for
    example, the test whose command appears on line 317 of
    docs/language/operators/README.md is named
    TestMarkdownExample/docs/language/operators/README.md:317.
    
    Closes brimdata/zed#2264

--- test/api/pools_test.ts
+++ test/api/pools_test.ts
@@ -1,6 +1,6 @@
 import {assertEquals, testApi} from "./helper/mod.ts"
 
-const defaultLayout = {"keys": [["ts"]], "order": "desc"}
+const defaultLayout = {keys: [["ts"]], order: "desc"}
 
 testApi("listing the pool", async (zealot: any) => {
   await zealot.pools.create({name: "pool1", layout: defaultLayout})
@@ -11,11 +11,11 @@ testApi("listing the pool", async (zealot: any) => {
 testApi("creating a pool", async (zealot: any) => {
   const pool = await zealot.pools.create({name: "pool1", layout: defaultLayout})
   assertEquals(pool.name, "pool1")
-  assertEquals(pool.layout, {keys: [["ts"]], order:"desc"})
+  assertEquals(pool.layout, {keys: [["ts"]], order: "desc"})
 })
 
 testApi("pool stats", async (zealot: any) => {
-  const { id } = await zealot.pools.create({name: "pool1", layout: defaultLayout})
-  const stats = await zealot.pools.stats(id) 
+  const {id} = await zealot.pools.create({name: "pool1", layout: defaultLayout})
+  const stats = await zealot.pools.stats(id)
   assertEquals(stats, {size: 0, span: null})
 })
