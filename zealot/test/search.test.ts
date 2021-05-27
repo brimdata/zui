e298fccce836cae886471756b21552d99c9a8bd3
James Kerr <jkerr838@gmail.com>
2021-05-27 10:27:19 -0700

    Touch up search tests

--- zealot/test/search.test.ts
+++ zealot/test/search.test.ts
@@ -16,96 +16,103 @@ async function setup(zealot: any) {
   })
 }
 
-function testApi(name, fn) {
-  test(name, () => withLake(fn))
-}
+test("search#records", () => {
+  return withLake(async (zealot) => {
+    await setup(zealot)
+    const resp = await zealot.search("* | sort ts")
+    const results = await resp.records()
 
-function assertEquals(a, b) {
-  expect(a).toEqual(b)
-}
-
-testApi("search#records", async (zealot) => {
-  await setup(zealot)
-  const resp = await zealot.search("* | sort ts")
-  const results = await resp.records()
-
-  assertEquals(results.length, 30)
-})
-
-testApi("search#iterator", async (zealot) => {
-  await setup(zealot)
-
-  const stream = await zealot.search("* | sort ts")
-  const types = []
-  for await (const payload of stream) {
-    types.push(payload.type)
-  }
-
-  assertEquals(uniq(types), [
-    "TaskStart",
-    "SearchRecords",
-    "SearchEnd",
-    "SearchStats",
-    "TaskEnd"
-  ])
-})
-
-testApi("search#callbacks start and end", async (zealot: any) => {
-  await setup(zealot)
-  const resp = await zealot.search("*")
-  const start = jest.fn()
-  const end = jest.fn()
-
-  await new Promise<void>((resolve, reject) => {
-    resp
-      .callbacks()
-      .start(start)
-      .end((args: any) => {
-        end(args)
-        resolve()
-      })
-      .error(reject)
+    expect(results.length).toBe(30)
   })
-
-  expect(start).toHaveBeenCalledWith({task_id: 0, type: "TaskStart"})
-  expect(end).toHaveBeenCalledWith({task_id: 0, type: "TaskEnd"})
 })
 
-testApi("search#callbacks record", async (zealot: any) => {
-  await setup(zealot)
-  const resp = await zealot.search('_path=="conn" | sort ts | head 1')
-  const records = jest.fn()
-  await new Promise((resolve, reject) => {
-    resp
-      .callbacks()
-      .records(records)
-      .end(resolve)
-      .error(reject)
+test("search#iterator", () => {
+  return withLake(async (zealot) => {
+    await setup(zealot)
+
+    const stream = await zealot.search("* | sort ts")
+    const types = []
+    for await (const payload of stream) {
+      types.push(payload.type)
+    }
+
+    expect(uniq(types)).toEqual([
+      "TaskStart",
+      "SearchRecords",
+      "SearchEnd",
+      "SearchStats",
+      "TaskEnd"
+    ])
   })
-
-  const args = records.mock.calls[0][0]
-  assertEquals(Object.keys(args), ["channel", "rows", "newRows", "schemas"])
-  assertEquals(args.channel, 0)
-  assertEquals(Object.keys(args.schemas).length, 1)
-  assertEquals(args.newRows.length, 1)
-  assertEquals(args.rows.length, 1)
 })
 
-testApi("search#originResponse format=zng", async (zealot: any) => {
-  await setup(zealot)
-  const resp = await zealot.search("*", {format: "zng", controlMessages: false})
-  for await (const chunk of resp.origResp.body) {
-    assertEquals(chunk instanceof Buffer, true) // in node its a buffer, in browser a Uint8Array
-  }
+test("search#callbacks start and end", () => {
+  return withLake(async (zealot) => {
+    await setup(zealot)
+    const resp = await zealot.search("*")
+    const start = jest.fn()
+    const end = jest.fn()
+
+    await new Promise<void>((resolve, reject) => {
+      resp
+        .callbacks()
+        .start(start)
+        .end((args: any) => {
+          end(args)
+          resolve()
+        })
+        .error(reject)
+    })
+
+    expect(start).toHaveBeenCalledWith({task_id: 0, type: "TaskStart"})
+    expect(end).toHaveBeenCalledWith({task_id: 0, type: "TaskEnd"})
+  })
 })
 
-testApi("search with abortController", async (zealot: any) => {
-  await setup(zealot)
-  const onAbort = jest.fn()
-  const ctl = new AbortController()
-  ctl.signal.onabort = onAbort
-  await zealot.search("*", {signal: ctl.signal})
-  ctl.abort()
+test("search#callbacks record", () => {
+  return withLake(async (zealot) => {
+    await setup(zealot)
+    const resp = await zealot.search('_path=="conn" | sort ts | head 1')
+    const records = jest.fn()
+    await new Promise((resolve, reject) => {
+      resp
+        .callbacks()
+        .records(records)
+        .end(resolve)
+        .error(reject)
+    })
 
-  expect(onAbort).toHaveBeenCalled()
+    const args = records.mock.calls[0][0]
+    expect(Object.keys(args)).toEqual(["channel", "rows", "newRows", "schemas"])
+    expect(args.channel).toBe(0)
+    expect(Object.keys(args.schemas).length).toBe(1)
+    expect(args.newRows.length).toBe(1)
+    expect(args.rows.length).toBe(1)
+  })
+})
+
+test("search#originResponse format=zng", () => {
+  return withLake(async (zealot) => {
+    await setup(zealot)
+    const resp = await zealot.search("*", {
+      format: "zng",
+      controlMessages: false
+    })
+    for await (const chunk of resp.origResp.body) {
+      expect(chunk).toBeInstanceOf(Buffer) // in node its a buffer, in browser a Uint8Array
+    }
+  })
+})
+
+test("search with abortController", () => {
+  return withLake(async (zealot) => {
+    await setup(zealot)
+    const onAbort = jest.fn()
+    const ctl = new AbortController()
+    ctl.signal.onabort = onAbort
+    await zealot.search("*", {signal: ctl.signal})
+    ctl.abort()
+
+    expect(onAbort).toHaveBeenCalled()
+  })
 })
