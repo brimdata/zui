0d706a092ce86f6727eff62cfbc29d8a6a0c1e90
James Kerr <jkerr838@gmail.com>
2021-07-19 16:29:23 -0700

    Copy html into dist
    Previously, all html files were at the root of the project.
    This commit adds a build step to copy each html file and place
    it in the dist folder keeping its place in the folder structure.
    
    This will afford more modular code where the html file can live
    right next to the corresponding main process code that needs it.

--- scripts/util/commands.js
+++ scripts/util/commands.js
@@ -4,5 +4,6 @@ const JS =
 const SCSS = "node-sass-chokidar src --output dist --quiet"
 
 const STATIC = 'cpx "src/static/**" dist/static'
+const HTML = 'cpx "{*.html,src/**/*.html}" dist'
 
-module.exports = {JS, SCSS, STATIC}
+module.exports = {JS, SCSS, STATIC, HTML}
