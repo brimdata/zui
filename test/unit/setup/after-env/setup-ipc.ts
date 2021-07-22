5acfc9cecda0d8752d053265c0751d2c48745fe5
James Kerr <jkerr838@gmail.com>
2021-07-22 11:35:58 -0700

    Remove unecessary test setup (#1759)

--- test/unit/setup/after-env/setup-ipc.ts
+++
@@ -1,9 +0,0 @@
-// TODO: Delete this file
-import {meta} from "app/ipc/meta"
-import {serve} from "src/pkg/electron-ipc-service"
-
-/**
- * Add the rest of the ipc services here over time
- */
-
-serve(meta)
