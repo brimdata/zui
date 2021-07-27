eb0ce86a88a3e408302d07704c185739e6bef1bb
James Kerr <jkerr838@gmail.com>
2021-07-27 13:50:01 -0700

    Fetch stats when initializing a pool

--- src/js/flows/initPool.ts
+++ src/js/flows/initPool.ts
@@ -1,3 +1,4 @@
+import interop from "../brim/interop"
 import Current from "../state/Current"
 import Pools from "../state/Pools"
 import {Thunk} from "../state/types"
@@ -10,10 +11,11 @@ export const initPool = (poolId: string): Thunk<Promise<void>> => (
   const workspaceId = Current.getWorkspaceId(getState())
   if (!workspaceId) return
   const zealot = dispatch(getZealot())
-  return zealot.pools
-    .get(poolId)
-    .then((data) => {
-      dispatch(Pools.setDetail(workspaceId, data))
+
+  return Promise.all([zealot.pools.get(poolId), zealot.pools.stats(poolId)])
+    .then(([data, rawStats]) => {
+      const stats = interop.poolStatsPayloadToPool(rawStats)
+      dispatch(Pools.setDetail(workspaceId, {...data, ...stats}))
     })
     .catch((error) => {
       console.error(error)
--- src/js/flows/refreshPoolNames.ts
+++ src/js/flows/refreshPoolNames.ts
@@ -1,33 +1,22 @@
-import {Thunk} from "../state/types"
+import {BrimWorkspace} from "../brim"
 import Current from "../state/Current"
 import Pools from "../state/Pools"
+import {Thunk} from "../state/types"
 import {getZealot} from "./getZealot"
-import {BrimWorkspace} from "../brim"
-import interop from "../brim/interop"
 
+/**
+ * This only gets the list of names from the server, all the
+ * details like the min_time and max_time need to be requested
+ * somewhere else. (like in initPool)
+ */
 export default function refreshPoolNames(
   ws?: BrimWorkspace
 ): Thunk<Promise<void>> {
   return (dispatch, getState) => {
     const zealot = dispatch(getZealot(ws))
-
-    let pools = []
-    return zealot.pools
-      .list()
-      .then((data) => {
-        pools = data || []
-        return Promise.all(
-          pools.map(async (pool, i) => {
-            const stats = interop.poolStatsPayloadToPool(
-              await zealot.pools.stats(pool.id)
-            )
-            pools[i] = {...pool, ...stats}
-          })
-        )
-      })
-      .then(() => {
-        const id = ws?.id || Current.getWorkspaceId(getState())
-        if (id) dispatch(Pools.setPools(id, pools))
-      })
+    return zealot.pools.list().then((pools) => {
+      const id = ws?.id || Current.getWorkspaceId(getState())
+      if (id) dispatch(Pools.setPools(id, pools))
+    })
   }
 }
