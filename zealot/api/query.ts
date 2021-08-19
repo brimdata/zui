db3a1c292cc312931eda123baa49c0b04a2a49c6
Mason Fish <mason@brimsecurity.com>
2021-08-19 11:36:00 -0700

    use header for format instead of qParam
    Signed-off-by: Mason Fish <mason@brimsecurity.com>

--- zealot/api/query.ts
+++ zealot/api/query.ts
@@ -5,13 +5,35 @@ export default function queryApi(zql: string, args: QueryArgs): FetchArgs {
   return {
     method: "POST",
     path: `/query?${getQueryParams(args)}`,
-    body: JSON.stringify({query: zql})
+    body: JSON.stringify({query: zql}),
+    headers: getHeaders(args)
   }
+}
+
+function getHeaders(args: QueryArgs) {
+  let h = new Headers()
+
+  let format
+  switch (args.format) {
+    case "zng":
+      format = "application/x-zng"
+      break
+    case "ndjson":
+      format = "application/x-ndjson"
+      break
+    case "csv":
+      format = "text/csv"
+      break
+    default:
+      format = "application/x-zjson"
+  }
+  h.set("Accept", format)
+
+  return h
 }
 
 function getQueryParams(args: QueryArgs) {
   let p = new URLSearchParams()
-  p.set("format", args.format)
 
   if (args.controlMessages === false) {
     p.set("noctrl", "true")
