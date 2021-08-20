be8a2b6d2bd47dad5a339c4315c2d3663ca16523
James Kerr <jkerr838@gmail.com>
2021-08-20 14:07:45 -0700

    Remove sci notation

---
+++ app/core/format.test.ts
@@ -0,0 +1,10 @@
+import {createData} from "test/shared/factories/zed-factory"
+import {zed} from "zealot"
+import {formatPrimitive} from "./format"
+
+test("format a long number", () => {
+  const num = createData(1394668388559068) as zed.Primitive
+  const str = formatPrimitive(num)
+
+  expect(str).toBe("1,394,668,388,559,068")
+})
