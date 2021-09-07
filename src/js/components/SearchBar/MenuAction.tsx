61a1b5a6eb2840299ed8aa27596866a9c8b4a2dd
James Kerr <jkerr838@gmail.com>
2021-09-07 16:50:26 -0700

    Use abortables instead of handlers in searches

--- src/js/components/SearchBar/MenuAction.tsx
+++ src/js/components/SearchBar/MenuAction.tsx
@@ -1,17 +1,17 @@
-import {useDispatch, useSelector} from "react-redux"
+import {useBrimApi} from "app/core/context"
 import React from "react"
-
+import {useDispatch, useSelector} from "react-redux"
 import {reactElementProps} from "../../../../test/integration/helpers/integration"
-import Handlers from "../../state/Handlers"
-import InputAction from "./InputAction"
-import Modal from "../../state/Modal"
-import PopMenuPointy from "../PopMenu/PopMenuPointy"
-import Tab from "../../state/Tab"
 import ThreeDotsIcon from "../../icons/ThreeDotsIcon"
 import open from "../../lib/open"
+import Modal from "../../state/Modal"
+import Tab from "../../state/Tab"
+import PopMenuPointy from "../PopMenu/PopMenuPointy"
+import InputAction from "./InputAction"
 
 export default function MenuAction() {
   const dispatch = useDispatch()
+  const api = useBrimApi()
   const isFetching = useSelector(Tab.isFetching)
 
   const menu = [
@@ -25,7 +25,7 @@ export default function MenuAction() {
     },
     {
       label: "Kill search",
-      click: () => dispatch(Handlers.abortAll()),
+      click: () => api.abortables.abort(),
       disabled: !isFetching
     }
   ]
--- src/js/flows/search/mod.ts
+++ src/js/flows/search/mod.ts
@@ -1,12 +1,10 @@
+import Tabs from "src/js/state/Tabs"
+import Current from "../../state/Current"
+import Tab from "../../state/Tab"
 import {Thunk} from "../../state/types"
 import {getZealot} from "../getZealot"
 import {handle} from "./handler"
-import Handlers from "../../state/Handlers"
 import {SearchResponse} from "./response"
-import {Handler} from "src/js/state/Handlers/types"
-import Current from "../../state/Current"
-import Tab from "../../state/Tab"
-import randomHash from "../../brim/randomHash"
 
 type Args = {
   query: string
@@ -22,29 +20,27 @@ export type BrimSearch = {
   abort: () => void
 }
 
-export function search({
-  query,
-  from,
-  to,
-  poolId,
-  id = randomHash()
-}: Args): Thunk<BrimSearch> {
-  return (dispatch, getState) => {
+export function search({query, from, to, poolId, id}: Args): Thunk<BrimSearch> {
+  return (dispatch, getState, {api}) => {
     const [defaultFrom, defaultTo] = Tab.getSpanAsDates(getState())
+    const tab = Tabs.getActive(getState())
     const defaultPoolId = Current.getPoolId(getState())
     const zealot = dispatch(getZealot())
     const ctl = new AbortController()
-    const searchHandle: Handler = {type: "SEARCH", abort: () => ctl.abort()}
+    const abort = () => ctl.abort()
+    const tag = id
+
     poolId = poolId || defaultPoolId
     to = to || defaultTo
     from = from || defaultFrom
     const req = zealot.search(query, {from, to, poolId, signal: ctl.signal})
-    dispatch(Handlers.abort(id, false))
-    dispatch(Handlers.register(id, searchHandle))
 
-    const abort = () => ctl.abort()
+    api.abortables.abort({tab, tag})
+    const aId = api.abortables.add({abort, tab, tag})
+
     const {response, promise} = handle(req)
-    promise.finally(() => dispatch(Handlers.remove(id)))
+    promise.finally(() => api.abortables.remove(aId))
+
     return {response, promise, abort}
   }
 }
