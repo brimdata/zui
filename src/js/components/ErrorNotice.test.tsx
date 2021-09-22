f70aab9689796ad59ecb6e99d1335ee9e1de6d20
Mason Fish <mason@brimsecurity.com>
2021-09-22 16:04:54 -0700

    address pr comment
    Signed-off-by: Mason Fish <mason@brimsecurity.com>

--- src/js/components/ErrorNotice.test.tsx
+++ src/js/components/ErrorNotice.test.tsx
@@ -1,15 +1,20 @@
 import React from "react"
 
-import logInto from "../../../test/unit/helpers/loginTo"
-import provide from "../../../test/unit/helpers/provide"
 import Notice from "../state/Notice"
 import ErrorFactory from "../models/ErrorFactory"
 import ErrorNotice from "./ErrorNotice"
 import {screen} from "@testing-library/react"
+import {setupBrim} from "../../../test/unit/helpers/setup-brim"
+import {render} from "../../../test/unit/helpers"
+
+const brim = setupBrim()
+
+beforeEach(() => {
+  render(<ErrorNotice />, {store: brim.store})
+})
 
 test("renders Error notice with no details", async () => {
-  const {store} = await logInto("workspace1", "pool1")
-  store.dispatch(
+  brim.store.dispatch(
     Notice.set(
       ErrorFactory.create({
         type: "Bad",
@@ -17,13 +22,11 @@ test("renders Error notice with no details", async () => {
       })
     )
   )
-  provide(store, <ErrorNotice />)
   expect(screen.getByRole("alert")).toHaveTextContent("Test error message")
 })
 
 test("renders Error notice with detail string", async () => {
-  const {store} = await logInto("workspace1", "pool1")
-  store.dispatch(
+  brim.store.dispatch(
     Notice.set(
       ErrorFactory.create({
         type: "Bad",
@@ -32,14 +35,12 @@ test("renders Error notice with detail string", async () => {
       })
     )
   )
-  provide(store, <ErrorNotice />)
   expect(screen.getByRole("alert")).toHaveTextContent("Test error message")
   expect(screen.getByRole("alert")).toHaveTextContent("Test detail")
 })
 
 test("renders Error notice with detail array", async () => {
-  const {store} = await logInto("workspace1", "pool1")
-  store.dispatch(
+  brim.store.dispatch(
     Notice.set(
       ErrorFactory.create({
         type: "Bad",
@@ -48,7 +49,6 @@ test("renders Error notice with detail array", async () => {
       })
     )
   )
-  provide(store, <ErrorNotice />)
   expect(screen.getByRole("alert")).toHaveTextContent("Test error message")
   expect(screen.getByRole("alert")).toHaveTextContent("Test detail 1")
   expect(screen.getByRole("alert")).toHaveTextContent("Test detail 2")
