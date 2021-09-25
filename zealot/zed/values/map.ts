e5482c73112403c121c19a842fa0e8dcba56c7fe
Noah Treuhaft <noah.treuhaft@gmail.com>
2021-09-24 21:49:32 -0400

    update ZSON map syntax

--- zealot/zed/values/map.ts
+++ zealot/zed/values/map.ts
@@ -1,4 +1,5 @@
 import {isNull} from "../utils"
+import {TypeIp} from "../types/type-ip"
 import {TypeMap} from "../types/type-map"
 import {ZedValue, ZedValueInterface} from "./types"
 
@@ -11,11 +12,12 @@ export class ZedMap implements ZedValueInterface {
   toString() {
     if (isNull(this.value)) return "null"
     const contents = Array.from(this.value.entries())
-      .map(
-        ([key, value]) => "{" + key.toString() + "," + value.toString() + "}"
-      )
+      .map(([key, value]) => {
+        const sep = isIPv6(key) ? " :" : ":"
+        return key.toString() + sep + value.toString()
+      })
       .join(",")
-    return `|{${contents}}|}`
+    return `|{${contents}}|`
   }
 
   serialize() {
@@ -28,4 +30,8 @@ export class ZedMap implements ZedValueInterface {
   isUnset() {
     return isNull(this.value)
   }
+}
+
+function isIPv6(v: ZedValue): boolean {
+  return v.type === TypeIp && v.toString().includes(":")
 }
