691de71084c89b31bd45064a405310800ecab86c
James Kerr <jkerr838@gmail.com>
2021-09-30 17:35:08 -0700

    Get Devtools Back (#1860)
    We were in the dark ages for too long. Not being able to use redux
    or react devtools. Winter is over. By upgrading a few minor versions
    and passing the allowFileAccess: true options to our extensions,
    the devtools have been resurrected.

--- src/js/electron/extensions.ts
+++ src/js/electron/extensions.ts
@@ -8,7 +8,10 @@ import log from "electron-log"
 
 export function installExtensions() {
   if (env.isIntegrationTest) return
-  return installExtension([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS])
+  // @ts-ignore The types package is not up to date
+  return installExtension([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS], {
+    loadExtensionOptions: {allowFileAccess: true}
+  })
     .then(() => log.info("Devtools loaded"))
     .catch((err) => log.error("Devtools error occurred: ", err))
 }
