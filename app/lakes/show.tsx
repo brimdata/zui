f51410a0e5655e3949919149fa0f2b5109adc613
James Kerr <jkerr838@gmail.com>
2021-11-01 15:31:01 -0700

    Re-mount the CommitNotification compontent whenever a pool changes

--- app/lakes/show.tsx
+++ app/lakes/show.tsx
@@ -2,7 +2,7 @@ import useLakeId from "app/router/hooks/use-lake-id"
 import useWorkspaceId from "app/router/hooks/use-workspace-id"
 import {lakeSearch} from "app/router/routes"
 import {workspacePath} from "app/router/utils/paths"
-import React, {useEffect, useState} from "react"
+import React, {useEffect} from "react"
 import {useDispatch, useSelector} from "react-redux"
 import {Redirect, Route, Switch, useRouteMatch} from "react-router"
 import {initPool} from "src/js/flows/initPool"
@@ -30,25 +30,13 @@ function InitLake({children}) {
   const dispatch = useDispatch<AppDispatch>()
   const poolId = useLakeId()
   const workspaceId = useWorkspaceId()
-  const [fetching, setFetching] = useState(false)
   const pool = useSelector(Current.getPool)
 
   useEffect(() => {
-    if (poolId) {
-      setFetching(true)
-      dispatch(initPool(poolId)).finally(() => setFetching(false))
-    } else {
-      setFetching(false)
-    }
+    if (poolId) dispatch(initPool(poolId))
   }, [poolId])
 
-  if (pool) {
-    if (fetching || !pool.hasStats()) {
-      return null
-    } else {
-      return children
-    }
-  } else {
-    return <Redirect to={workspacePath(workspaceId)} />
-  }
+  if (!pool) return <Redirect to={workspacePath(workspaceId)} />
+  if (pool && !pool.hasStats()) return null
+  return children
 }
