2250e55fb6eb3c5a537df6c63a67634294007745
James Kerr <jkerr838@gmail.com>
2021-11-04 12:37:18 -0700

    Use the hook in the import page

--- app/lakes/import.tsx
+++ app/lakes/import.tsx
@@ -1,45 +1,13 @@
 import LoadFilesInput from "ppl/import/LoadFilesInput"
 import React from "react"
-import toast from "react-hot-toast"
-import {useDispatch} from "react-redux"
-import {TransactionError} from "src/js/lib/transaction"
 import Link from "../../src/js/components/common/Link"
-import errors from "../../src/js/errors"
-import ingestFiles from "../../src/js/flows/ingestFiles"
-import refreshPoolNames from "../../src/js/flows/refreshPoolNames"
-import ErrorFactory from "../../src/js/models/ErrorFactory"
-import Notice from "../../src/js/state/Notice"
-import {AppDispatch} from "../../src/js/state/types"
-
-function handleError(e: TransactionError, dispatch: AppDispatch) {
-  const cause = e.cause
-  if (/(Failed to fetch)|(network error)/.test(cause.message)) {
-    dispatch(Notice.set(errors.importInterrupt()))
-  } else if (/format detection error/i.test(cause.message)) {
-    dispatch(Notice.set(errors.formatDetection(cause.message)))
-  } else {
-    dispatch(Notice.set(ErrorFactory.create(e.cause)))
-  }
-
-  dispatch(refreshPoolNames())
-  console.error(e.message)
-}
 
 export default function TabImport() {
-  const dispatch = useDispatch<AppDispatch>()
-
-  function onChange(_e, files) {
-    if (!files.length) return
-    dispatch(ingestFiles(files))
-      .then(() => toast.success("Import complete."))
-      .catch((e) => handleError(e, dispatch))
-  }
-
   return (
     <div className="input-methods">
       <section>
         <h2>Import Files</h2>
-        <LoadFilesInput onChange={onChange} />
+        <LoadFilesInput />
         <footer>
           <p>
             <b>Accepted formats:</b>{" "}
