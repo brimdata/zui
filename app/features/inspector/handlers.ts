1fe3c79b3b69fc76c6cff039e9a0877c996b736f
James Kerr <kerr@hey.com>
2021-12-28 16:19:45 -0800

    Remove handlers file

--- app/features/inspector/handlers.ts
+++
@@ -1,9 +0,0 @@
-import Inspector from "src/js/state/Inspector"
-// Don't need this file anymore
-export const expandAll = () => (dispatch) => {
-  dispatch(Inspector.setAllExpanded(true))
-}
-
-export const collapseAll = () => (dispatch) => {
-  dispatch(Inspector.setAllExpanded(false))
-}
--- app/routes/search/expand-hook.ts
+++ app/routes/search/expand-hook.ts
@@ -1,15 +1,15 @@
-import {collapseAll, expandAll} from "app/features/inspector/handlers"
 import {useDispatch} from "react-redux"
+import Inspector from "src/js/state/Inspector"
 
 export function useExpandState() {
   const dispatch = useDispatch()
 
   return {
     expandAll() {
-      dispatch(expandAll())
+      dispatch(Inspector.setAllExpanded(true))
     },
     collapseAll() {
-      dispatch(collapseAll())
+      dispatch(Inspector.setAllExpanded(false))
     }
   }
 }
