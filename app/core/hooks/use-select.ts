4886c17c80d99d87b5f627d7033abfb99eb6ecc9
James Kerr <kerr@hey.com>
2021-12-28 16:16:06 -0800

    Add comment to use-select

--- app/core/hooks/use-select.ts
+++ app/core/hooks/use-select.ts
@@ -1,6 +1,12 @@
 import {useCallback} from "react"
 import {useStore} from "react-redux"
 
+/**
+ * This is useful when you need to select from the state
+ * inside an event handler. The component doesn't need to
+ * re-render when the selected state changes, it only
+ * needs to grab it once in the event handler.
+ */
 export default function useSelect() {
   const store = useStore()
   const select = useCallback((selector) => selector(store.getState()), [store])
