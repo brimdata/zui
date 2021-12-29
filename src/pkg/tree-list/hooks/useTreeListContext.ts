93ebd4274b51c625acfee1c5169f04fcabfbc79e
James Kerr <kerr@hey.com>
2021-12-29 08:59:57 -0800

    Remove unused tree list code

--- src/pkg/tree-list/hooks/useTreeListContext.ts
+++
@@ -1,6 +0,0 @@
-import {createContext, useContext} from "react"
-import {TreeController} from "../types"
-
-export const TreeListContext = createContext<TreeController>(null)
-
-export const useTreeListContext = () => useContext(TreeListContext)
