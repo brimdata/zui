b8389cf19f0afc513e7f67e06c4c6e126a69ff8b
James Kerr <kerr@hey.com>
2022-01-07 16:42:11 -0800

    Run affected unit tests

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  "*.{js,ts,tsx}": "eslint --cache --fix",
+  "*.{js,ts,tsx}": [
+    "eslint --cache --fix",
+    "jest --project=unit --findRelatedTests --bail"
+  ],
   "*.{js,ts,tsx,css,scss,html,md,json}": "prettier --write",
   "**/*.ts?(x)": () => "tsc -p tsconfig.json --noEmit"
 }
