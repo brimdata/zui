d84118b22561cb66979a49e3699c992067ccc973
James Kerr <kerr@hey.com>
2022-01-12 22:15:29 -0800

    Add zealot to workspace

--- zealot/rollup.config.js
+++ zealot/rollup.config.js
@@ -5,7 +5,7 @@ import commonjs from "@rollup/plugin-commonjs"
 export default {
   input: "dist/cjs/index.js",
   output: {
-    file: "dist/es/bundle.js",
+    file: "dist/es/index.js",
     format: "es"
   },
   plugins: [resolve({preferBuiltins: false, browser: true}), commonjs()]
