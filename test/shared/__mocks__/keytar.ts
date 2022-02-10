d85e9edbac44f182badd9accca12ba97c50d9416
James Kerr <kerr@hey.com>
2022-02-09 19:57:45 -0800

    mock keytar library

---
+++ test/shared/__mocks__/keytar.ts
@@ -0,0 +1,13 @@
+const map = new Map()
+
+export default {
+  getPassword: (key: string, username: string) => {
+    return map.get(key + username)
+  },
+  setPassword: (key, username, password) => {
+    map.set(key + username, password)
+  },
+  deletePassword: (key, username) => {
+    map.delete(key + username)
+  }
+}
