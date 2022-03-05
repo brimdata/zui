b593ff4585d7b22e696043ab86484ef548fe0d57
James Kerr <kerr@hey.com>
2022-03-04 16:29:17 -0800

    Fix import

--- src/app/features/sidebar/plus-button.tsx
+++ src/app/features/sidebar/plus-button.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent} from "React"
+import React, {ChangeEvent} from "react"
 import styled from "styled-components"
 
 import useCallbackRef from "src/js/components/hooks/useCallbackRef"
