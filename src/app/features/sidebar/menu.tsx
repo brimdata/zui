df38530c98bf589749116720800e1eb3f07e6a12
James Kerr <kerr@hey.com>
2022-03-18 14:46:07 -0700

    Make tabs on the sidebar

--- src/app/features/sidebar/menu.tsx
+++ src/app/features/sidebar/menu.tsx
@@ -1,49 +1,89 @@
-import React, {ReactNode} from "react"
+import React from "react"
 import styled from "styled-components"
 import Icon from "src/app/core/icon-temp"
-import {Item} from "./item"
 import {useDispatch} from "src/app/core/state"
 import {useSelector} from "react-redux"
 import Appearance from "src/js/state/Appearance"
-import {SectionName} from "src/js/state/Appearance/types"
 
-export const StyledMenu = styled.menu`
-  margin: 0 0 6px;
-  padding: 0;
+const BG = styled.div`
+  display: flex;
+  padding: 0 6px;
+
+  button {
+    background: none;
+    border: none;
+    display: flex;
+    opacity: 0.5;
+    border-radius: 5px;
+    padding: 0 6px;
+
+    span {
+      height: 22px;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+      border-bottom: 2px solid transparent;
+      padding: 4px;
+    }
+
+    svg {
+      width: 14px;
+      height: 14px;
+      margin-right: 4px;
+      fill: var(--foreground-color);
+      opacity: 0.5;
+      display: none;
+    }
+
+    &[aria-pressed="true"] {
+      opacity: 1;
+
+      span {
+        border-bottom: 2px solid var(--primary-color);
+      }
+    }
+
+    &:hover {
+      background: rgba(0, 0, 0, 0.05);
+    }
+
+    &:active {
+      background: rgba(0, 0, 0, 0.08);
+    }
+  }
 `
 
-const MenuItem = (props: {
-  name: SectionName
-  text: string
-  icon: ReactNode
-}) => {
-  const {icon, name, text} = props
+export function Menu() {
   const dispatch = useDispatch()
   const currentSectionName = useSelector(Appearance.getCurrentSectionName)
-  const isCurrent = name === currentSectionName
+  const onClick = (name) => () =>
+    dispatch(Appearance.setCurrentSectionName(name))
   return (
-    <Item
-      onClick={() => dispatch(Appearance.setCurrentSectionName(name))}
-      icon={icon}
-      text={text}
-      // @ts-ignore
-      state={{isSelected: isCurrent}}
-      // @ts-ignore
-      styles={{}}
-    />
-  )
-}
-
-export function Menu() {
-  return (
-    <StyledMenu>
-      <MenuItem name="pools" text="Pools" icon={<Icon name="pool" />} />
-      <MenuItem
-        name="queries"
-        text="Queries"
-        icon={<Icon name="doc-plain" />}
-      />
-      <MenuItem name="history" text="History" icon={<Icon name="history" />} />
-    </StyledMenu>
+    <BG>
+      <button
+        onClick={onClick("pools")}
+        aria-pressed={currentSectionName === "pools"}
+      >
+        <span>
+          <Icon name="pool" /> Pools
+        </span>
+      </button>
+      <button
+        onClick={onClick("queries")}
+        aria-pressed={currentSectionName === "queries"}
+      >
+        <span>
+          <Icon name="doc-plain" /> Queries
+        </span>
+      </button>
+      <button
+        onClick={onClick("history")}
+        aria-pressed={currentSectionName === "history"}
+      >
+        <span>
+          <Icon name="history" /> History
+        </span>
+      </button>
+    </BG>
   )
 }
