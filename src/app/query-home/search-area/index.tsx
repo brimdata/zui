influx6/brimdata-brim

refactor right pane, add versions section

src/app/query-home/search-area/index.tsx
@@ -8,12 +8,22 @@ import FromPinPicker from "./from-pin-picker"
 import {useSelector} from "react-redux"
 import SearchBar from "src/js/state/SearchBar"
 import Current from "src/js/state/Current"
+import {cssVar} from "../../../js/lib/cssVar"
 
-const Group = styled.div<{flex: number; isMultiLine: boolean}>`
+const Group = styled.div<{isMultiLine: boolean}>`
   display: flex;
-  flex: ${(p) => p.flex || "initial"};
   flex-direction: column;
   padding: ${(p) => (p.isMultiLine ? "0" : "0 16px 10px")};
+`
+
+const Wrap = styled.div<{isMultiLine: boolean}>`
+  padding-top: 12px;
+  ${(p) =>
+    p.isMultiLine &&
+    `
+    box-shadow: 0 0 1px 0 rgba(0, 0, 0, 0.16);
+    background: var(--editor-background);
+  `}
 `
 
 export default function SearchArea() {
@@ -22,9 +32,9 @@ export default function SearchArea() {
   const query = useSelector(Current.getQuery)
 
   return (
-    <>
+    <Wrap isMultiLine={isMultiLine}>
       <FromPinPicker isDisabled={query.isReadOnly} />
-      <Group isMultiLine={isMultiLine} flex={1}>
+      <Group isMultiLine={isMultiLine}>
         <Input
           inputValue={inputValue}
           isMultiLine={isMultiLine}
@@ -33,6 +43,6 @@ export default function SearchArea() {
         <Error />
         <Pins />
       </Group>
-    </>
+    </Wrap>
   )
 }

brimdata/zui

src/app/query-home/search-area/index.tsx
@@ -9,11 +9,20 @@ import {useSelector} from "react-redux"
 import SearchBar from "src/js/state/SearchBar"
 import Current from "src/js/state/Current"
 
-const Group = styled.div<{flex: number; multiline: boolean}>`
+const Group = styled.div<{multiline: boolean}>`
   display: flex;
-  flex: ${(p) => p.flex || "initial"};
   flex-direction: column;
   padding: ${(p) => (p.multiline ? "0" : "0 16px 10px")};
+`
+
+const Wrap = styled.div<{isMultiLine: boolean}>`
+  padding-top: 12px;
+  ${(p) =>
+    p.isMultiLine &&
+    `
+    box-shadow: 0 0 1px 0 rgba(0, 0, 0, 0.16);
+    background: var(--editor-background);
+  `}
 `
 
 export default function SearchArea() {
@@ -22,9 +31,9 @@ export default function SearchArea() {
   const query = useSelector(Current.getQuery)
 
   return (
-    <>
+    <Wrap isMultiLine={multiline}>
       <FromPinPicker isDisabled={query.isReadOnly} />
-      <Group multiline={multiline} flex={1}>
+      <Group multiline={multiline}>
         <Input
           value={value}
           multiline={multiline}
@@ -33,6 +42,6 @@ export default function SearchArea() {
         <Error />
         <Pins />
       </Group>
-    </>
+    </Wrap>
   )
 }