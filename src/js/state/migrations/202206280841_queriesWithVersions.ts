ff7467b411b4de43e838f41e01ba80f6687b551e
Mason Fish <mason.e.fish@gmail.com>
2022-06-29 13:08:26 -0700

    migrate old -> new queries (#2415)

---
+++ src/js/state/migrations/202206280841_queriesWithVersions.ts
@@ -0,0 +1,38 @@
+import {getAllStates} from "./utils/getTestState"
+import TreeModel from "tree-model"
+import {nanoid} from "@reduxjs/toolkit"
+
+export const flattenQueryTree = (root) => {
+  if (!root) return []
+  return new TreeModel({childrenPropertyName: "items"}).parse(root).all((n) => {
+    return !("items" in n.model)
+  })
+}
+
+export default function queriesWithVersions(state: any) {
+  for (const s of getAllStates(state)) {
+    // remove brim standard lib
+    const brimLibNdx = s.queries?.items?.findIndex(({id}) => id === "brim")
+    if (brimLibNdx > -1) s.queries?.items?.splice(brimLibNdx, 1)
+
+    const versions = {}
+    for (const q of flattenQueryTree(s.queries)) {
+      const version = {
+        version: nanoid(),
+        ts: new Date().toISOString(),
+        value: q.model.value,
+      }
+      versions[q.model.id] = {
+        ids: [version.version],
+        entities: {
+          [version.version]: version,
+        },
+      }
+      delete q.model.value
+      delete q.model.from
+    }
+    s.queryVersions = versions
+  }
+
+  return state
+}
