b9d45c7d66b9e07a60ad342f7cfe597e69481419
Mason Fish <mason.e.fish@gmail.com>
2022-07-11 17:00:11 -0700

    Change events request accept header (#2404)

--- packages/e2e-tests/tests/zed-events.spec.ts
+++ packages/e2e-tests/tests/zed-events.spec.ts
@@ -1,5 +1,5 @@
 import TestApp from "../helpers/test-app"
-import {test} from "@playwright/test"
+import {test, expect} from "@playwright/test"
 
 test.describe("Handle Zed server events", () => {
   const app = new TestApp("Zed Events")
@@ -15,9 +15,9 @@ test.describe("Handle Zed server events", () => {
     const {
       pool: {id},
     } = await app.zealot.createPool("test-pool-new")
-    await app.mainWin.locator("text=test-pool-new")
+    await expect(app.mainWin.locator("text=test-pool-new")).toBeVisible()
     await app.zealot.updatePool(id, {name: "test-pool-update"})
-    await app.mainWin.locator("text=test-pool-update")
+    await expect(app.mainWin.locator("text=test-pool-update")).toBeVisible()
     await app.zealot.deletePool(id)
     await app.mainWin.waitForSelector("text=test-pool-update", {
       state: "detached",
