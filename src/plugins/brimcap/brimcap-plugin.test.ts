aa0c28792d8a40018bc56ff1d2faf7ebc8e09e2b
James Kerr <kerr@hey.com>
2022-08-02 15:47:37 -0700

    Try again for windows

--- src/plugins/brimcap/brimcap-plugin.test.ts
+++ src/plugins/brimcap/brimcap-plugin.test.ts
@@ -6,6 +6,5 @@ test("brimcap path", () => {
   const api = setupApi()
   const plugin = new BrimcapPlugin(api)
   const path = normalize("/test/app/zdeps/brimcap")
-  const match = new RegExp(`${path}(.exe)?`)
-  expect(plugin.brimcapBinPath).toMatch(match)
+  expect(plugin.brimcapBinPath).toContain(path)
 })
