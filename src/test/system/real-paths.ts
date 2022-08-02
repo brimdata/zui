2b4dee7f1d70f4119db7468310fac997cdd87d3c
James Kerr <kerr@hey.com>
2022-08-02 12:58:52 -0700

    Correct directory and use it in appMenu.test.ts

--- src/test/system/real-paths.ts
+++ src/test/system/real-paths.ts
@@ -4,9 +4,9 @@ import {join} from "path"
 
 app.getPath = jest.fn((name) => {
   if (name === "temp") return os.tmpdir()
-  return join(__dirname, "../../../../run/unit/data")
+  return join(__dirname, "../../../run/unit/data")
 })
 
 app.getAppPath = jest.fn(() => {
-  return join(__dirname, "../../../../")
+  return join(__dirname, "../../../")
 })
