ce3a6691cedf73ea5346c210a850f6504134021f
James Kerr <kerr@hey.com>
2022-08-02 12:55:32 -0700

    This gets loaded when we start?

--- src/test/system/real-paths.ts
+++ src/test/system/real-paths.ts
@@ -2,11 +2,13 @@ import {app} from "electron"
 import os from "os"
 import {join} from "path"
 
-app.getPath = jest.fn((name) => {
-  if (name === "temp") return os.tmpdir()
-  return join(__dirname, "../../../../run/unit/data")
-})
+if ("jest" in globalThis) {
+  app.getPath = jest.fn((name) => {
+    if (name === "temp") return os.tmpdir()
+    return join(__dirname, "../../../../run/unit/data")
+  })
 
-app.getAppPath = jest.fn(() => {
-  return join(__dirname, "../../../../")
-})
+  app.getAppPath = jest.fn(() => {
+    return join(__dirname, "../../../../")
+  })
+}
