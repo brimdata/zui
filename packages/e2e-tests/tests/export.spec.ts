0c3f04c911ba1e2eb0164558a9eb9eda70e16483
James Kerr <kerr@hey.com>
2022-12-07 15:56:12 -0800

    End to end tests are fixed

--- packages/e2e-tests/tests/export.spec.ts
+++ packages/e2e-tests/tests/export.spec.ts
@@ -7,10 +7,10 @@ import fsExtra from "fs-extra"
 
 const tempDir = os.tmpdir()
 const formats = [
-  {label: "zng", expectedSize: 3648},
+  {label: "zng", expectedSize: 3643},
   {label: "zson", expectedSize: 15137},
   {label: "zjson", expectedSize: 18007},
-  {label: "zeek", expectedSize: 7520},
+  {label: "zeek", expectedSize: 9772},
   {label: "json", expectedSize: 13659},
   {label: "ndjson", expectedSize: 13657},
   {label: "csv", expectedSize: 12208},
@@ -28,6 +28,7 @@ test.describe("Export tests", () => {
       .locator('#app-root button:above(:text("Query Pool"))')
       .first()
       .click()
+    await app.query("sort ts")
   })
 
   test.afterAll(async () => {
