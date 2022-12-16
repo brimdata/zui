0f1dadcf64adcc030877cebdb891c4b8b155f4f8
Phil Rzewski <phil@brimdata.io>
2022-12-16 19:07:33 +0000

    Tidy the format selections for load and export (#2622)

--- src/components/data-format-select.tsx
+++ src/components/data-format-select.tsx
@@ -4,15 +4,15 @@ import SelectInput from "src/js/components/common/forms/SelectInput"
 export function DataFormatSelect(props: JSX.IntrinsicElements["select"]) {
   return (
     <SelectInput {...props}>
-      <option value="auto">Auto detect</option>
-      <option value="csv">csv</option>
-      <option value="json">json</option>
-      <option value="line">line</option>
-      <option value="parquet">parquet</option>
-      <option value="zeek">zeek</option>
-      <option value="zjson">zjson</option>
-      <option value="zng">zng</option>
-      <option value="zson">zson</option>
+      <option value="auto">Auto-detect</option>
+      <option value="csv">CSV</option>
+      <option value="json">JSON</option>
+      <option value="line">Line</option>
+      <option value="parquet">Parquet</option>
+      <option value="zeek">Zeek</option>
+      <option value="zjson">ZJSON</option>
+      <option value="zng">ZNG</option>
+      <option value="zson">ZSON</option>
     </SelectInput>
   )
 }
