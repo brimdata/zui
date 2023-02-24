abe58ff6a81687a825b3466e54fbed084164bda0
Phil Rzewski <phil@brimdata.io>
2023-02-23 18:10:43 -0800

    Don't cache isFirstRun

--- src/js/electron/first-run.ts
+++ src/js/electron/first-run.ts
@@ -5,17 +5,14 @@ import {app} from "electron"
 
 export const getPath = () => path.join(app.getPath("userData"), "first-run")
 
-let firstRun = undefined
-
 export function isFirstRun() {
   return new Promise<boolean>((res) => {
-    if (firstRun !== undefined) return res(firstRun)
     fs.stat(getPath(), (err) => {
       if (err) {
         fs.createFile(getPath()).catch((e) => log.error(e))
-        res((firstRun = true))
+        res(true)
       } else {
-        res((firstRun = false))
+        res(false)
       }
     })
   })
