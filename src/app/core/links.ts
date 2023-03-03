8a1fa8942a6a21719efe9175128f39c4f4fd635a
Phil Rzewski <phil@brimdata.io>
2023-03-03 13:50:33 -0800

    Move ZUI_DOCS_ROOT down by ZUI_DOWNLOAD

--- src/app/core/links.ts
+++ src/app/core/links.ts
@@ -4,12 +4,12 @@ const currentZedTag = pkg.dependencies.zed.split("#")[1] || "main"
 const zedDocsTag = currentZedTag.startsWith("v") ? currentZedTag : "next"
 
 export default {
-  ZUI_DOCS_ROOT: `https://zui.brimdata.io/docs`,
   ZED_DOCS_ROOT: `https://zed.brimdata.io/docs/${zedDocsTag}/commands/zed`,
   ZED_DOCS_LANGUAGE: `https://zed.brimdata.io/docs/${zedDocsTag}/language`,
   ZED_DOCS_FORMATS_ZJSON: `https://zed.brimdata.io/docs/${zedDocsTag}/formats/zjson`,
   ZED_DOCS_FORMATS_ZNG: `https://zed.brimdata.io/docs/${zedDocsTag}/formats/zng`,
   ZED_DOCS_FORMATS_ZSON: `https://zed.brimdata.io/docs/${zedDocsTag}/formats/zson`,
   ZED_DOCS_FORMATS_VNG: `https://zed.brimdata.io/docs/${zedDocsTag}/formats/vng`,
+  ZUI_DOCS_ROOT: `https://zui.brimdata.io/docs`,
   ZUI_DOWNLOAD: `https://www.brimdata.io/download/`,
 }
