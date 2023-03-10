35e3c219586b3cca852913a750193a2459350b85
Phil Rzewski <phil@brimdata.io>
2023-03-09 19:03:41 -0800

    Offer similar link in another context

---
+++ src/panes/results-pane/errors/network-error.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+import styled from "styled-components"
+import ToolbarButton from "src/app/query-home/toolbar/actions/button"
+import {shell} from "electron"
+import links from "src/app/core/links"
+
+const BG = styled.div`
+  margin-top: 16px;
+  padding: 24px;
+  width: 100%;
+  height: 100%;
+  h2 {
+    margin-bottom: 0.5em;
+  }
+`
+
+const StyledP = styled.p`
+  margin: 18px 0 0 0;
+  color: var(--aqua);
+  ${(p) => p.theme.typography.labelNormal}
+`
+
+const StyledButton = styled(ToolbarButton)`
+  margin: 10px 0 0 0;
+`
+
+const viewTroubleshootingDocs = async () => {
+  shell.openExternal(links.ZUI_DOCS_CONNNECTION_TROUBLESHOOTING)
+}
+
+export function isNetworkError(e: unknown) {
+  return e === "The service could not be reached."
+}
+
+export function NetworkError(props: {error: string}) {
+  return (
+    <BG>
+      <h2>Error</h2>
+      <StyledP>{props.error}</StyledP>
+      <StyledButton
+        onClick={viewTroubleshootingDocs}
+        text="View Troubleshooting Docs"
+      />
+    </BG>
+  )
+}
