8ceba5ed39b0c6418c22ef9ef3fc6220f8dde41f
James Kerr <kerr@hey.com>
2023-05-05 10:51:31 -0700

    This now points to the zeps for the zed binary

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -4,7 +4,3 @@ run("node", "scripts/download-zdeps", {
   desc: "Downloading zdeps (skip with ZDEPS=false yarn)",
   if: process.env.ZDEPS !== "false",
 })
-
-run("yarn", "workspaces foreach -t --no-private run build", {
-  desc: "Building dependent workspaces",
-})
