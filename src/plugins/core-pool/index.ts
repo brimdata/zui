021a4d846240bd1a020c8d63bfe96fd38f0132b7
James Kerr <kerr@hey.com>
2023-06-30 10:28:10 -0700

    Create settings with defaults when a pool is created

--- src/plugins/core-pool/index.ts
+++ src/plugins/core-pool/index.ts
@@ -1,10 +1,13 @@
 import {FieldPath} from "src/core/field-path"
+import {createSettings} from "src/domain/pools/operations"
 import {pools, PluginContext} from "src/zui"
 
 export function activate(_ctx: PluginContext) {
   pools.on("create", ({pool}) => {
-    const poolKeyAccessor = new FieldPath(pool.layout.keys[0]).toString()
+    createSettings.run(pool.id)
 
-    pools.configure(pool.id).set("timeField", poolKeyAccessor)
+    pools
+      .configure(pool.id)
+      .set("timeField", new FieldPath(pool.layout.keys[0]).toString())
   })
 }
