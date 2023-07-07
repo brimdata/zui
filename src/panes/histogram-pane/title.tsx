15eaab928356c06427b79376799531a550d59fa7
James Kerr <kerr@hey.com>
2023-07-07 15:54:52 -0700

    Add keys to array

--- src/panes/histogram-pane/title.tsx
+++ src/panes/histogram-pane/title.tsx
@@ -5,6 +5,7 @@ import {State} from "src/js/state/types"
 import Current from "src/js/state/Current"
 import Histogram from "src/js/state/Histogram"
 import * as d3 from "d3"
+import {Fragment} from "react"
 
 // Make all this data change together
 // The null count should not appear if there is no data
@@ -22,23 +23,23 @@ export function Title() {
 
   if (nullCount) {
     content.push(
-      <>
+      <Fragment key="1">
         {format(nullCount)} null {timeField} values •{" "}
-      </>
+      </Fragment>
     )
   }
   if (missingCount) {
     content.push(
-      <>
+      <Fragment key="2">
         {format(missingCount)} missing {timeField} values •{" "}
-      </>
+      </Fragment>
     )
   }
 
   content.push(
-    <>
+    <Fragment key="3">
       counts by <i>{timeField}</i> and <i>{colorField}</i>
-    </>
+    </Fragment>
   )
 
   return <p className={styles.title}>{content}</p>
