367846ec8a04ad4739342cd543decbe9c60b4870
James Kerr <kerr@hey.com>
2023-07-10 11:31:20 -0700

    Create migrations for urls

--- src/js/state/migrations/202307101053_migrateUrls.test.ts
+++ src/js/state/migrations/202307101053_migrateUrls.test.ts
@@ -1,19 +1,13 @@
 import {migrate} from "src/test/unit/helpers/migrate"
-
-function removeLakeFromUrl(pathname) {
-  if (/^\/lake\/.*\//.test(pathname)) {
-    // Remove the lake segments from the url
-    return "/" + pathname.split("/").slice(3).join("/")
-  } else {
-    return pathname
-  }
-}
+import {getAllStates} from "./utils/getTestState"
+import {removeLakeFromUrl} from "./202307101053_migrateUrls"
 
 const expectations = {
-  "/lake/localhost:9867/query/123/version/2323": "/query/123/version/2323",
-  "/lake/localhost:9867/pool/123": "/pool/123",
-  "/lake/localhost:9867/release-notes": "/release-notes",
-  "/lake/123abc/welcome": "/welcome",
+  "/lakes/localhost:9867": "/",
+  "/lakes/localhost:9867/query/123/version/2323": "/query/123/version/2323",
+  "/lakes/localhost:9867/pool/123": "/pool/123",
+  "/lakes/localhost:9867/release-notes": "/release-notes",
+  "/lakes/123abc/welcome": "/welcome",
   "/welcome": "/welcome",
   "/pool/123": "/pool/123",
   "/": "/",
@@ -25,8 +19,29 @@ for (const key in expectations) {
   })
 }
 
+function collectAllUrls(state) {
+  let urls = []
+  for (const window of getAllStates(state)) {
+    const histories = window.tabHistories
+    if (!histories) continue
+
+    for (const id in histories.entities) {
+      urls = urls.concat(histories.entities[id].entries)
+    }
+  }
+  return urls
+}
+
 test("migrating 202307101053_migrateUrls", async () => {
   const next = await migrate({state: "v1.0.1", to: "202307101053"})
 
-  expect(next).toBe("what you'd expect")
+  const urls = collectAllUrls(next)
+  expect(urls).toEqual([
+    "/",
+    "/pools/new",
+    "/pools/0x1139f717ead61a50518e8af0af9fa70836893148",
+    "/queries/vYXJGwpRlfWYc3VEwzwhw/versions/4MSGELmT0aatQA-0EBUzB",
+    "/queries/vYXJGwpRlfWYc3VEwzwhw/versions/Oy9bEnaX1Ho7dhqakXyNN",
+    "/release-notes",
+  ])
 })
--- src/js/state/migrations/202307101053_migrateUrls.ts
+++ src/js/state/migrations/202307101053_migrateUrls.ts
@@ -1,5 +1,23 @@
+import {getAllStates} from "./utils/getTestState"
+
+export function removeLakeFromUrl(pathname: string) {
+  if (/^\/lakes\/.*\/?/.test(pathname)) {
+    // Remove the lake segments from the url
+    return "/" + pathname.split("/").slice(3).join("/")
+  } else {
+    return pathname
+  }
+}
 
 export default function migrateUrls(state: any) {
-  // Migrate state here
+  for (const window of getAllStates(state)) {
+    const histories = window.tabHistories
+    if (!histories) continue
+
+    for (const id in histories.entities) {
+      const urls = histories.entities[id].entries
+      histories.entities[id].entries = urls.map(removeLakeFromUrl)
+    }
+  }
   return state
 }
