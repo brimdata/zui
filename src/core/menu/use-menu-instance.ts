de5f98deb5a2204d9177d232956fc60f3422e610
James Kerr <kerr@hey.com>
2023-07-20 15:12:28 -0700

    Update Result Menu Items Whenever the Tab Changes (#2811)
    Fixes #2808

--- src/core/menu/use-menu-instance.ts
+++ src/core/menu/use-menu-instance.ts
@@ -1,4 +1,4 @@
-import {useEffect, useMemo} from "react"
+import {useLayoutEffect, useMemo} from "react"
 import {useSelector} from "react-redux"
 import {useDispatch} from "src/app/core/state"
 import ResultsToolbarMenu from "src/js/state/ResultsToolbar"
@@ -7,6 +7,7 @@ import {createSelector} from "@reduxjs/toolkit"
 import Layout from "src/js/state/Layout"
 import {compile} from "../when/compile"
 import {invoke} from "../invoke"
+import {useTabId} from "src/app/core/hooks/use-tab-id"
 
 /**
  * 1. get the items from redux
@@ -26,13 +27,14 @@ export function useMenuInstance(name: string) {
   const dispatch = useDispatch()
   const items = useSelector(ResultsToolbarMenu.get)
   const whenContext = useSelector(getWhenContext)
+  const tabId = useTabId()
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     invoke("getMenuTemplateOp", name).then((template) => {
       const items = compileMenuItems(template, whenContext)
       dispatch(setMenu(name, items))
     })
-  }, [whenContext])
+  }, [whenContext, tabId])
 
   return useMemo(() => new BuiltMenu({id: name}, items), [items])
 }
