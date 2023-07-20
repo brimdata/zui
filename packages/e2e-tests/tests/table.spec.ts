d49b7d1e8601e958e528c5c03443da9b608bc661
James Kerr <kerr@hey.com>
2023-07-20 09:29:25 -0700

    All projects can build now

--- packages/e2e-tests/tests/table.spec.ts
+++ packages/e2e-tests/tests/table.spec.ts
@@ -1,26 +1,26 @@
-import {expect, test} from "@playwright/test"
-import {getPath} from "packages/zui-test-data"
-import TestApp from "../helpers/test-app"
+import { expect, test } from '@playwright/test';
+import { getPath } from 'zui-test-data';
+import TestApp from '../helpers/test-app';
 
-test.describe("Table Testing", () => {
-  const app = new TestApp("Table Testing")
+test.describe('Table Testing', () => {
+  const app = new TestApp('Table Testing');
 
   test.beforeAll(async () => {
-    await app.init()
-  })
+    await app.init();
+  });
 
   test.afterAll(async () => {
-    await app.shutdown()
-  })
+    await app.shutdown();
+  });
 
-  test("named type shows columns", async () => {
-    const path = getPath("named-type.zson")
-    await app.createPool([path])
-    await app.mainWin.getByRole("button", {name: "Query Pool"}).click()
-    await app.query("yield value.after") // This is a named type
-    const columnheader = app.results.getByRole("columnheader")
-    await columnheader.first().waitFor()
-    const texts = await columnheader.allInnerTexts()
-    expect(texts).toEqual(["Id", "IsDeleted"])
-  })
-})
+  test('named type shows columns', async () => {
+    const path = getPath('named-type.zson');
+    await app.createPool([path]);
+    await app.mainWin.getByRole('button', { name: 'Query Pool' }).click();
+    await app.query('yield value.after'); // This is a named type
+    const columnheader = app.results.getByRole('columnheader');
+    await columnheader.first().waitFor();
+    const texts = await columnheader.allInnerTexts();
+    expect(texts).toEqual(['Id', 'IsDeleted']);
+  });
+});
