bf950d7a6032ca0bc157322aa184dc8a1a5c1a76
James Kerr <kerr@hey.com>
2023-07-27 10:34:55 -0700

    Basic Zed Editor Working Really Well

---
+++ apps/zui/src/app/query-home/search-area/zed-editor.tsx
@@ -0,0 +1,37 @@
+import {Editor} from "@monaco-editor/react"
+import {useEffect, useRef} from "react"
+
+export function ZedEditor(props: {
+  path: string
+  value: string
+  onChange: (value: string | undefined, ev: any) => void
+  autoFocus?: boolean
+}) {
+  const ref = useRef<any>()
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (ref.current) {
+        ref.current.focus()
+      }
+    })
+  }, [props.path, props.value])
+
+  return (
+    <Editor
+      height="100%"
+      width="100%"
+      value={props.value}
+      onChange={props.onChange}
+      options={{
+        minimap: {enabled: false},
+        renderLineHighlightOnlyWhenFocus: true,
+        renderControlCharacters: false,
+      }}
+      onMount={(editor) => {
+        ref.current = editor
+      }}
+      path={props.path}
+    />
+  )
+}
