3d3b66d929649659ae23073b2715d8f17861de9f
Phil Rzewski <phil@brimdata.io>
2023-08-25 10:41:31 -0700

    Only use Meta+KeyA combo to Select All on macOS (#2842)

--- packages/e2e-tests/helpers/test-app.ts
+++ packages/e2e-tests/helpers/test-app.ts
@@ -89,7 +89,7 @@ export default class TestApp {
 
   async setEditor(zed: string) {
     await this.mainWin.locator('[aria-label=main-editor]').click();
-    await this.mainWin.keyboard.press('Meta+KeyA');
+    await this.mainWin.keyboard.press(isMac() ? 'Meta+KeyA' : 'Control+KeyA');
     await this.mainWin.keyboard.type(zed);
   }
 
