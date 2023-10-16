2e5328db38dc9bc7197bdab3f7c31b6270e286ef
James Kerr <kerr@hey.com>
2023-10-16 13:41:27 -0700

    Make sure the pool exists

--- apps/zui/src/domain/loads/handlers/preview-load-files.ts
+++ apps/zui/src/domain/loads/handlers/preview-load-files.ts
@@ -3,12 +3,18 @@ import {
   loadFiles as poolsLoadFiles,
 } from "src/app/commands/pools"
 import {createHandler} from "src/core/handlers"
+import Current from "src/js/state/Current"
 import LoadDataForm from "src/js/state/LoadDataForm"
+import Pools from "src/js/state/Pools"
 
 export const previewLoadFiles = createHandler(
-  async ({dispatch, invoke}, opts: {files: string[]; poolId?: string}) => {
+  async (
+    {dispatch, invoke, select},
+    opts: {files: string[]; poolId?: string}
+  ) => {
     const files = await invoke("loads.getFileTypes", opts.files)
 
+    // This is wrong
     if (files.length === 1 && files[0].type === "pcap") {
       if (opts.poolId) {
         poolsLoadFiles.run(opts.poolId, opts.files)
@@ -16,7 +22,9 @@ export const previewLoadFiles = createHandler(
         createAndLoadFiles.run(opts.files)
       }
     } else {
-      dispatch(LoadDataForm.setPoolId(opts.poolId))
+      const lakeId = select(Current.getLakeId)
+      const pool = select(Pools.get(lakeId, opts.poolId))
+      dispatch(LoadDataForm.setPoolId(pool ? pool.id : null))
       dispatch(LoadDataForm.setFiles(opts.files))
       dispatch(LoadDataForm.setShow(true))
     }
