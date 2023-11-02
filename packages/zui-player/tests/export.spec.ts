74dea1a3eafa9c6b29fac411926471fbd120c464
Phil Rzewski <phil@brimdata.io>
2023-11-02 08:57:41 -0700

    Adapt to change in VNG format from zed/4834 (#2874)

--- packages/zui-player/tests/export.spec.ts
+++ packages/zui-player/tests/export.spec.ts
@@ -11,7 +11,7 @@ const formats = [
   { label: 'CSV', expectedSize: 10851 },
   { label: 'JSON', expectedSize: 13659 },
   { label: 'NDJSON', expectedSize: 13657 },
-  { label: 'VNG', expectedSize: 7753 },
+  { label: 'VNG', expectedSize: 8025 },
   { label: 'Zeek', expectedSize: 10138 },
   { label: 'ZJSON', expectedSize: 18007 },
   { label: 'ZNG', expectedSize: 3745 },
