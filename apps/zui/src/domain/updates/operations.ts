91d76c9cb7dd4bfd9f50cae9bc5933cb970c6660
James Kerr <kerr@hey.com>
2023-11-02 11:38:18 -0700

    Added log messages

--- apps/zui/src/domain/updates/operations.ts
+++ apps/zui/src/domain/updates/operations.ts
@@ -2,6 +2,7 @@ import {createOperation} from "src/core/operations"
 import {updater} from "./updater"
 import Updates from "src/js/state/Updates"
 import {errorToString} from "src/util/error-to-string"
+import {info} from "src/core/log"
 
 export const open = createOperation("updates.open", ({main}) => {
   main.windows.activate("update")
@@ -11,13 +12,16 @@ export const check = createOperation(
   "updates.check",
   async ({main, dispatch}) => {
     try {
+      info("Checking for Updates...")
       dispatch(Updates.setIsChecking(true))
       const newVersion = await updater.check()
       if (newVersion) {
+        info("New Version Found: " + newVersion)
         dispatch(Updates.setNextVersion(newVersion))
         main.windows.activate("update")
       }
     } catch (e) {
+      info("Error Checking for Update: " + errorToString(e))
       dispatch(Updates.setError(errorToString(e)))
     } finally {
       dispatch(Updates.setIsChecking(false))
@@ -28,12 +32,14 @@ export const check = createOperation(
 export const install = createOperation(
   "updates.install",
   async ({dispatch}) => {
+    info("Installing Update")
     const onProgress = (n: number) => dispatch(Updates.setDownloadProgress(n))
     try {
       dispatch(Updates.setIsDownloading(true))
       dispatch(Updates.setDownloadProgress(0))
       await updater.install(onProgress)
     } catch (e) {
+      info("Error Installing")
       dispatch(Updates.setError(errorToString(e)))
     } finally {
       dispatch(Updates.setIsDownloading(false))
--- apps/zui/src/initializers/auto-update.ts
+++ apps/zui/src/initializers/auto-update.ts
@@ -4,11 +4,16 @@ import ConfigPropValues from "src/js/state/ConfigPropValues"
 import {Scheduler} from "src/domain/updates/scheduler"
 import {check} from "src/domain/updates/operations"
 import {select} from "src/core/main/select"
+import {info} from "src/core/log"
 
 export function initialize(_main: MainObject) {
-  if (env.isTest) return
+  if (env.isTest) {
+    info("Not Checking for Updates when env.isTest")
+    return
+  }
 
   const mode = select(ConfigPropValues.get("application", "updateMode"))
   const schedule = new Scheduler()
+  info("Starting updater in mode: ", mode)
   schedule.start(mode, check)
 }
