54dc7d72846db901c64f88efdd2a4a9290907248
James Kerr <kerr@hey.com>
2023-12-15 14:03:49 -0800

    Refactor Paste Operation

--- apps/zui/src/domain/loads/operations/paste.ts
+++ apps/zui/src/domain/loads/operations/paste.ts
@@ -5,8 +5,16 @@ import * as zui from "src/zui"
 import {sendToFocusedWindow} from "src/core/ipc"
 import {TempFileHolder} from "../temp-file-holder"
 
-const namespace = os.userInfo().username + "_pastes"
-const pastes = new TempFileHolder(namespace)
+const pastes = new TempFileHolder(os.userInfo().username + "_pastes")
+
+function removeFiles(loadFiles: string[]) {
+  for (let file of loadFiles) if (pastes.has(file)) pastes.removeFile(file)
+}
+
+zui.loads.on("error", (load) => removeFiles(load.files))
+zui.loads.on("abort", (load) => removeFiles(load.files))
+zui.loads.on("success", (load) => removeFiles(load.files))
+zui.app.on("quit", () => pastes.destroy())
 
 export const paste = createOperation("loads.paste", () => {
   const data = clipboard.readText()
@@ -14,14 +22,3 @@ export const paste = createOperation("loads.paste", () => {
   sendToFocusedWindow("loads.previewLoadFiles", {files: [file]})
   return file
 })
-
-function removeFiles(loadFiles: string[]) {
-  for (let file of loadFiles) {
-    if (pastes.has(file)) pastes.removeFile(file)
-  }
-}
-
-zui.loads.on("error", (load) => removeFiles(load.files))
-zui.loads.on("abort", (load) => removeFiles(load.files))
-zui.loads.on("success", (load) => removeFiles(load.files))
-zui.app.on("quit", () => pastes.destroy())
