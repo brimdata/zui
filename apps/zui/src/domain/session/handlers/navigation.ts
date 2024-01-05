5db81447df3f3039e8ce842a3f31972303ff2387
James Kerr <kerr@hey.com>
2024-01-05 15:56:40 -0800

    Fix Virus Total WhoIs and View Details

--- apps/zui/src/domain/session/handlers/navigation.ts
+++ apps/zui/src/domain/session/handlers/navigation.ts
@@ -2,11 +2,11 @@ import tabHistory from "src/app/router/tab-history"
 import {createHandler} from "src/core/handlers"
 import Current from "src/js/state/Current"
 import * as zed from "@brimdata/zed-js"
-import {viewLogDetail} from "src/js/flows/viewLogDetail"
 import virusTotal from "src/js/services/virusTotal"
 import Modal from "src/js/state/Modal"
 import {invoke} from "src/core/invoke"
 import {activatePane} from "src/domain/window/handlers"
+import Selection from "src/js/state/Selection"
 
 export const goBack = createHandler("session.goBack", ({dispatch}) => {
   dispatch(tabHistory.goBack())
@@ -29,29 +29,25 @@ export const canGoForward = createHandler(
 
 export const toggleHistoryPane = createHandler(
   "session.toggleHistoryPane",
-  () => {
-    activatePane("history")
-  }
+  () => activatePane("history")
 )
 
-export const showValueDetails = createHandler(
-  "session.showValueDetails",
-  ({dispatch}, value: zed.Value) => {
-    dispatch(viewLogDetail(value as zed.Record))
-    activatePane("detail")
-  }
+export const showValueDetails = createHandler("session.showValueDetails", () =>
+  activatePane("detail")
 )
 
 export const showWhoIs = createHandler(
   "session.showWhoIs",
-  ({dispatch}, value: zed.Type) => {
+  ({dispatch, select}) => {
+    const value = select(Selection.getValue)
     dispatch(Modal.show("whois", {addr: value.toString()}))
   }
 )
 
 export const openVirusTotal = createHandler(
   "session.openVirusTotal",
-  (_ctx, value: zed.Type) => {
+  ({select}) => {
+    const value = select(Selection.getValue)
     if (value instanceof zed.Primitive && !value.isUnset()) {
       invoke("openLinkOp", virusTotal.url(value.toString()))
     }
