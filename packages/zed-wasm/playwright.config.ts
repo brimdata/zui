68b03c963a8eb36af49cb183b4855c734acffae2
James Kerr <kerr@hey.com>
2024-01-17 09:09:30 -0800

    Only test chromium

--- packages/zed-wasm/playwright.config.ts
+++ packages/zed-wasm/playwright.config.ts
@@ -42,17 +42,15 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
+    //     {
+    //       name: 'firefox',
+    //       use: { ...devices['Desktop Firefox'] },
+    //     },
+    //
+    //     {
+    //       name: 'webkit',
+    //       use: { ...devices['Desktop Safari'] },
+    //     },
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
