7c71b85b228d6d9527be4a27175841b908663b18
James Kerr <kerr@hey.com>
2024-01-22 09:11:51 -0800

    Add unit test for quick-load bad pcap

--- apps/zui/src/domain/loads/handlers/quick-load-files.test.ts
+++ apps/zui/src/domain/loads/handlers/quick-load-files.test.ts
@@ -2,9 +2,16 @@
 
 import {SystemTest} from "src/test/system"
 import {quickLoadFiles} from "./quick-load-files"
+import {getPath} from "zui-test-data"
+import {loads} from "src/zui"
 
 new SystemTest("quick-load")
 
-test("pcap", async () => {
-  await quickLoadFiles({files: ["/Users/jkerr/Downloads/VanSpy.pcapng"]})
+test("pcap", (done) => {
+  loads.on("error", (ref) => {
+    expect(ref.errors[0]).toContain("zeekrunner exited with code 1")
+    done()
+  })
+
+  quickLoadFiles({files: [getPath("vanspy.pcapng")]})
 })
