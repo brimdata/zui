7b7c5e86de16c6211e5c9544b29f316c602ad742
James Kerr <kerr@hey.com>
2024-02-09 15:37:08 -0800

    Remove Console

--- apps/zui/src/views/pool-form/data.ts
+++ apps/zui/src/views/pool-form/data.ts
@@ -1,6 +1,5 @@
 export function getPoolFormData(form: HTMLFormElement) {
   const data = new FormData(form)
-  console.log(Array.from(data.entries()))
   return {
     name: data.get("name"),
     key: data.get("key"),
