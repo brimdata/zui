86bf01fc035da5f2e37d024561a612e5bb38cef2
James Kerr <kerr@hey.com>
2024-02-27 11:23:23 -0800

    Add isFetching to QueryInfo

--- apps/zui/src/js/state/QueryInfo/reducer.ts
+++ apps/zui/src/js/state/QueryInfo/reducer.ts
@@ -1,17 +1,25 @@
 import {createSlice} from "@reduxjs/toolkit"
 
-const slice = createSlice({
-  name: "TAB_QUERY_INFO",
-  initialState: {
+const getInitialState = () => {
+  return {
+    isParsed: false,
     isSummarized: false,
     poolName: null,
     sorts: [],
     error: null,
     groupByKeys: [],
-  },
+  }
+}
+
+const slice = createSlice({
+  name: "TAB_QUERY_INFO",
+  initialState: getInitialState(),
   reducers: {
     set(_, action) {
       return action.payload
+    },
+    reset(_) {
+      return getInitialState()
     },
   },
 })
