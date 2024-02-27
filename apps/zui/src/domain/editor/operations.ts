86b8e5278abb0a6527bab21de33ab6b64400f89a
James Kerr <kerr@hey.com>
2024-02-27 11:20:27 -0800

    Use the current lake's compile endpoint

--- apps/zui/src/domain/editor/operations.ts
+++ apps/zui/src/domain/editor/operations.ts
@@ -1,7 +1,7 @@
 import {createOperation} from "src/core/operations"
+import {lake} from "src/zui"
 
 export const parse = createOperation("editor.parse", async (ctx, string) => {
-  const client = await ctx.main.createDefaultClient()
-  const resp = await client.compile(string)
+  const resp = await lake.client.compile(string)
   return resp.toJS()
 })
