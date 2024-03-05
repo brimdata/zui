6efaf2a6ad7a6fe9606982a5fe9cfa14c76e7966
James Kerr <kerr@hey.com>
2024-03-05 11:51:21 -0800

    Add a Run method to Async Task

--- apps/zui/src/modules/async-tasks/async-task.ts
+++ apps/zui/src/modules/async-tasks/async-task.ts
@@ -1,6 +1,9 @@
+import {isAbortError} from "src/util/is-abort-error"
+
 export class AsyncTask {
   private controller = new AbortController()
-  constructor(public tags: string[]) {}
+
+  constructor(public tags: string[], private destroy: () => void) {}
 
   containsAll(targetTags: string[]) {
     return targetTags.every((targetTag) => this.tags.includes(targetTag))
@@ -12,5 +15,16 @@ export class AsyncTask {
 
   get signal() {
     return this.controller.signal
+  }
+
+  async run<T>(taskBody: (signal: AbortSignal) => T) {
+    try {
+      return await taskBody(this.signal)
+    } catch (error) {
+      if (isAbortError(error)) return
+      throw error
+    } finally {
+      this.destroy()
+    }
   }
 }
--- apps/zui/src/modules/async-tasks/async-tasks.ts
+++ apps/zui/src/modules/async-tasks/async-tasks.ts
@@ -3,37 +3,31 @@ import {AsyncTask} from "./async-task"
 export class AsyncTasks {
   tasks: AsyncTask[] = []
 
-  create(tags: string[]) {
-    const task = new AsyncTask(tags)
+  create(tags: string[]): AsyncTask {
+    const task = new AsyncTask(tags, () => this.remove(task))
     this.tasks.push(task)
     return task
   }
 
+  remove(task: AsyncTask) {
+    const index = this.tasks.indexOf(task)
+    if (index >= 0) this.tasks.splice(index, 1)
+  }
+
   createOrReplace(tags: string[]) {
-    for (const task of this.where(tags)) {
-      task.abort()
-      this.remove(task)
-    }
+    this.abort(tags)
     return this.create(tags)
   }
 
   abort(tags: string[]) {
-    for (const task of this.where(tags)) {
-      task.abort()
-      this.remove(task)
-    }
+    for (const task of this.where(tags)) task.abort()
   }
 
   abortAll() {
     for (const task of this.tasks) task.abort()
-    this.tasks = []
   }
 
   private where(tags: string[]) {
     return this.tasks.filter((task) => task.containsAll(tags))
-  }
-
-  private remove(task: AsyncTask) {
-    return this.tasks.splice(this.tasks.indexOf(task), 1)
   }
 }
