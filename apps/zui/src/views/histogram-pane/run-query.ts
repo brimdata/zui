b953b1f372577e2255fbd6c1b25f7c7bf0003e49
James Kerr <kerr@hey.com>
2024-03-05 11:52:22 -0800

    Refactor Run Histogram Query

--- apps/zui/src/views/histogram-pane/run-query.ts
+++ apps/zui/src/views/histogram-pane/run-query.ts
@@ -10,111 +10,97 @@ import QueryInfo from "src/js/state/QueryInfo"
 import {query} from "src/domain/lake/handlers"
 
 export const HISTOGRAM_RESULTS = "histogram"
-const HISTOGRAM_TASK_ID = "run-histogram-query-task"
 
 export const runHistogramQuery = createHandler(
   async ({select, dispatch, waitForSelector, asyncTasks}) => {
     const tabId = select(Current.getTabId)
-    const {signal} = asyncTasks.createOrReplace([tabId, HISTOGRAM_TASK_ID])
+    const taskId = "run-histogram-query-task"
 
-    await waitForSelector(QueryInfo.getIsParsed, {signal}).toReturn(true)
-
-    const id = HISTOGRAM_RESULTS
-    const key = select(Current.getLocation).key
-    const version = select(Current.getVersion)
-    const poolId = select(Current.getPoolFromQuery)?.id
-    const baseQuery = select(Current.getQueryText)
-    const {timeField, colorField} = select((s) =>
-      PoolSettings.findWithDefaults(s, poolId)
-    )
-
-    function setup() {
-      dispatch(Results.init({id, tabId, key, query: ""}))
-      dispatch(Histogram.init())
-    }
-
-    function collect({rows}) {
-      dispatch(Results.setValues({id, tabId, values: rows}))
-    }
-
-    function error(error: Error) {
-      if (isAbortError(error)) return
-      dispatch(Results.error({id, tabId, error: error.message}))
-    }
-
-    function success() {
-      dispatch(Results.success({id, tabId}))
-    }
-
-    function isRangePin(p: QueryPin) {
-      return p.type === "time-range" && !p.disabled && p.field === timeField
-    }
-
-    function getPinRange() {
-      const rangePin = version.pins.find(isRangePin) as TimeRangeQueryPin
-      return rangePin
-        ? ([new Date(rangePin.from), new Date(rangePin.to)] as [Date, Date])
-        : null
-    }
-
-    async function getPoolRange() {
-      const queryText = `from ${poolId} | min(${timeField}), max(${timeField})`
-      const resp = await query(queryText, {signal})
-      const [{min, max}] = await resp.js()
-      if (!(min instanceof Date && max instanceof Date)) return null
-      return [min, max] as [Date, Date]
-    }
-
-    async function getNullTimeCount() {
-      // Newline after baseQuery in case it ends with a comment.
-      const queryText = `${baseQuery}\n | ${timeField} == null | count()`
-      try {
-        const resp = await query(queryText, {signal})
-        const [count] = await resp.js()
-        dispatch(Histogram.setNullXCount(count ?? 0))
-      } catch (e) {
-        if (isAbortError(e)) return
-        throw e
+    asyncTasks.createOrReplace([tabId, taskId]).run(async (signal) => {
+      await waitForSelector(QueryInfo.getIsParsed, {signal}).toReturn(true)
+      const id = HISTOGRAM_RESULTS
+      const key = select(Current.getLocation).key
+      const version = select(Current.getVersion)
+      const poolId = select(Current.getPoolFromQuery)?.id
+      const baseQuery = select(Current.getQueryText)
+      const {timeField, colorField} = select((s) =>
+        PoolSettings.findWithDefaults(s, poolId)
+      )
+      function getPinRange() {
+        const rangePin = version.pins.find(
+          (pin: QueryPin) =>
+            pin.type === "time-range" &&
+            !pin.disabled &&
+            pin.field === timeField
+        ) as TimeRangeQueryPin
+        return rangePin
+          ? ([new Date(rangePin.from), new Date(rangePin.to)] as [Date, Date])
+          : null
       }
-    }
-
-    async function getMissingTimeCount() {
-      // Newline after baseQuery in case it ends with a comment.
-      const queryText = `${baseQuery}\n | !has(${timeField}) | count()`
-      try {
+      async function getPoolRange() {
+        const queryText = `from ${poolId} | min(${timeField}), max(${timeField})`
         const resp = await query(queryText, {signal})
-        const [count] = await resp.js()
-        dispatch(Histogram.setMissingXCount(count ?? 0))
-      } catch (e) {
-        if (isAbortError(e)) return
-        throw e
+        const [{min, max}] = await resp.js()
+        if (!(min instanceof Date && max instanceof Date)) return null
+        return [min, max] as [Date, Date]
       }
-    }
 
-    async function run() {
-      const range = getPinRange() || (await getPoolRange())
-      if (!range)
-        throw new Error(`Unable to determine date range using '${timeField}'.`)
+      async function getNullTimeCount() {
+        // Newline after baseQuery in case it ends with a comment.
+        const queryText = `${baseQuery}\n | ${timeField} == null | count()`
+        try {
+          const resp = await query(queryText, {signal})
+          const [count] = await resp.js()
+          dispatch(Histogram.setNullXCount(count ?? 0))
+        } catch (e) {
+          if (isAbortError(e)) return
+          throw e
+        }
+      }
 
-      const {unit, number, fn} = getInterval(range)
-      const interval = `${number}${timeUnits[unit]}`
-      // Newline after baseQuery in case it ends with a comment.
-      const queryText = `${baseQuery}\n | ${timeField} != null | count() by time := bucket(${timeField}, ${interval}), group := ${colorField} | sort time`
-      const resp = await query(queryText, {signal})
-      dispatch(Histogram.setInterval({unit, number, fn}))
-      dispatch(Histogram.setRange(range))
-      resp.collect(collect)
-      getNullTimeCount()
-      getMissingTimeCount()
-      await resp.promise
-    }
+      async function getMissingTimeCount() {
+        // Newline after baseQuery in case it ends with a comment.
+        const queryText = `${baseQuery}\n | !has(${timeField}) | count()`
+        try {
+          const resp = await query(queryText, {signal})
+          const [count] = await resp.js()
+          dispatch(Histogram.setMissingXCount(count ?? 0))
+        } catch (e) {
+          if (isAbortError(e)) return
+          throw e
+        }
+      }
 
-    try {
-      setup()
-      await run()
-      success()
-    } catch (e) {
-      error(e)
-    }
+      try {
+        //setup
+        dispatch(Results.init({id, tabId, key, query: ""}))
+        dispatch(Histogram.init())
+        // run
+        const range = getPinRange() || (await getPoolRange())
+        if (!range)
+          throw new Error(
+            `Unable to determine date range using '${timeField}'.`
+          )
+
+        const {unit, number, fn} = getInterval(range)
+        const interval = `${number}${timeUnits[unit]}`
+        // Newline after baseQuery in case it ends with a comment.
+        const queryText = `${baseQuery}\n | ${timeField} != null | count() by time := bucket(${timeField}, ${interval}), group := ${colorField} | sort time`
+        const resp = await query(queryText, {signal})
+        dispatch(Histogram.setInterval({unit, number, fn}))
+        dispatch(Histogram.setRange(range))
+        resp.collect(({rows}) => {
+          dispatch(Results.setValues({id, tabId, values: rows}))
+        })
+        getNullTimeCount()
+        getMissingTimeCount()
+        await resp.promise
+        // success
+        dispatch(Results.success({id, tabId}))
+      } catch (error) {
+        if (isAbortError(error)) return
+        dispatch(Results.error({id, tabId, error: error.message}))
+      }
+    })
   }
 )
