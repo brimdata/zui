a50d35dd0f14a52b3df7ccf809e73e48d0bbbab7
Phil Rzewski <phil@brimdata.io>
2024-03-27 12:58:09 -0700

    Separate out pool load failure e2e test

--- packages/zui-player/ci.config.js
+++ packages/zui-player/ci.config.js
@@ -3,5 +3,5 @@ const baseConfig = require('./playwright.config');
 module.exports = {
   ...baseConfig,
   /* This is the list of flaky tests to ignore when running on CI */
-  testIgnore: /(pool-loads|pool-groups).spec/,
+  testIgnore: /(pool-load-fail.spec.ts|pool-groups).spec/,
 };
