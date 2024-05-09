78880addcafcac95a157a9f2c53c774ac13837b4
Phil Rzewski <phil@brimdata.io>
2024-05-09 16:46:28 -0700

    lint fix

--- apps/zui/src/core/electron-zed-client.ts
+++ apps/zui/src/core/electron-zed-client.ts
@@ -2,5 +2,6 @@ import {Client} from "@brimdata/zed-node"
 import {net} from "electron"
 
 export class ElectronZedClient extends Client {
-  fetch = (process.env.JEST_WORKER_ID === undefined) ? net.fetch : globalThis.fetch
+  fetch =
+    process.env.JEST_WORKER_ID === undefined ? net.fetch : globalThis.fetch
 }
--- apps/zui/src/core/electron-zed-lake.ts
+++ apps/zui/src/core/electron-zed-lake.ts
@@ -2,5 +2,6 @@ import {Lake} from "@brimdata/zed-node"
 import {net} from "electron"
 
 export class ElectronZedLake extends Lake {
-  fetch = (process.env.JEST_WORKER_ID === undefined) ? net.fetch : globalThis.fetch
+  fetch =
+    process.env.JEST_WORKER_ID === undefined ? net.fetch : globalThis.fetch
 }
