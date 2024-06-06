404496ffc55f1ebfce292baae4b8fee86172fc25
James Kerr <kerr@hey.com>
2024-06-06 13:51:39 -0700

    Use the electron mock

--- apps/zui/src/core/electron-zed-client.ts
+++ apps/zui/src/core/electron-zed-client.ts
@@ -2,6 +2,5 @@ import {Client} from "@brimdata/zed-node"
 import {net} from "electron"
 
 export class ElectronZedClient extends Client {
-  // This avoids attempting to use Electron net.fetch in Jest tests
-  fetch = net !== undefined ? net.fetch : globalThis.fetch
+  fetch = net.fetch
 }
--- apps/zui/src/core/electron-zed-lake.ts
+++ apps/zui/src/core/electron-zed-lake.ts
@@ -2,6 +2,5 @@ import {Lake} from "@brimdata/zed-node"
 import {net} from "electron"
 
 export class ElectronZedLake extends Lake {
-  // This avoids attempting to use Electron net.fetch in Jest tests
-  fetch = net !== undefined ? net.fetch : globalThis.fetch
+  fetch = net.fetch
 }
