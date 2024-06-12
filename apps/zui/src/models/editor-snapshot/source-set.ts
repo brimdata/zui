0b2805176cf580ebae51ff8c9842e73dde8a63ab
James Kerr <kerr@hey.com>
2024-06-12 10:39:03 -0700

    Fix source set to add the pipe at the right moment

--- apps/zui/src/models/editor-snapshot/source-set.ts
+++ apps/zui/src/models/editor-snapshot/source-set.ts
@@ -11,21 +11,21 @@ export class SourceSet {
     prefixes
       .concat(mainText)
       .filter((snippet) => snippet.trim().length)
-      .forEach((snippet, index) => {
-        const isFirst = index === 0
-        const text = isFirst ? snippet : " | " + snippet
-        this.appendSource(text)
-      })
+      .forEach((snippet) => this.appendSource(snippet))
 
     if (!this.sources.length) this.appendSource("*")
   }
 
   appendSource(text: string) {
+    const isFirst = this.contents.length === 0
+    if (!isFirst) this.contents += " | "
     this.sources.push(new Source(this.contents.length, text))
     this.contents += text
   }
 
   sourceOf(pos: number) {
-    return this.sources.find((s) => pos >= s.start && pos <= s.start + s.length)
+    return this.sources.find(
+      (s) => pos >= s.start && pos < s.start + s.length + 1
+    )
   }
 }
