3fca224e6b3353db5033d5d17272931c599ead5d
James Kerr <kerr@hey.com>
2024-06-18 16:19:32 -0700

    Only check if you can paginate if the lake has a describe endpoint

--- apps/zui/src/views/results-pane/run-results-query.tsx
+++ apps/zui/src/views/results-pane/run-results-query.tsx
@@ -3,6 +3,7 @@ import {firstPage} from "src/core/query/run"
 import Current from "src/js/state/Current"
 import QueryInfo from "src/js/state/QueryInfo"
 import Results from "src/js/state/Results"
+import {Active} from "src/models/active"
 
 export const RESULTS_QUERY = "zui-results/main"
 export const RESULTS_QUERY_COUNT = "zui-results/main-count"
@@ -14,8 +15,11 @@ export const runResultsMain = createHandler(
     dispatch(firstPage({id: RESULTS_QUERY, query}))
 
     // See if we can paginate this query
-    await waitForSelector(QueryInfo.getIsParsed).toReturn(true)
-    const canPaginate = !select(QueryInfo.hasAggregation)
+    let canPaginate = false
+    if (Active.lake.features.describe) {
+      await waitForSelector(QueryInfo.getIsParsed).toReturn(true)
+      canPaginate = !select(QueryInfo.hasAggregation)
+    }
     dispatch(Results.setCanPaginate({id: RESULTS_QUERY, canPaginate, tabId}))
   }
 )
