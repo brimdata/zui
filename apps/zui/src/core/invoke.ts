7b670bc4343592a350a47a70dc9476f71d45ae6c
James Kerr <kerr@hey.com>
2024-06-25 16:36:48 -0700

    Remove the error prefix electron tacks on from remote calls

--- apps/zui/src/core/invoke.ts
+++ apps/zui/src/core/invoke.ts
@@ -4,5 +4,12 @@ export function invoke<K extends OperationName>(
   name: K,
   ...args: Parameters<Operations[K]>
 ): Promise<Awaited<ReturnType<Operations[K]>>> {
-  return global.zui.invoke(name, ...args)
+  return global.zui.invoke(name, ...args).catch((error) => {
+    throw new Error(
+      error
+        .toString()
+        // Here we are stripping the error prefix that electron tacks on.
+        .replace(`Error: Error invoking remote method '${name}': `, "")
+    )
+  })
 }
