895ce3fbf66a1358dc6733ef96e819461d00da1b
James Kerr <kerr@hey.com>
2024-07-19 11:21:03 -0700

    Persist the query sessions'

--- apps/zui/src/models/query-session.ts
+++ apps/zui/src/models/query-session.ts
@@ -1,11 +1,14 @@
 import {AttributeTypes} from "bullet"
 import {ApplicationEntity} from "./application-entity"
+import {EntityState} from "@reduxjs/toolkit"
 
 const schema = {
   name: {type: String, default: null as string},
 }
 
 type Attributes = AttributeTypes<typeof schema>
+
+export type QuerySessionState = EntityState<Attributes, string>
 
 export class QuerySession extends ApplicationEntity<Attributes> {
   static schema = schema
