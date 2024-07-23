ece6617691ffbb41a9680547a02632a91f21dfb4
James Kerr <kerr@hey.com>
2024-07-23 10:06:55 -0700

    Write the migration for the query_sessions

--- apps/zui/src/js/state/migrations/202407221450_populateSessions.test.ts
+++ apps/zui/src/js/state/migrations/202407221450_populateSessions.test.ts
@@ -1,7 +1,32 @@
 import {migrate} from "src/test/unit/helpers/migrate"
+import {getAllStates} from "./utils/getTestState"
 
 test("migrating 202407221450_populateSessions", async () => {
   const next = await migrate({state: "v1.17.0", to: "202407221450"})
 
-  // expect(next).toBe)
+  const result = {
+    entities: {
+      KbZNe9FuSHnKKfB398B0Z: {
+        id: "KbZNe9FuSHnKKfB398B0Z",
+        name: null,
+      },
+      Zf8vsxTZ4mqT7IK1OtvRf: {
+        id: "Zf8vsxTZ4mqT7IK1OtvRf",
+        name: null,
+      },
+      sIpManYfhNgo6gWdu10bA: {
+        id: "sIpManYfhNgo6gWdu10bA",
+        name: null,
+      },
+    },
+    ids: [
+      "KbZNe9FuSHnKKfB398B0Z",
+      "sIpManYfhNgo6gWdu10bA",
+      "Zf8vsxTZ4mqT7IK1OtvRf",
+    ],
+  }
+
+  for (const state of getAllStates(next)) {
+    expect(state.query_sessions).toEqual(result)
+  }
 })
--- apps/zui/src/js/state/migrations/202407221450_populateSessions.ts
+++ apps/zui/src/js/state/migrations/202407221450_populateSessions.ts
@@ -1,12 +1,40 @@
-import {getAllRendererStates, getAllTabs} from "./utils/getTestState"
+import {getAllRendererStates, getAllStates} from "./utils/getTestState"
 
 export default function populateSessions(state: any) {
-  for (let win of getAllRendererStates(state)) {
-    console.log(win.query_sessions)
+  let sessions = []
+
+  // First gather all the tab histories,
+  for (const win of getAllRendererStates(state)) {
+    if (!win.tabHistories) break
+
+    const entities = win.tabHistories.ids.map(
+      (id) => win.tabHistories.entities[id]
+    )
+    // filter the ones that are on a query session url
+    const sessionTabs = entities.filter((history) =>
+      /queries\/.*\/versions\/.*/.test(history.entries[history.index])
+    )
+    // Push those tab ides to the session
+    sessions = sessions.concat(
+      sessionTabs.map((tab) => {
+        return {id: tab.id, name: null}
+      })
+    )
   }
 
-  for (const tab of getAllTabs(state)) {
-    // console.log(tab)
+  // Put these in the entity state shape
+  const query_sessions = {
+    ids: sessions.map((session) => session.id),
+    entities: sessions.reduce((entities, session) => {
+      entities[session.id] = session
+      return entities
+    }, {}),
   }
+
+  // Next add all those sessions to each state object, since these will be global
+  for (const s of getAllStates(state)) {
+    s.query_sessions = query_sessions
+  }
+
   return state
 }
