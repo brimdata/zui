f5b117816bc627f2b0ee91a0a3e0701222f984e2
James Kerr <kerr@hey.com>
2024-09-30 11:55:41 -0700

    Add time and names of queries to history

--- apps/zui/src/views/right-pane/history/handler.ts
+++ apps/zui/src/views/right-pane/history/handler.ts
@@ -1,7 +1,9 @@
+import {formatDistanceToNowStrict} from "date-fns"
 import {useMemo} from "react"
 import {useSelector} from "react-redux"
 import {ViewHandler} from "src/core/view-handler"
 import Current from "src/js/state/Current"
+import Queries from "src/js/state/Queries"
 import {QuerySession} from "src/models/query-session"
 import {Snapshot} from "src/models/snapshot"
 
@@ -16,11 +18,24 @@ export class HistoryHandler extends ViewHandler {
   useEntries() {
     const sessionId = useSelector(Current.getSnapshot).sessionId
     const snapshots = Snapshot.useWhere({sessionId})
+    const _queries = useSelector(Queries.raw)
     return useMemo(() => snapshots.slice(0).reverse(), [snapshots])
   }
 
   onActivate(id: string) {
     const snapshot = Snapshot.find(id)
     QuerySession.load(snapshot)
+  }
+
+  formatTimestamp(date: Date) {
+    if (!date) return "-"
+    try {
+      let text = formatDistanceToNowStrict(date)
+      if (/second/.test(text)) return "now"
+      else return text.replace("second", "sec").replace("minute", "min")
+    } catch (e) {
+      console.error(e)
+      return ""
+    }
   }
 }
--- apps/zui/src/views/right-pane/history/history-item.tsx
+++ apps/zui/src/views/right-pane/history/history-item.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import {NodeRendererProps} from "react-arborist"
 import {Snapshot} from "src/models/snapshot"
 import {HistoryHandler} from "./handler"
+import classNames from "classnames"
 
 type Props = NodeRendererProps<Snapshot> & {handler: HistoryHandler}
 
@@ -13,9 +14,16 @@ export function HistoryItem({node, handler, style}: Props) {
       onClick={() => handler.onActivate(node.data.id)}
     >
       <div className="h-full gutter-half">
-        <div className="sidebar-item gutter-half h-full flex items-center">
-          <span className="truncate font:mono step--1">
-            {node.data.queryText}
+        <div className="sidebar-item gutter-half h-full repel step--1 flex-nowrap">
+          <span
+            className={classNames("truncate font:mono", {
+              "weight:bold": node.data.query,
+            })}
+          >
+            {node.data.query?.name ?? node.data.queryText}
+          </span>
+          <span className="nowrap text-meta">
+            {handler.formatTimestamp(node.data.createdAt)}
           </span>
         </div>
       </div>
