bd36dd2378585d2a84f1745015ae4322067a377b
James Kerr <kerr@hey.com>
2024-09-30 11:57:43 -0700

    Fix lint

--- apps/zui/src/js/state/migrations/202409271055_migrateToSnapshots.test.ts
+++ apps/zui/src/js/state/migrations/202409271055_migrateToSnapshots.test.ts
@@ -1,6 +1,6 @@
 import {migrate} from "src/test/unit/helpers/migrate"
 
 test("migrating 202409271055_migrateToSnapshots", async () => {
-  const next = await migrate({state: "v1.18.0", to: "202409271055"})
-  // console.log(snapshots)
+  const _next = await migrate({state: "v1.18.0", to: "202409271055"})
+  /* Create the assertions here */
 })
