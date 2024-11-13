44d8e4f3382b2af40e1f40e7e32bc922369925d5
James Kerr <kerr@hey.com>
2024-11-13 12:09:00 -0800

    Tests pass

--- apps/zui/src/electron/reset-state.test.ts
+++ apps/zui/src/electron/reset-state.test.ts
@@ -2,14 +2,6 @@ import "src/test/system/real-paths"
 import {app} from "electron"
 import {MainObject} from "../core/main/main-object"
 
-jest.mock("./session", () => {
-  return () => ({
-    load: jest.fn(() => Promise.resolve()),
-    delete: jest.fn(() => Promise.resolve()),
-    save: jest.fn(() => Promise.resolve()),
-  })
-})
-
 test("reset state", async () => {
   const main = await MainObject.boot({
     lake: false,
