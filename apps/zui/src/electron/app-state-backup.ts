0383d64179f16474608e8abb483ca920530d3f05
James Kerr <kerr@hey.com>
2024-11-13 13:07:35 -0800

    Ensure backup files are not overwritten

--- apps/zui/src/electron/app-state-backup.ts
+++ apps/zui/src/electron/app-state-backup.ts
@@ -1,14 +1,15 @@
 import path from "path"
 import fs from "fs"
 import {AppStateFile} from "./app-state-file"
+import {plusOne} from "src/util/plus-one"
 
 export class AppStateBackup {
   constructor(public dir: string) {}
 
   save(file: AppStateFile) {
-    const backupName = `${file.version}_${file.name}`
-    const backupPath = this.join(backupName)
     if (!fs.existsSync(this.dir)) fs.mkdirSync(this.dir)
+    const backupName = plusOne(`${file.version}_${file.name}`)
+    const backupPath = this.join(backupName)
     fs.copyFileSync(file.path, backupPath)
   }
 
