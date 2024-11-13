45265f4cd76faf179dddc78d249ce39713786cd6
James Kerr <kerr@hey.com>
2024-11-13 15:57:20 -0800

    Clean up error handling

--- apps/zui/src/electron/app-state-file.ts
+++ apps/zui/src/electron/app-state-file.ts
@@ -6,26 +6,11 @@ export class AppStateFile {
   state: any = undefined
 
   constructor(public path: string) {
-    if (this.path && fs.existsSync(path)) {
-      const contents = fs.readFileSync(this.path, "utf-8").toString()
-      if (contents == "") return
-      let object
-      try {
-        object = JSON.parse(fs.readFileSync(this.path, "utf8"))
-      } catch (e) {
-        throw new Error(
-          "The application state file could not be parsed as JSON:\npath: " +
-            this.path
-        )
-      }
-      if (!object || typeof object !== "object" || !isNumber(object.version)) {
-        throw new Error(
-          "The application state file is a JSON object but is missing the top-level version key of type number\npath: " +
-            this.path
-        )
-      }
-      this.state = object
-    }
+    if (this.noFile) return
+    if (this.noContent) return
+    this.state = this.parse()
+    if (this.noJSON) throw new Error(JSON_ERROR_MSG(this.path))
+    if (this.noVersion) throw new Error(VERSION_ERROR_MSG(this.path))
   }
 
   create(version: number) {
@@ -64,4 +49,35 @@ export class AppStateFile {
   get data() {
     return this.state.data
   }
+
+  private get noFile() {
+    return !fs.existsSync(this.path)
+  }
+
+  private get noContent() {
+    return fs.statSync(this.path).size === 0
+  }
+
+  private get noJSON() {
+    return !this.state
+  }
+
+  private get noVersion() {
+    return !(typeof this.state === "object" && isNumber(this.state.version))
+  }
+
+  private parse() {
+    try {
+      return JSON.parse(fs.readFileSync(this.path, "utf8"))
+    } catch {
+      return null
+    }
+  }
 }
+
+const JSON_ERROR_MSG = (path) =>
+  "The application state file could not be parsed as JSON:\npath: " + path
+
+const VERSION_ERROR_MSG = (path) =>
+  "The application state file is a JSON object but is missing the top-level version key of type number\npath: " +
+  path
