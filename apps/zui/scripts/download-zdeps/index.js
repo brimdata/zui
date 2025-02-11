b8e857486f3e1720af7b32049c94f309e6b283e4
James Kerr <kerr@hey.com>
2025-02-11 13:47:28 -0800

    zui tests pass

--- apps/zui/scripts/download-zdeps/index.js
+++ apps/zui/scripts/download-zdeps/index.js
@@ -5,26 +5,23 @@ const fs = require("fs-extra")
 const got = require("got")
 const path = require("path")
 const tmp = require("tmp")
-const zuiPackage = require("../../package.json")
+const package = require("../../package.json")
 const decompress = require("decompress")
-const zdepsPath = path.resolve("zdeps")
+const depsDir = path.resolve("zdeps")
 
 const platformDefs = {
   darwin: {
-    zqBin: "zq",
-    zedBin: "zed",
+    superBin: "super",
     osarch: "darwin-amd64",
     ext: "tar.gz",
   },
   linux: {
-    zqBin: "zq",
-    zedBin: "zed",
+    superBin: "super",
     osarch: "linux-amd64",
     ext: "tar.gz",
   },
   win32: {
-    zqBin: "zq.exe",
-    zedBin: "zed.exe",
+    superBin: "super.exe",
     osarch: "windows-amd64",
     ext: "zip",
   },
@@ -52,11 +49,11 @@ async function unzipTo(zipfile, dir) {
   await decompress(zipfile, dir)
 }
 
-function zedArtifactPaths(version) {
+function artifactPaths(version) {
   const plat = platformDefs[process.platform]
 
-  const artifactFile = `zed-${version}.${plat.osarch}.${plat.ext}`
-  const artifactUrl = `https://github.com/brimdata/zed/releases/download/${version}/${artifactFile}`
+  const artifactFile = `super-${version}.${plat.osarch}.${plat.ext}`
+  const artifactUrl = `https://github.com/brimdata/super/releases/download/${version}/${artifactFile}`
 
   return {
     artifactFile,
@@ -66,9 +63,9 @@ function zedArtifactPaths(version) {
 
 // Download and extract the zed binary for this platform to the specified
 // directory. Returns the absolute path of the zed binary file.
-async function zedArtifactsDownload(version, destPath) {
+async function artifactsDownload(version, destPath) {
   const plat = platformDefs[process.platform]
-  const paths = zedArtifactPaths(version)
+  const paths = artifactPaths(version)
 
   const tmpdir = tmp.dirSync({unsafeCleanup: true})
   try {
@@ -78,7 +75,7 @@ async function zedArtifactsDownload(version, destPath) {
     console.log("Download and unzip success")
     fs.mkdirpSync(destPath)
 
-    for (let f of [plat.zqBin, plat.zedBin]) {
+    for (let f of [plat.superBin]) {
       fs.moveSync(path.join(tmpdir.name, f), path.join(destPath, f), {
         overwrite: true,
       })
@@ -88,74 +85,51 @@ async function zedArtifactsDownload(version, destPath) {
   }
 }
 
-async function zedDevBuild(destPath) {
+async function devBuild(destPath) {
   if (!(process.platform in platformDefs)) {
     throw new Error("unsupported platform")
   }
   const plat = platformDefs[process.platform]
 
-  const zedPackageDir = path.join(
+  const packageDir = path.join(
     __dirname,
     "..",
     "..",
     "..",
     "..",
     "node_modules",
-    "zed"
+    "super"
   )
 
   fs.mkdirpSync(destPath)
 
-  for (let f of [plat.zqBin, plat.zedBin]) {
-    fs.copyFileSync(path.join(zedPackageDir, "dist", f), path.join(destPath, f))
-  }
-}
-
-// Suricata rules are dropped from the Windows build to fix a false positive
-// malware flagging. See https://github.com/brimdata/zui/issues/2857.
-const filterBrimcapZdeps = (src, dest) => {
-  if (process.platform == "win32" &&
-      (/suricata\.rules$/.test(src) || /emerging\.rules\.tar\.gz$/.test(src)) &&
-      fs.statSync(src).isFile()) {
-    return false
-  } else {
-    return true
+  for (let f of [plat.superBin]) {
+    fs.copyFileSync(path.join(packageDir, "dist", f), path.join(destPath, f))
   }
 }
 
 async function main() {
   try {
-    fs.copySync(
-      path.resolve("..", "..", "node_modules", "brimcap", "build", "dist"),
-      zdepsPath,
-      { filter: filterBrimcapZdeps }
-    )
-    const brimcapVersion = child_process
-      .execSync(path.join(zdepsPath, "brimcap") + " -version")
-      .toString()
-      .trim()
-    console.log("copied brimcap artifacts " + brimcapVersion)
-
     // The Zed dependency should be a git tag or commit. Any tag that
     // begins with "v*" is expected to be a released artifact, and will
     // be downloaded from the Zed repository. Otherwise, copy Zed
     // artifacts from node_modules via zedDevBuild.
-    const zedVersion = zuiPackage.devDependencies.zed.split("#")[1]
-    if (zedVersion.startsWith("v")) {
-      await zedArtifactsDownload(zedVersion, zdepsPath)
-      console.log("downloaded Zed artifacts version " + zedVersion)
+    const version = package.devDependencies.super.split("#")[1]
+    if (version.startsWith("v")) {
+      await artifactsDownload(version, depsDir)
+      console.log("downloaded artifacts version " + version)
     } else {
-      await zedDevBuild(zdepsPath)
+      await devBuild(depsDir)
       // Print the version inside zq derived during prepack as
       // opposed to what's in package.json.
-      let realZqVersion = child_process
-        .execSync(path.join(zdepsPath, "zq") + " -version")
+      let realVersion = child_process
+        .execSync(path.join(depsDir, "super") + " -version")
         .toString()
         .trim()
-      console.log("copied Zed artifacts " + realZqVersion)
+      console.log("copied artifacts " + realVersion)
     }
   } catch (err) {
-    console.error("zdeps setup: ", err)
+    console.error("setup: ", err)
     process.exit(1)
   }
 }
--- apps/zui/src/core/zq.ts
+++ apps/zui/src/core/zq.ts
@@ -2,7 +2,7 @@ import * as zed from "@brimdata/zed-node"
 import * as zdeps from "../electron/zdeps"
 
 export const zq = ((opts) => {
-  return zed.zq({...opts, bin: zdeps.zq})
+  return zed.zq({...opts, bin: zdeps.superdb})
 }) as typeof zed.zq
 
 export const createReadableStream = ((opts) => {
