8713bba7c7db6cdc399d1d1610fda48e7164ecf9
James Kerr <kerr@hey.com>
2025-02-18 13:46:38 -0800

    Remove Zed Wasm (#3171)

--- apps/zui/src/electron/paths.ts
+++ apps/zui/src/electron/paths.ts
@@ -1,18 +1,7 @@
 import {app} from "electron"
-import os from "os"
 import {join} from "path"
 
-const exe = (name) => (os.platform() === "win32" ? `${name}.exe` : name)
-
 class Paths {
-  brimcap() {
-    return join(
-      app.getAppPath().replace("app.asar", "app.asar.unpacked"),
-      "zdeps",
-      exe("brimcap")
-    )
-  }
-
   root() {
     return app.getAppPath()
   }
--- packages/zed-wasm/lib/node-globals.js
+++
@@ -1,9 +0,0 @@
-import crypto from 'node:crypto';
-import getRandomValues from 'polyfill-crypto.getrandomvalues';
-
-globalThis.crypto = { ...crypto, getRandomValues };
-globalThis.requestAnimationFrame = setTimeout;
-globalThis.require = require;
-globalThis.fs = require('fs');
-globalThis.TextEncoder = require('util').TextEncoder;
-globalThis.TextDecoder = require('util').TextDecoder;
