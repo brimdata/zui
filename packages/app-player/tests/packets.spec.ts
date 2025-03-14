231a1fd1a8efda735276e400e3ecbb8397fe9ff2
Phil Rzewski <phil@brimdata.io>
2025-03-13 17:34:04 -0700

    Add debug line

--- packages/app-player/tests/packets.spec.ts
+++ packages/app-player/tests/packets.spec.ts
@@ -4,6 +4,7 @@ import { getPath } from '@brimdata/sample-data';
 play('packets.spec', (app, test) => {
   test('dropping a pcap throw an error now', async () => {
     await app.dropFile(getPath('sample.pcap'));
+    console.log('I did dropfile');
     await app.attached(/wrong number of fields/);
   });
 });
